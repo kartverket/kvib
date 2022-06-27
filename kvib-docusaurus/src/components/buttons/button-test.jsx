8be92afb8bc0a9400882fb607fa1354cd852421b
FarAhm1 <farhad.ahmadhadi@kartverket.no>
2022-06-27 12:42:40 +0200

    Importert button.css i main.css

--- kvib-docusaurus/src/components/buttons/button-test.jsx
+++ kvib-docusaurus/src/components/buttons/button-test.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import '../../../../css/buttons.css';
+import '../../../../css/main.css';
 
 export default function BasicButtons() {
   return (
     <Stack spacing={2} direction="row">
-      <Button claas={"kv-button-blue-primary"} variant="text">Text</Button>
-      <Button claas={"kv-button--xs"} variant="contained">Contained</Button>
-      <Button claas={"kv-button--xs"} variant="outlined">Outlined</Button>
+      <Button className={"kv-button--l"} variant="text">Text</Button>
+      <Button className={"kv-button--xs"} variant="contained">Contained</Button>
+      <Button className={"kv-button--xs"} variant="outlined">Outlined</Button>
     </Stack>
   );
 }
