def2dbe01cc4e306e9df653e17c6ddc7d8253573
FarAhm1 <farhad.ahmadhadi@kartverket.no>
2022-06-27 12:06:57 +0200

    Slettet CSS mappen i Kvib-docusaurus, la til component-test.mdx for test av react components.

--- kvib-docusaurus/src/components/ColorsPage/Colors.jsx
+++ kvib-docusaurus/src/components/ColorsPage/Colors.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import '../../css/palettes.css'
+import './palettes.css';
 import {Typography} from "@mui/material";
 
 
