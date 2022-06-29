da419e10fc02bf328a2e16a02b832beefccede49
FarAhm1 <46712165+FarAhm1@users.noreply.github.com>
2022-06-29 10:38:29 +0200

    Update index.js

--- kvib-docusaurus/src/index.js
+++ kvib-docusaurus/src/index.js
@@ -1,7 +1,7 @@
 import * as React from "@types/react";
 import ReactDOM from 'react-dom';
 
-import './css/main.module.css';
+import './css/main.css';
 import Colors from './components/ColorsPage/Colors';
 
 ReactDOM.render(<Colors />, document.getElementById('root'));
