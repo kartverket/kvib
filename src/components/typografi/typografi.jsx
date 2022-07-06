3445b920779ea6d67cd367908ef6a46ac4610907
eliseki <eliseki@stud.ntnu.no>
2022-07-06 10:56:27 +0200

    rettet BEM bug, css-klassenavn

--- src/components/typografi/typografi.jsx
+++ src/components/typografi/typografi.jsx
@@ -4,45 +4,45 @@ import '../../css/main.css';
 
 
 const h1Variants = [
-    { størrelse: "Extra Small", utseende: "H1 / Mulish / Bold / 20 ", klasse: "heading heading_h1--x-small" },
-    { størrelse: "Small", utseende: "H1 / Mulish / Bold / 24 ", klasse: "heading heading_h1--small" },
-    { størrelse: "Medium", utseende: "H1 / Mulish / Bold / 32 ", klasse: "heading heading_h1--medium"},
-    { størrelse: "Large", utseende: "H1 / Mulish / Bold / 36 ", klasse: "heading heading_h1--large"}
+    { størrelse: "Extra Small", utseende: "H1 / Mulish / Bold / 20 ", klasse: "heading heading__h1--xs" },
+    { størrelse: "Small", utseende: "H1 / Mulish / Bold / 24 ", klasse: "heading heading__h1--s" },
+    { størrelse: "Medium", utseende: "H1 / Mulish / Bold / 32 ", klasse: "heading heading__h1--m"},
+    { størrelse: "Large", utseende: "H1 / Mulish / Bold / 36 ", klasse: "heading heading__h1--l"}
   ]
 
 const h2Variants = [
-    { størrelse: "Extra Small", utseende: "H2 / Mulish / Bold / 18 ", klasse: "heading heading_h2--x-small" },
-    { størrelse: "Small", utseende: "H2 / Mulish / Bold / 20 ", klasse: "heading heading_h2--small" },
-    { størrelse: "Medium", utseende: "H2 / Mulish / Bold / 24 ", klasse: "heading heading_h2--medium"},
-    { størrelse: "Large", utseende: "H2 / Mulish / Bold / 26 ", klasse: "heading heading_h2--large"}
+    { størrelse: "Extra Small", utseende: "H2 / Mulish / Bold / 18 ", klasse: "heading heading__h2--xs" },
+    { størrelse: "Small", utseende: "H2 / Mulish / Bold / 20 ", klasse: "heading heading__h2--s" },
+    { størrelse: "Medium", utseende: "H2 / Mulish / Bold / 24 ", klasse: "heading heading__h2--m"},
+    { størrelse: "Large", utseende: "H2 / Mulish / Bold / 26 ", klasse: "heading heading__h2--l"}
   ]
 
 const h3Variants= [
-    { størrelse: "Extra Small", utseende: "H3 / Mulish / Bold / 16 ", klasse: "heading heading_h3--x-small" },
-    { størrelse: "Small", utseende: "H3 / Mulish / Bold / 18 ", klasse: "heading heading_h3--small-medium" },
-    { størrelse: "Medium", utseende: "H3 / Mulish / Bold / 18 ", klasse: "heading heading_h3--small-medium"},
-    { størrelse: "Large", utseende: "H3 / Mulish / Bold / 20 ", klasse: "heading heading_h3--large"}
+    { størrelse: "Extra Small", utseende: "H3 / Mulish / Bold / 16 ", klasse: "heading heading__h3--xs" },
+    { størrelse: "Small", utseende: "H3 / Mulish / Bold / 18 ", klasse: "heading heading__h3--sm" },
+    { størrelse: "Medium", utseende: "H3 / Mulish / Bold / 18 ", klasse: "heading heading__h3--sm"},
+    { størrelse: "Large", utseende: "H3 / Mulish / Bold / 20 ", klasse: "heading heading__h3--l"}
   ]
 
 const bodyTextVariants= [
-    { størrelse: "Extra Small", utseende: "Brødtekst / Mulish / Regular / 16 ", klasse: "body-text body-text--x-small" },
-    { størrelse: "Small", utseende: "Brødtekst / Mulish / Regular / 18 ", klasse: "body-text body-text--small-medium-large" },
-    { størrelse: "Medium", utseende: "Brødtekst / Mulish / Regular / 18 ", klasse: "body-text body-text--small-medium-large"},
-    { størrelse: "Large", utseende: "Brødtekst / Mulish / Regular / 18 ", klasse: "body-text body-text--small-medium-large"}
+    { størrelse: "Extra Small", utseende: "Brødtekst / Mulish / Regular / 16 ", klasse: "body-text body-text--xs" },
+    { størrelse: "Small", utseende: "Brødtekst / Mulish / Regular / 18 ", klasse: "body-text body-text--sml" },
+    { størrelse: "Medium", utseende: "Brødtekst / Mulish / Regular / 18 ", klasse: "body-text body-text--sml"},
+    { størrelse: "Large", utseende: "Brødtekst / Mulish / Regular / 18 ", klasse: "body-text body-text--sml"}
   ]
 
   const labelVariants= [
-    { størrelse: "Extra Small", utseende: "Label / Mulish / Regular / 12 ", klasse: "label label--x-small" },
-    { størrelse: "Small", utseende: "Label / Mulish / Regular / 14 ", klasse: "label label--small-medium-large" },
-    { størrelse: "Medium", utseende: "Label / Mulish / Regular / 14 ", klasse: "label label--small-medium-large"},
-    { størrelse: "Large", utseende: "Label / Mulish / Regular / 14", klasse: "label label--small-medium-large"}
+    { størrelse: "Extra Small", utseende: "Label / Mulish / Regular / 12 ", klasse: "label label--xs" },
+    { størrelse: "Small", utseende: "Label / Mulish / Regular / 14 ", klasse: "label label--sml" },
+    { størrelse: "Medium", utseende: "Label / Mulish / Regular / 14 ", klasse: "label label--sml"},
+    { størrelse: "Large", utseende: "Label / Mulish / Regular / 14", klasse: "label label--sml"}
   ]
 
   const detailVariants= [
-    { størrelse: "Extra Small", utseende: "Label / Mulish / Regular / 12 ", klasse: "detail detail--x-small" },
-    { størrelse: "Small", utseende: "Label / Mulish / Regular / 14 ", klasse: "detail detail--small-medium-large" },
-    { størrelse: "Medium", utseende: "Label / Mulish / Regular / 14 ", klasse: "detail detail--small-medium-large"},
-    { størrelse: "Large", utseende: "Label / Mulish / Regular / 14", klasse: "detail detail--small-medium-large"}
+    { størrelse: "Extra Small", utseende: "Label / Mulish / Regular / 12 ", klasse: "detail detail--xs" },
+    { størrelse: "Small", utseende: "Label / Mulish / Regular / 14 ", klasse: "detail detail--sml" },
+    { størrelse: "Medium", utseende: "Label / Mulish / Regular / 14 ", klasse: "detail detail--sml"},
+    { størrelse: "Large", utseende: "Label / Mulish / Regular / 14", klasse: "detail detail--sml"}
   ]
 
 
@@ -194,12 +194,12 @@ function Underline(){
             </tr>
                 <tr>
                     <td>Med understrekning</td>
-                    <td class="detail--small-medium-large underline">Alpha Bravo Charlie</td>
+                    <td class="detail--sml underline">Alpha Bravo Charlie</td>
                     <td> underline</td>
                 </tr>
                 <tr>
                     <td>Uten understrekning</td>
-                    <td class="detail--small-medium-large">Alpha Bravo Charlie</td>
+                    <td class="detail--sml">Alpha Bravo Charlie</td>
                     <td></td>
                 </tr>
         </table>
