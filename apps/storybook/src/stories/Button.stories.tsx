07e2934fac3325302e46f63c0764d08986ed73cd
Espen Henriksen <espen@oslomarketsolutions.no>
2022-11-10 22:31:13 +0100

    Add chromatic + tweaks

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -5,9 +5,6 @@ import { Button } from "ui";
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
@@ -15,22 +12,22 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  children: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  children: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  children: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Button',
+  children: 'Button',
 };
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,29 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
-};
+import { ButtonHTMLAttributes } from 'react';
+import styled from "styled-components";
+
+const StyledButton = styled.button<Pick<Props, 'primary' | 'size'>>`
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Are am I a primary buttom man? */
+  primary?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  children?: React.ReactNode;
+}
+
+export const Button = ({ 
+  primary = false,
+  size = 'medium',
+  children,
+  ...props
+ }: Props ) => (
+  <StyledButton {...props} primary={primary} size={size}>{children}</StyledButton>
+)
