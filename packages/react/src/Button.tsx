9639a821903479273d07b6191ac4c615685e3a0a
Anna Li <halloannali@gmail.com>
2023-03-22 14:13:41 +0100

    eksportere props

--- packages/react/src/Button.tsx
+++ packages/react/src/Button.tsx
@@ -1,5 +1,7 @@
 import { Button as ChakraButton, ButtonProps as ChakraButtonProps } from "@chakra-ui/react";
 
+export type ButtonProps = ChakraButtonProps;
+
 export const Button = ({ children, ...props }: ChakraButtonProps) => {
   return (
     <ChakraButton {...props} colorScheme="brand">
