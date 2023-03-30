a5c62015090baa62bdc6e4533d7994a0d763c1a2
Anna Li <halloannali@gmail.com>
2023-03-30 11:41:32 +0200

    skrevet docs for button

--- apps/storybook/stories/components/Button.stories.tsx
+++ apps/storybook/stories/components/Button.stories.tsx
@@ -1,27 +1,60 @@
 import { Button, ButtonProps } from "@kvib/react/src/components/Button";
+import { Home } from "@mui/icons-material";
 import { StoryFn, Meta } from "@storybook/react";
 
 const meta: Meta<typeof Button> = {
   title: "Komponenter/Knapper",
   component: Button,
+  parameters: {
+    docs: {
+      story: { inline: true }, // render the story in an iframe
+      canvas: { sourceState: "shown" }, // start with the source open
+    },
+  },
   argTypes: {
     children: {
-      defaultValue: "",
+      description: "Text in button",
+      table: {
+        type: { summary: "string" },
+      },
       control: "text",
     },
     variant: {
+      description: "Variant",
+      table: {
+        type: { summary: "primary | secondary | tertiary" },
+      },
       options: ["primary", "secondary", "tertiary"],
       control: { type: "radio" },
     },
     colorScheme: {
+      description: "Color of button",
+      table: {
+        type: { summary: "green | blue" },
+      },
       options: ["green", "blue"],
       control: { type: "radio" },
     },
     size: {
+      description: "Size of button",
+      table: {
+        type: { summary: "sm | md" },
+      },
       options: ["sm", "md"],
       control: { type: "radio" },
     },
     isDisabled: {
+      description: "Toggles if button should be disabled",
+      table: {
+        type: { summary: "boolean" },
+      },
+      control: "boolean",
+    },
+    isLoading: {
+      description: "Toggles if button should be loading",
+      table: {
+        type: { summary: "boolean" },
+      },
       control: "boolean",
     },
   },
@@ -35,8 +68,8 @@ const Template: StoryFn<ButtonProps> = (args) => {
   return <Button {...args}>{children}</Button>;
 };
 
-export const Component = Template.bind({});
-Component.args = {
+export const Default = Template.bind({});
+Default.args = {
   children: "Klikk her",
   variant: "primary",
   colorScheme: "green",
@@ -45,4 +78,8 @@ Component.args = {
   isLoading: false,
 };
 
-Component.storyName = "Button";
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  children: "Knapp med ikon",
+  leftIcon: <Home />,
+};
