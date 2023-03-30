9d0a28d82e2ba1c9fac986e99e733b557587e625
Anna Li <halloannali@gmail.com>
2023-03-30 19:38:10 +0200

    endret navn og url

--- apps/storybook/.storybook/theme.js
+++ apps/storybook/.storybook/theme.js
@@ -3,8 +3,8 @@ import logo from "./kvib_logo.svg";
 
 export default create({
   base: "light",
-  brandTitle: "KVIB Components",
-  brandUrl: "https://kartverket.no",
+  brandTitle: "KVIB",
+  brandUrl: "https://kartverket.github.io/kvib/storybook",
   brandImage: logo,
   brandTarget: "_self",
 });
