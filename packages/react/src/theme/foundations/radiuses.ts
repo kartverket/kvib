918e67c8808ad15050bbee8c4ce90d7e1c5dbc5d
Sebastian Piotr Maro <marseb@kartverket.no>
2023-04-21 10:07:55 +0200

    La til radiuser. Prøvde å gjøre det mer "futureproof" enn skyggene

---
+++ packages/react/src/theme/foundations/radiuses.ts
@@ -0,0 +1,20 @@
+export const radiuses = {
+  none: {
+    value: "0rem", //0px
+  },
+  sm: {
+    value: "0.125rem", //2px
+  },
+  base: {
+    value: "0.25rem", //4px
+  },
+  md: {
+    value: "0.375rem", //6px
+  },
+  lg: {
+    value: "0.5rem", //8px
+  },
+  xl: {
+    value: "0.75rem", // 12px
+  },
+};
