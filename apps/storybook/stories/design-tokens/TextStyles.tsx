53e41838fbec60e5498fd8bd9ec07ba1cc211ab9
Anna Li <halloannali@gmail.com>
2023-05-09 18:55:34 +0200

    added text styles to storybook

---
+++ apps/storybook/stories/design-tokens/TextStyles.tsx
@@ -0,0 +1,44 @@
+import { Text, Table, Thead, Tbody, Tr, Th, Td, TableContainer, Code } from "@chakra-ui/react";
+import { textStyles } from "@kvib/react/src/theme/foundations";
+
+export const TextStyles = () => {
+  const renderValue = (value: string | string[]) => {
+    if (typeof value === "string") {
+      return value;
+    }
+
+    return value.map((size: any) => `${size} | `);
+  };
+
+  return (
+    <TableContainer>
+      <Table variant="simple" width="100%">
+        <Thead textAlign="left">
+          <Tr>
+            <Th width="20%">Navn</Th>
+            <Th width="50%">Verdi</Th>
+            <Th width="30%">Kode</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {Object.entries(textStyles).map(([name, value]) => (
+            <Tr key={name}>
+              <Td backgroundColor="white">
+                <Text variant={name}>{name}</Text>
+              </Td>
+              <Td backgroundColor="white">
+                <Text margin="0">font-size: {renderValue(value.fontSize)}</Text>
+                <Text margin="0">font-weight: {value.fontWeight}</Text>
+                <Text margin="0">line-height: {value.lineHeight}</Text>
+                <Text margin="0">letter-spacing: {renderValue(value.letterSpacing)}</Text>
+              </Td>
+              <Td backgroundColor="white">
+                <Code>{name}</Code>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
