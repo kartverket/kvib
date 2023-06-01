9ebbaf7b14881aeabd16d17ba8bacac879bbf2be
Sebastian Piotr Maro <marseb@kartverket.no>
2023-06-01 08:32:43 +0200

    la på riktig styling med riktig størrelser

--- packages/react/src/theme/components/radio.ts
+++ packages/react/src/theme/components/radio.ts
@@ -1,25 +1,75 @@
 import { radioAnatomy } from "@chakra-ui/anatomy";
 import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
+import { colors } from "../foundations";
 
 const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(radioAnatomy.keys);
 
+const sizes = {
+  sm: definePartsStyle({
+    control: { width: "20px", height: "20px" },
+    label: { fontSize: "sm" },
+  }),
+  md: definePartsStyle({
+    control: { w: "24px", h: "24px" },
+    label: { fontSize: "md" },
+  }),
+  lg: definePartsStyle({
+    control: { w: "32px", h: "32px" },
+    label: { fontSize: "lg" },
+  }),
+};
+// @ts-ignore
 const baseStyle = ({ colorScheme }) =>
   definePartsStyle({
+    container: {
+      _hover: {
+        "input:enabled + .chakra-radio__control": {
+          borderColor: `${colorScheme}.400`,
+          color: `${colorScheme}.400`,
+        },
+        ".chakra-radio:disabled": {
+          pointerEvents: "visiblePainted",
+        },
+      },
+    },
     control: {
       borderColor: `${colorScheme}.500`,
-      _hover: {
-        borderColor: `${colorScheme}.400`,
-      },
+      backgroundColor: "white",
       _checked: {
         backgroundColor: "white",
         color: `${colorScheme}.500`,
+        // _before: {
+        // h: "50%",
+        // w: "50%",
+        // },
         _hover: {
           color: `${colorScheme}.400`,
           borderColor: `${colorScheme}.400`,
           backgroundColor: "white",
         },
       },
+      _disabled: {
+        borderColor: colors.gray[400],
+        backgroundColor: "white",
+        _checked: {
+          color: colors.gray[400],
+          borderColor: colors.gray[400],
+          backgroundColor: "white",
+        },
+      },
+    },
+    label: {
+      _hover: {
+        color: `${colorScheme}.400`,
+      },
+      _disabled: {
+        opacity: "100%",
+        color: colors.gray[400],
+        _hover: {
+          color: colors.gray[400],
+        },
+      },
     },
   });
 
-export const radioTheme = defineMultiStyleConfig({ baseStyle });
+export const radioTheme = defineMultiStyleConfig({ baseStyle, sizes });
