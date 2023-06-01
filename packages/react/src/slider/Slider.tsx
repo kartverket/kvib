13e56906414e76c23e459a79fdcaa5ba67da3cb6
eliseki <eliseki@stud.ntnu.no>
2023-06-01 15:10:53 +0200

    fix: omitted unused Chakraprops

--- packages/react/src/slider/Slider.tsx
+++ packages/react/src/slider/Slider.tsx
@@ -7,12 +7,23 @@ import {
   forwardRef,
 } from "@chakra-ui/react";
 
-export type SliderProps = Omit<ChakraSliderProps, "colorScheme" | "size" | "variant"> & {
+export type SliderProps = Omit<
+  ChakraSliderProps,
+  | "colorScheme"
+  | "size"
+  | "variant"
+  | "value"
+  | "isReversed"
+  | "isReadOnly"
+  | "focusThumbOnChange"
+  | "direction"
+  | "orientation"
+> & {
   size?: "sm" | "md" | "lg";
   colorScheme?: "green" | "blue";
 };
 
-export const Slider = forwardRef<SliderProps, "div">(({ size = "lg", colorScheme = "green", ...props }, ref) => {
+export const Slider = forwardRef<SliderProps, "div">(({ size = "md", colorScheme = "green", ...props }, ref) => {
   return (
     <ChakraSlider ref={ref} {...props} colorScheme={colorScheme} size={size}>
       <SliderTrack>
