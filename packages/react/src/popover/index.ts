191b6d3406478b1fdb441ec34637d0604fc5ab5b
Anna Li <halloannali@gmail.com>
2023-06-08 15:35:42 +0200

    exported more typography components and all overlay components

---
+++ packages/react/src/popover/index.ts
@@ -0,0 +1,20 @@
+export {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+} from "@chakra-ui/react";
+export type {
+  PopoverProps,
+  PopoverContentProps,
+  PopoverHeaderProps,
+  PopoverBodyProps,
+  PopoverFooterProps,
+  PopoverArrowProps,
+  CloseButtonProps,
+} from "@chakra-ui/react";
