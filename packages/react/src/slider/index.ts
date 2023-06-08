8d5c5403164b3fb32508e41d8aedcc1e47e09592
Anna Li <halloannali@gmail.com>
2023-06-08 14:04:36 +0200

    exported slider and range slider

---
+++ packages/react/src/slider/index.ts
@@ -0,0 +1,8 @@
+export { Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark } from "@chakra-ui/react";
+export type {
+  SliderProps,
+  SliderTrackProps,
+  SliderInnerTrackProps,
+  SliderThumbProps,
+  SliderMarkProps,
+} from "@chakra-ui/react";
