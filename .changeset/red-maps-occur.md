96eacd306e818bce32eef33f8c68d3323a67cebb
Anna Li <halloannali@gmail.com>
2023-06-08 16:52:50 +0200

    exported all disclosure, navigation and media and icons components

---
+++ .changeset/red-maps-occur.md
@@ -0,0 +1,25 @@
+---
+"@kvib/react": minor
+---
+
+Eksportert følgende komponenter:
+
+Disclosure
+
+- Accordion
+- Tabs
+- VisuallyHidden
+
+Navigation
+
+- Breadcrumbs
+- Link
+- LinkBox
+- SkipNavLink
+- Stepper
+
+Media and icons
+
+- Avatar
+- Icon
+- Image
---
+++ packages/react/src/accordion/index.ts
@@ -0,0 +1,8 @@
+export { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
+export type {
+  AccordionProps,
+  AccordionItemProps,
+  AccordionButtonProps,
+  AccordionPanelProps,
+  AccordionIconProps,
+} from "@chakra-ui/react";
