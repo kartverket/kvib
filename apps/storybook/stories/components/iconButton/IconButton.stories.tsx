5ecb52e1a9f568a7fcc7a81411fa1c138dda3403
eliseki <eliseki@stud.ntnu.no>
2023-06-09 07:49:38 +0200

    feat: story for iconButton

---
+++ apps/storybook/stories/components/iconButton/IconButton.stories.tsx
@@ -0,0 +1,71 @@
+import { IconButton as KvibIconButton } from "@kvib/react/src/iconButton";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof KvibIconButton> = {
+  title: "Komponenter/Ikon Knapp",
+  component: KvibIconButton,
+  parameters: {
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: "shown" },
+    },
+  },
+  argTypes: {
+    icon: {
+      description: "Icon in button",
+      table: {
+        type: { summary: "string" },
+      },
+      control: { type: "string" },
+    },
+    variant: {
+      description: "Variant",
+      table: {
+        type: { summary: "primary | secondary" },
+        defaultValue: { summary: "primary" },
+      },
+      options: ["primary", "secondary"],
+      control: { type: "radio" },
+    },
+    colorScheme: {
+      description: "Color of button",
+      table: {
+        type: { summary: "green | blue| gray" },
+        defaultValue: { summary: "green" },
+      },
+      options: ["green", "blue", "gray"],
+      control: { type: "radio" },
+    },
+    size: {
+      description: "Size of Icon button",
+      table: {
+        type: { summary: "sm | md | lg" },
+        defaultValue: { summary: "md" },
+      },
+      options: ["sm", "md", "lg"],
+      control: { type: "radio" },
+    },
+    isDisabled: {
+      description: "Toggles if button should be disabled",
+      table: {
+        type: { summary: "boolean" },
+      },
+      control: "boolean",
+    },
+    isLoading: {
+      description: "Toggles if Icon button should be loading",
+      table: {
+        type: { summary: "boolean" },
+      },
+      control: "boolean",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof KvibIconButton>;
+
+export const IconButton: Story = {
+  args: { colorScheme: "green", size: "md", variant: "ordinary", icon: "search" },
+  render: (args) => <KvibIconButton {...args}></KvibIconButton>,
+};
