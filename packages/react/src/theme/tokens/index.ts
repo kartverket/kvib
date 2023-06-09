3fa885de8d1d376c9969f583c59653a1fc5d0e3f
eliseki <eliseki@stud.ntnu.no>
2023-06-09 11:46:15 +0200

    feat:exported chakra tokens; blur, colors, sizes, spacing, transitions, typography, z-index.

--- packages/react/src/theme/tokens/index.ts
+++ packages/react/src/theme/tokens/index.ts
@@ -1,12 +1,20 @@
+//Rene chakra-tokens, copyPaste
 export * from "./config";
 export * from "./colors";
-export * from "./fonts";
-export * from "./fontSizes";
-export * from "./fontWeights";
-export * from "./lineHeights";
-export * from "./letterSpacings";
-export * from "./textStyles";
 export * from "./shadows";
 export * from "./radius";
 export * from "./breakpoints";
 export * from "./borders";
+export * from "./blur";
+export * from "./sizes";
+export * from "./spacing";
+export * from "./transitions";
+export * from "./typography";
+export * from "./z-index";
+
+// export * from "./textStyles";
+// export * from "./fontSizes";
+// export * from "./fonts";
+// export * from "./letterSpacings";
+// export * from "./fontWeights";
+// export * from "./lineHeights";
