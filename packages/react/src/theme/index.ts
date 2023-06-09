65d02b192a919cfc2ae13724dffd96cfb6d6ef11
Anna Li <halloannali@gmail.com>
2023-06-09 15:37:34 +0200

    fjerne custom stiling fra kvibifiserte komponenter

--- packages/react/src/theme/index.ts
+++ packages/react/src/theme/index.ts
@@ -1,6 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 import { theme as defaultTheme } from "@chakra-ui/theme";
-import * as components from "./components";
+// import * as components from "./components";
 import * as tokens from "./tokens";
 
 export const theme = extendTheme({
@@ -8,6 +8,6 @@ export const theme = extendTheme({
   ...tokens,
   components: {
     ...defaultTheme.components,
-    ...components,
+    // ...components,
   },
 });
