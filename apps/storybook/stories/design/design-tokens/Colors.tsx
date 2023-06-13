7b4c2fe57467ab2dbc8c91e18d9da2b76de5f8d8
eliseki <eliseki@stud.ntnu.no>
2023-06-13 10:11:13 +0200

    feat:kvibifisert color-tokens. docs:updated color docs

--- apps/storybook/stories/design/design-tokens/Colors.tsx
+++ apps/storybook/stories/design/design-tokens/Colors.tsx
@@ -3,7 +3,18 @@ import { colors } from "@kvib/react/src/theme/tokens";
 
 type ColorScaleType =
   | string
-  | { 50: string; 100: string; 200: string; 300: string; 400: string; 500: string; 600: string };
+  | {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+    };
 
 const capitalizeFirstLetter = (word: string) => word.charAt(0).toUpperCase() + word.slice(1);
 
@@ -25,13 +36,15 @@ const renderColors = (name: string, colorScale: ColorScaleType) => {
     <VStack
       key={value}
       alignItems="flex-start"
-      spacing="4px"
+      spacing="2px"
       borderRadius="6px"
       border={`1px solid ${colors.gray[100]}`}
     >
       <Box backgroundColor={hex} height="60px" width="100%"></Box>
-      <Text as="b" paddingLeft="6px">{`${name}-${value}`}</Text>
-      <Text paddingLeft="6px">{hex}</Text>
+      <Text as="b" fontSize="14px" paddingLeft="6px">{`${name}-${value}`}</Text>
+      <Text fontSize="12px" paddingLeft="6px">
+        {hex}
+      </Text>
     </VStack>
   ));
 };
@@ -42,7 +55,7 @@ export const Colors = () => {
       {Object.entries(colors).map(([name, colorScale]) => (
         <Box key={name} marginTop="20px">
           <Heading as="h3">{capitalizeFirstLetter(name)}</Heading>
-          <SimpleGrid columns={[4, 7]} spacing="20px">
+          <SimpleGrid columns={10} spacing="8px">
             {renderColors(name, colorScale)}
           </SimpleGrid>
         </Box>
