78e8ef8f3636cb308f50a318f755956a2df20b75
eliseki <eliseki@stud.ntnu.no>
2023-06-15 10:52:18 +0200

    bugfix: removed kvibifisert theme som ikke var revertet for steg 1

--- packages/react/src/theme/index.ts
+++ packages/react/src/theme/index.ts
@@ -1,13 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 import { theme as defaultTheme } from "@chakra-ui/theme";
-// import * as components from "./components";
+import * as components from "./components";
 import * as tokens from "./tokens";
 
 export const theme = extendTheme({
   ...defaultTheme,
   ...tokens,
+
   components: {
     ...defaultTheme.components,
-    // ...components,
+    ...components,
   },
 });
