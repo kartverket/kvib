f5c28ece2336b9545ea8029e5f86915a0681abb2
eliseki <eliseki@stud.ntnu.no>
2023-06-19 10:58:08 +0200

    refactor: removed unimplemented components-theme

--- packages/react/src/theme/components/index.ts
+++ packages/react/src/theme/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from "./button";
-export { default as Link } from "./link";
-export { default as Radio } from "./radio";
-export { default as Heading } from "./heading";
+//export { default as Link } from "./link";
+// export { default as Radio } from "./radio";
+// export { default as Heading } from "./heading";
