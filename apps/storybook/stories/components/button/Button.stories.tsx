e62d150bae45c436c3ad0cc850cdb7ed70935846
eliseki <eliseki@stud.ntnu.no>
2023-06-19 10:56:27 +0200

    docs: set argType for isDisabled

--- apps/storybook/stories/components/button/Button.stories.tsx
+++ apps/storybook/stories/components/button/Button.stories.tsx
@@ -10,12 +10,22 @@ const meta: Meta<typeof KvibButton> = {
       canvas: { sourceState: "shown" },
     },
   },
+  argTypes: {
+    isDisabled: {
+      description: "If true, the button will be disabled.",
+      table: {
+        type: { summary: Boolean },
+        defaultValue: { summary: false },
+      },
+      control: "boolean",
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof KvibButton>;
 
 export const Button: Story = {
-  args: { children: "Klikk her", colorScheme: "green", size: "md", variant: "solid" },
+  args: { children: "Klikk her", colorScheme: "green", size: "md", variant: "solid", isDisabled: false },
   render: (args) => <KvibButton {...args}>{args.children}</KvibButton>,
 };
