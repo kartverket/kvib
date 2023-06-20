df6f656030013f51b93e6b6420342dc72fc5f154
eliseki <eliseki@stud.ntnu.no>
2023-06-20 10:45:37 +0200

    feat: added iconButton.tsx

---
+++ packages/react/src/button/IconButton.tsx
@@ -0,0 +1,49 @@
+import { IconButton as ChakraIconButton, IconButtonProps as ChakraIconButtonProps, forwardRef } from "@chakra-ui/react";
+
+export type IconButtonProps = Omit<
+  ChakraIconButtonProps,
+  | "colorScheme"
+  | "size"
+  | "variant"
+  | "leftIcon"
+  | "rightIcon"
+  | "iconSpacing"
+  | "isActive"
+  | "loadingText"
+  | "spinnerPlacement"
+  | "icon"
+  | "spinner"
+> & {
+  /**The variant of the IconButton*/
+  variant: "ordinary" | "outlined";
+
+  /**The size of the Button*/
+  size: "sm" | "md" | "lg";
+
+  /**The visual color appearance of the component*/
+  colorScheme: "green" | "blue" | "gray" | "red";
+
+  /**The icon to be used in the button.*/
+  icon?: string;
+
+  /**A label that describes the button*/
+  "aria-label": string;
+};
+
+export const IconButton = forwardRef<IconButtonProps, "button">(
+  ({ size = "md", variant = "ordinary", colorScheme = "green", isDisabled, isLoading, ...props }, ref) => {
+    return (
+      <ChakraIconButton
+        {...props}
+        ref={ref}
+        size={size}
+        variant={variant}
+        colorScheme={colorScheme}
+        isDisabled={isDisabled || isLoading}
+        aria-busy={isLoading}
+        aria-label={"aria-label"}
+        icon={<span className="material-symbols-outlined">{props.icon}</span>}
+      ></ChakraIconButton>
+    );
+  }
+);
--- packages/react/src/button/index.tsx
+++ packages/react/src/button/index.tsx
@@ -1,3 +1,4 @@
-export { Button, ButtonGroup, IconButton } from "@chakra-ui/react";
+export { Button, ButtonGroup } from "@chakra-ui/react";
 export type { ButtonProps, ButtonGroupProps, IconButtonProps } from "@chakra-ui/react";
 // export * from "./Button";
+export * from "./IconButton";
