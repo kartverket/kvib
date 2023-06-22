0c69587c2c182fcd1a12d2279602c06db85da4e6
eliseki <eliseki@stud.ntnu.no>
2023-06-22 13:09:03 +0200

    feat: iconButton component. Defined seperate styling for Iconbutton from scratch.

--- packages/react/src/button/IconButton.tsx
+++ packages/react/src/button/IconButton.tsx
@@ -1,19 +1,9 @@
 import { IconButton as ChakraIconButton, IconButtonProps as ChakraIconButtonProps, forwardRef } from "@chakra-ui/react";
-
-import {
-  chakra,
-  omitThemingProps,
-  SystemStyleObject,
-  ThemingProps,
-  useStyleConfig,
-  HTMLChakraProps,
-} from "@chakra-ui/system";
-
-//import {ButtonProps} from "./Button";
+import { useStyleConfig } from "@chakra-ui/system";
 
 export type IconButtonProps = Omit<
   ChakraIconButtonProps,
-  "colorScheme" | "size" | "variant" | "isActive" | "icon" | "spinner"
+  "colorScheme" | "variant" | "isActive" | "icon" | "spinner"
 > & {
   /**The variant of the IconButton*/
   variant: "solid" | "outlined" | "link";
@@ -21,61 +11,20 @@ export type IconButtonProps = Omit<
   /**The icon to be used in the button.*/
   icon: string;
 
-  //   export interface IconButtonProps extends HTMLChakraProps<"button">, ThemingProps<"IconButton">{
-  //   /**The variant of the IconButton*/
-  //   variant: "solid" | "outlined" | "link";
-  //
-  //   /**The icon to be used in the button.*/
-  //   icon: string;
-  // }
-
-  // /**The size of the Button*/
-  // size: "xs"| "sm" | "md" | "lg";
-  //
-  // /**The visual color appearance of the component*/
-  // colorScheme: "green" | "blue" | "gray" | "red";
-  //
-  //
-  // /**A label that describes the button*/
-  // "aria-label": string;
+  /**The visual color appearance of the component.
+     @default green*/
+  colorScheme: "green" | "blue" | "gray" | "red";
 };
-
-// export type IconButtonProps= ButtonProps & {
-//
-//     /**The icon to be used in the button.*/
-//     icon: string;
-//     variant: "iconButton"
-// }
-
-// export const IconButton = forwardRef<IconButtonProps, "button">(
-//   ({ size = "md",  colorScheme = "green", isDisabled, isLoading, ...props }, ref) => {
-//
-//     // const styles = useMultiStyleConfig('IconButtonTheme', { variant, size })
-//     return (
-//       <ChakraIconButton
-//         {...props}
-//         ref={ref}
-//         size={size}
-//         variant = "solid"
-//         colorScheme={colorScheme}
-//         isDisabled={isDisabled || isLoading}
-//         aria-busy={isLoading}
-//         aria-label={"aria-label"}
-//         icon={<span  className="material-symbols-outlined">{props.icon}</span>}
-//         padding={"0px"}
-//         // paddingX={"12px"}
-//         // paddingY={"12px"}
-//       >
-//       </ChakraIconButton>
-//     );
-//   }
-// );
-
-//TODO:remove one of this.
 
 export const IconButton = forwardRef<IconButtonProps, "button">(
   (
-    { variant = "solid", isDisabled, isLoading, ...props },
+    {
+      isDisabled,
+      isLoading,
+      // variant="solid",
+      // colorScheme="green" ,
+      ...props
+    },
 
     ref
   ) => {
@@ -83,13 +32,14 @@ export const IconButton = forwardRef<IconButtonProps, "button">(
 
     return (
       <ChakraIconButton
-        variant={variant}
         {...props}
-        __css={styles}
         ref={ref}
+        __css={{ ...styles }}
+        // size={size}
+        // variant={variant}
+        // colorScheme={colorScheme}
         isDisabled={isDisabled || isLoading}
         aria-busy={isLoading}
-        aria-label={"aria-label"}
         icon={<span className="material-symbols-outlined">{props.icon}</span>}
       ></ChakraIconButton>
     );
--- packages/react/src/theme/components/iconButton.ts
+++ packages/react/src/theme/components/iconButton.ts
@@ -1,105 +1,245 @@
-import { colors, radii } from "../tokens";
+import { colors } from "../tokens";
 // import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
 
 import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
 
-const outline = defineStyle({
-  bg: "red",
-  border: "2px dashed", // change the appearance of the border
-  borderRadius: 0, // remove the border radius
-  fontWeight: "semibold", // change the font weight
+const variantSolid = defineStyle((props) => {
+  const { colorScheme: c } = props;
+
+  if (c === "gray") {
+    return {
+      bg: colors.gray[100],
+      color: colors.black,
+      _hover: {
+        bg: colors.gray[200],
+      },
+      _active: {
+        bg: colors.gray[300],
+      },
+    };
+  }
+  return {
+    bg: `${c}.500`,
+    color: colors.white,
+    _hover: {
+      bg: `${c}.400`,
+    },
+    _active: {
+      bg: `${c}.600`,
+    },
+  };
 });
 
-const solid = defineStyle({
-  bg: colors.green[500],
-  alignItems: "center",
-  padding: "12px",
-  border: "2px dashed",
-  borderRadius: 0,
-  fontWeight: "semibold",
+const variantOutline = defineStyle((props) => {
+  const { colorScheme: c } = props;
+
+  if (c === "gray") {
+    return {
+      bg: colors.white,
+      borderColor: colors.gray[200],
+      color: colors.black,
+      _hover: {
+        bg: colors.gray[50],
+        _disabled: {
+          bg: colors.white,
+          color: colors.black,
+        },
+      },
+      _active: {
+        bg: colors.gray[100],
+        _disabled: {
+          bg: colors.white,
+          color: colors.black,
+        },
+      },
+    };
+  }
+  return {
+    bg: colors.white,
+    borderColor: `${c}.500`,
+    color: `${c}.500`,
+    _hover: {
+      bg: `${c}.400`,
+      color: colors.white,
+      _disabled: {
+        bg: colors.white,
+        color: `${c}.500`,
+      },
+    },
+    _active: {
+      bg: `${c}.600`,
+      color: colors.white,
+      _disabled: {
+        bg: colors.white,
+        color: `${c}.500`,
+      },
+    },
+  };
+});
+
+const variantLink = defineStyle((props) => {
+  const { colorScheme: c } = props;
+
+  if (c === "gray") {
+    return {
+      color: colors.gray[800],
+      backgroundColor: "transparent",
+      _hover: {
+        textDecoration: "none",
+        color: colors.gray[500],
+        ".text": {
+          textDecoration: "Underline",
+        },
+        _disabled: {
+          ".text": {
+            textDecoration: "none",
+            color: colors.gray[800],
+          },
+        },
+      },
+      _active: {
+        color: colors.gray[900],
+        textDecoration: "none",
+        ".text": {
+          textDecoration: "Underline",
+        },
+        _disabled: {
+          color: colors.gray[800],
+        },
+      },
+      _loading: {
+        color: colors.gray[800],
+      },
+    };
+  }
+  return {
+    color: `${c}.500`,
+    backgroundColor: "transparent",
+    _hover: {
+      textDecoration: "none",
+      color: `${c}.400`,
+      ".text": {
+        textDecoration: "Underline",
+      },
+      _disabled: {
+        ".text": {
+          textDecoration: "none",
+        },
+        textDecoration: "none",
+        color: `${c}.500`,
+      },
+    },
+    _active: {
+      textDecoration: "none",
+      color: `${c}.600`,
+      ".text": {
+        textDecoration: "underline",
+      },
+      _disabled: {
+        ".text": {
+          textDecoration: "none",
+          color: `${c}.500`,
+        },
+      },
+    },
+    _loading: {
+      color: `${c}.500`,
+    },
+  };
 });
 
 export const IconButtonTheme = defineStyleConfig({
-  variants: { outline, solid },
+  baseStyle: () => ({
+    borderRadius: "6px",
+    outline: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexShrink: 0,
+    _disabled: {
+      opacity: 0.5,
+      cursor: "not-allowed",
+    },
+  }),
+  sizes: {
+    xs: {
+      fontSize: "12px",
+      padding: "6px",
+      gap: "6px",
+      width: "24px",
+      height: "24px",
+    },
+    sm: {
+      fontSize: "14px",
+      padding: "9px",
+      width: "32px",
+      height: "32px",
+    },
+    md: {
+      fontSize: "16px",
+      padding: "12px",
+      width: "40px",
+      height: "40px",
+    },
+    lg: {
+      fontSize: "18px",
+      lineHeight: "28px",
+      padding: "15px",
+      width: "48px",
+      height: "48px",
+    },
+  },
+  variants: {
+    solid: variantSolid,
+    outline: variantOutline,
+    link: variantLink,
+  },
+  defaultProps: {
+    variant: "solid",
+    size: "md",
+    colorScheme: "green",
+  },
 });
 
-// const helpers = createMultiStyleConfigHelpers(["button", "icon"]);
-// export const IconButtonTheme = helpers.defineMultiStyleConfig({
-//   baseStyle: {
-//     button: {
-//       gap: "4px",
-//       alignItems: "center",
-//       justifyContent: "center",
-//       borderRadius: radii.lg,
-//       _disabled: {
-//         opacity: 1,
-//         color: colors.gray[100],
-//         cursor: "not-allowed",
-//       },
-//     },
-//     icon: {
-//       width: "18px",
-//       height: "18px",
-//       _disabled: {
-//         opacity: 1,
-//         color: colors.gray[100],
-//         cursor: "not-allowed",
-//       },
-//     },
-//   },
-//   sizes: {
-//     sm: {
-//       button: {
-//         padding: "8px",
-//         minWidth: "40px",
-//         minHeight: "40px",
-//       },
-//     },
-//     md: {
-//       button: {
-//         padding: "12px",
-//         minWidth: "48px",
-//         minHeight: "48px",
-//       },
-//     },
-//     lg: {
-//       button: {
-//         padding: "14px",
-//         minWidth: "52px",
-//         minHeight: "52px",
-//       },
-//     },
-//   },
+// const variantOutline = defineStyle({
+//   bg: "red",
+//   border: "2px dashed", // change the appearance of the border
+//   borderRadius: 0, // remove the border radius
+//   fontWeight: "semibold", // change the font weight
+// });
+//
+// const variantSolid = defineStyle({
+//   bg: colors.green[500],
+//   alignItems: "center",
+//   padding: "12px",
+//   border: "2px dashed",
+//   borderRadius: 0,
+//   fontWeight: "semibold",
+// });
+// const variantLink = defineStyle({
+//   bg: colors.green[500],
+//   alignItems: "center",
+//   padding: "12px",
+//   border: "2px dashed",
+//   borderRadius: 0,
+//   fontWeight: "semibold",
+// });
+//
+//
+// const sizeLg= defineStyle({
+//   bg: "red",
+// });
+// const sizemD= defineStyle({
+//   bg: "red",
+// });
+//
+// export const IconButtonTheme = defineStyleConfig({
 //   variants: {
-//     ordinary: ({ colorScheme }) => ({
-//       button: {
-//         bg: `${colorScheme}.500`,
-//         _hover: {
-//           bg: `${colorScheme}.400`,
-//         },
-//         _active: {
-//           bg: `${colorScheme}.600`,
-//         },
-//       },
-//       icon: {
-//         color: colors.white,
-//       },
-//     }),
-//
-//     outlined: ({ colorScheme }) => ({
-//       button: {
-//         bg: colors.white,
-//         // TODO: sett inn borderShadow
-//       },
-//       icon: {
-//         color: `${colorScheme}.500`,
-//       },
-//     }),
+//     outline: variantOutline,
+//     solid: variantSolid,
+//     link: variantLink
 //   },
-//
 //   defaultProps: {
-//     size: "md",
-//     colorScheme: "green",
-//     variant: "ordinary",
-//   },
+//     variant: "solid"
+//   }
 // });
