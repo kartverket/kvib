17e1ebba097492ec2bdfd17b0de0d479aaeaaaa3
Sebastian Piotr Maro <marseb@kartverket.no>
2023-06-22 14:45:12 +0200

    mangler bare å vise et eller to eksempler til

--- apps/storybook/stories/design/Icon.stories.tsx
+++ apps/storybook/stories/design/Icon.stories.tsx
@@ -1,9 +1,10 @@
-import { Icon as KvibIcon } from "@kvib/react/src/icon";
+import { Icon } from "@kvib/react/src/icon";
 import { Meta, StoryObj } from "@storybook/react";
+import { Colors } from "@kvib/react/src/theme/tokens";
 
-const meta: Meta<typeof KvibIcon> = {
+const meta: Meta<typeof Icon> = {
   title: "Design Fundament/Ikoner**",
-  component: KvibIcon,
+  component: Icon,
   parameters: {
     docs: {
       story: { inline: true },
@@ -12,15 +13,37 @@ const meta: Meta<typeof KvibIcon> = {
   },
   argTypes: {
     color: {
-      control: color,
+      control: "color",
+      defaultValue: { summary: "black" },
+    },
+    weight: {
+      control: "number",
+      defaultValue: { summary: 600 },
+    },
+    grade: {
+      control: "number",
+      defaultValue: { summary: 0 },
+    },
+    size: {
+      control: "number",
+      defaultValue: { summary: 24 },
+    },
+    fill: {
+      control: "boolean",
+      defaultValue: { summary: false },
     },
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof KvibIcon>;
+type Story = StoryObj<typeof Icon>;
 
 export const defaultIcon: Story = {
   args: { icon: "warning" },
-  render: (args) => <KvibIcon {...args} />,
+  render: (args) => <Icon {...args} />,
+};
+
+export const exampleIcon: Story = {
+  args: {},
+  render: (args, colors) => <Icon icon="search" color={colors.green[400]} />,
 };
--- packages/react/src/icon/Icon.tsx
+++ packages/react/src/icon/Icon.tsx
@@ -1,16 +1,36 @@
 /**Denne komponenten displayer ikoner fra material symbols rounded
  * https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Rounded*/
+
 export const Icon = ({
   icon,
-  color,
+  size = 24,
+  color = "black",
+  weight = 600,
+  grade = 0,
+  fill = false,
 }: {
-  /**The icon from material symbols you want to display*/
+  /**The icon from Material symbols you want to display*/
   icon: string;
+  /**The font size of the icon */
+  size?: number;
   /**The color of the icon*/
   color?: string;
+  /**The weight of the icon*/
+  weight?: number;
+  /**The of grade the icon*/
+  grade?: number;
+  /**Decides whether the icon is filled or not*/
+  fill?: boolean;
 }) => {
   return (
-    <span className="material-symbols-rounded" style={{ color: color }}>
+    <span
+      className="material-symbols-rounded"
+      style={{
+        fontSize: size,
+        color: color,
+        fontVariationSettings: `'FILL' ${fill ? 1 : 0}, 'wght' ${weight ? weight : 600}, 'GRAD' ${grade ? grade : 0}`,
+      }}
+    >
       {icon}
     </span>
   );
