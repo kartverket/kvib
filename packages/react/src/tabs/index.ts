3c1c6c87a78546672ab7e7134e54b3bcd06dd6ca
Adrian Flatner <adrianrflatner@gmail.com>
2023-06-26 15:36:50 +0200

    Adding imports to tabs

--- packages/react/src/tabs/index.ts
+++ packages/react/src/tabs/index.ts
@@ -1,2 +1,3 @@
 export { Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
+export type { TabsProps, TabListProps, TabPanelsProps, TabProps, TabPanelProps } from "@chakra-ui/react";
 export * from "./Tabs";
