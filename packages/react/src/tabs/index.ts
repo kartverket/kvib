0c7e5d78938ac24b7cc0536e38b0c5f9a2885820
Adrian Flatner <adrianrflatner@gmail.com>
2023-06-26 16:24:12 +0200

    Updating imports

--- packages/react/src/tabs/index.ts
+++ packages/react/src/tabs/index.ts
@@ -1,3 +1,3 @@
 export { Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
-export type { TabsProps, TabListProps, TabPanelsProps, TabProps, TabPanelProps } from "@chakra-ui/react";
+export type { TabListProps, TabPanelsProps, TabProps, TabPanelProps } from "@chakra-ui/react";
 export * from "./Tabs";
