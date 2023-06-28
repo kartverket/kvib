d5dc0cb9e151ff43238405a7f9eb504ca07862b0
Sebastian Piotr Maro <marseb@kartverket.no>
2023-06-28 13:10:33 +0200

    Fikset eksportering

--- packages/react/src/tabs/index.ts
+++ packages/react/src/tabs/index.ts
@@ -1,3 +1,1 @@
-export { Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
-export type { TabListProps, TabPanelsProps, TabProps, TabPanelProps } from "@chakra-ui/react";
 export * from "./Tabs";
