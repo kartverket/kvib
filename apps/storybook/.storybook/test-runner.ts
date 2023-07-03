5233c6f43daec0e8d5bbe9fe85be2a03a922284b
eliseki <eliseki@stud.ntnu.no>
2023-07-03 15:25:14 +0200

    test

--- apps/storybook/.storybook/test-runner.ts
+++ apps/storybook/.storybook/test-runner.ts
@@ -1,6 +1,6 @@
-// .storybook/test-runner.ts
+import { injectAxe, checkA11y, configureAxe } from "axe-playwright";
 
-import { injectAxe, checkA11y } from "axe-playwright";
+import { getStoryContext } from "@storybook/test-runner";
 
 import type { TestRunnerConfig } from "@storybook/test-runner";
 
@@ -12,7 +12,20 @@ const a11yConfig: TestRunnerConfig = {
   async preRender(page) {
     await injectAxe(page);
   },
-  async postRender(page) {
+  async postRender(page, context) {
+    // Get the entire context of a story, including parameters, args, argTypes, etc.
+    const storyContext = await getStoryContext(page, context);
+
+    // Do not run a11y tests on disabled stories.
+    if (storyContext.parameters?.a11y?.disable) {
+      return;
+    }
+
+    // Apply story-level a11y rules
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules,
+    });
+
     await checkA11y(page, "#storybook-root", {
       detailedReport: true,
       detailedReportOptions: {
