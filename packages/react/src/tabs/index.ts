1e6fea79856d6a9a00dce496b9af4ff54254517b
eliseki <eliseki@stud.ntnu.no>
2023-07-03 14:00:21 +0200

    feat: added test runner dependecy. feat: added github workflow for testrunner

--- packages/react/src/tabs/index.ts
+++ packages/react/src/tabs/index.ts
@@ -1,1 +1,3 @@
 export * from "./Tabs";
+export { Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
+export type { TabProps, TabListProps, TabPanelProps, TabPanelsProps } from "@chakra-ui/react";
