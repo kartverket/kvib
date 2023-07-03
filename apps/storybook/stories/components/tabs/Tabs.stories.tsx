c097b7f984a6a5a0291e0a29a71c169be1636c22
eliseki <eliseki@stud.ntnu.no>
2023-07-03 15:36:16 +0200

    test to see if test runer fails

--- apps/storybook/stories/components/tabs/Tabs.stories.tsx
+++ apps/storybook/stories/components/tabs/Tabs.stories.tsx
@@ -16,10 +16,10 @@ const meta: Meta<typeof KvibTabs> = {
       story: { inline: true },
       canvas: { sourceState: "shown" },
     },
-    a11y: {
-      // This option disables all a11y checks on this story
-      disable: true,
-    },
+    // a11y: {
+    //   // This option disables all a11y checks on this story
+    //   disable: true,
+    // },
   },
 
   argTypes: {
