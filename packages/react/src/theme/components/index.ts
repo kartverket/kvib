08189283726a32c909d820c5ffae510f88275f30
eliseki <eliseki@stud.ntnu.no>
2023-07-04 08:22:07 +0200

    refactor: changes name of theme to match.

--- packages/react/src/theme/components/index.ts
+++ packages/react/src/theme/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Link } from "./link";
+export { linkTheme as Link } from "./link";
 export { buttonTheme as Button } from "./button";
-export { default as Tabs } from "./tabs";
+export { tabsTheme as Tabs } from "./tabs";
 export { IconButtonTheme as IconButton } from "./iconButton";
