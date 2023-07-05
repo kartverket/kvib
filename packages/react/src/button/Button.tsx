8b6896c55ac52535af1d2499513f7c3e0665cf21
eliseki <eliseki@stud.ntnu.no>
2023-07-05 16:19:24 +0200

    feat: added prop "iconFill" to Button. refactor: added <Icon/> to display icon in Button.

--- packages/react/src/button/Button.tsx
+++ packages/react/src/button/Button.tsx
@@ -6,6 +6,7 @@ import {
   Spinner,
   HStack,
 } from "@chakra-ui/react";
+import { Icon } from "../icon";
 
 export type ButtonProps = Omit<
   ChakraButtonProps,
@@ -19,13 +20,16 @@ export type ButtonProps = Omit<
   | "loadingText"
   | "spinnerPlacement"
 > & {
-  /**The size of the Button*/
+  /**The size of the Button
+   * @default green */
   size: "xs" | "sm" | "md" | "lg";
 
-  /**The variant of the Button*/
+  /**The variant of the Button
+   * @default solid */
   variant: "solid" | "outline" | "link";
 
-  /**The visual color appearance of the component*/
+  /**The visual color appearance of the component
+   * @default green */
   colorScheme: "green" | "blue" | "gray" | "red";
 
   /**If added, the button will show an icon before the button's label.*/
@@ -33,6 +37,10 @@ export type ButtonProps = Omit<
 
   /**If added, the button will show an icon after the button's label.*/
   rightIcon?: string;
+
+  /**If true, the rightIcon/leftIcon in the button will be filled.
+   * @default false */
+  iconFill?: boolean;
 };
 
 export const Button = forwardRef<ButtonProps, "button">(
@@ -46,6 +54,7 @@ export const Button = forwardRef<ButtonProps, "button">(
       isLoading,
       leftIcon,
       rightIcon,
+      iconFill = false,
       ...props
     },
     ref
@@ -59,6 +68,7 @@ export const Button = forwardRef<ButtonProps, "button">(
         colorScheme={colorScheme}
         isDisabled={isDisabled || isLoading}
         aria-busy={isLoading}
+        iconFill={iconFill && (rightIcon || leftIcon)}
       >
         {isLoading && (
           <Center position="absolute" right="0" left="0">
@@ -66,11 +76,11 @@ export const Button = forwardRef<ButtonProps, "button">(
           </Center>
         )}
         <HStack spacing={1} visibility={isLoading ? "hidden" : "visible"}>
-          {leftIcon && <span className="material-symbols-rounded">{leftIcon}</span>}
+          {leftIcon && <Icon icon={leftIcon} isFilled={iconFill} />}
           <Center className="text" as="span">
             {children}
           </Center>
-          {rightIcon && <span className="material-symbols-rounded">{rightIcon}</span>}
+          {rightIcon && <Icon icon={rightIcon} isFilled={iconFill} />}
         </HStack>
       </ChakraButton>
     );
