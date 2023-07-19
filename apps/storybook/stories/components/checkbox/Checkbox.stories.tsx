26b34702859401b47990043d2b050bc2bcbc5241
Sebastian Piotr Maro <marseb@kartverket.no>
2023-07-19 10:24:10 +0200

    Dockumentasjon for sjekkboks uten tekst

--- apps/storybook/stories/components/checkbox/Checkbox.stories.tsx
+++ apps/storybook/stories/components/checkbox/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
-import { Checkbox as KvibCheckbox } from "@kvib/react/src";
-import { HStack } from "@kvib/react/src/layout";
+import { Checkbox as KvibCheckbox, CheckboxGroup as KvibCheckboxGroup } from "@kvib/react/src";
+import { Stack } from "@kvib/react/src/layout";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof KvibCheckbox> = {
@@ -13,12 +13,38 @@ const meta: Meta<typeof KvibCheckbox> = {
   },
   argTypes: {
     colorScheme: {
-      description: "The color of the Checkbox.",
+      description: "The color of the Checkbox",
       table: {
         type: { summary: "green | blue" },
-        defaultValue: { summary: "green" },
+        defaultValue: { summary: "blue" },
       },
-      control: "string",
+      control: "radio",
+      options: ["green", "blue"],
+    },
+    size: {
+      description: "The size of the checkbox",
+      table: {
+        type: { summary: "sm | md | lg" },
+        defaultValue: { summary: "md" },
+      },
+      control: "radio",
+      options: ["sm", "md", "lg"],
+    },
+    isDisabled: {
+      description: "If true, the checkbox will be disabled",
+      table: {
+        type: { summary: "true | false" },
+        defaultValue: { summary: false },
+      },
+      control: "boolean",
+    },
+    iconColor: {
+      description: "The color of the checkmark",
+      table: {
+        type: { summary: "css | color variable" },
+        defaultValue: { summary: "white" },
+      },
+      control: "color",
     },
   },
 };
@@ -29,24 +55,71 @@ type Story = StoryObj<typeof KvibCheckbox>;
 export const Checkbox: Story = {
   args: {
     children: "Dette er en avmerkingsboks",
+    colorScheme: "green",
+    size: "md",
+    isDisabled: false,
   },
   render: (args) => <KvibCheckbox {...args}>{args.children}</KvibCheckbox>,
 };
 
 export const CheckboxColors: Story = {
+  args: {},
+  render: (args) => (
+    <Stack>
+      <KvibCheckbox {...args} colorScheme="blue">
+        Blå
+      </KvibCheckbox>
+      <KvibCheckbox {...args} colorScheme="green">
+        Grønn
+      </KvibCheckbox>
+    </Stack>
+  ),
+};
+
+export const CheckboxSizes: Story = {
   args: {
-    children: "Dette er en avmerkingsboks",
+    children: "Huk av",
   },
   render: (args) => (
-    <HStack>
-      <KvibCheckbox {...args} colorScheme="blue">
+    <Stack>
+      <KvibCheckbox {...args} size="sm">
         {args.children}
       </KvibCheckbox>
-      ,
-      <KvibCheckbox {...args} colorScheme="green">
+      <KvibCheckbox {...args} size="md">
         {args.children}
       </KvibCheckbox>
-      ,
-    </HStack>
+      <KvibCheckbox {...args} size="lg">
+        {args.children}
+      </KvibCheckbox>
+    </Stack>
+  ),
+};
+
+export const CheckboxStates: Story = {
+  args: {},
+  render: (args) => (
+    <Stack>
+      <KvibCheckbox {...args}>Normal</KvibCheckbox>
+      <KvibCheckbox isDisabled>Disabled</KvibCheckbox>
+      <KvibCheckbox isInvalid>Invalid</KvibCheckbox>
+    </Stack>
+  ),
+};
+
+export const CheckboxGroup: Story = {
+  args: { size: "sm" },
+  render: (args) => (
+    <Stack>
+      <p>Grønn</p>
+      <KvibCheckboxGroup colorScheme="green" {...args}>
+        <KvibCheckbox>Valg 1</KvibCheckbox>
+        <KvibCheckbox>Valg 2</KvibCheckbox>
+      </KvibCheckboxGroup>
+      <p>Blå</p>
+      <KvibCheckboxGroup colorScheme="blue" {...args}>
+        <KvibCheckbox>Valg 1</KvibCheckbox>
+        <KvibCheckbox>Valg 2</KvibCheckbox>
+      </KvibCheckboxGroup>
+    </Stack>
   ),
 };
