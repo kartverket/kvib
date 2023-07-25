38fcb6be8fbd74567004909e864108fa2ae2067c
ahmfar <farhad.ahmadhadi@kartverket.no>
2023-07-25 15:04:36 +0200

    Input Bugfix

--- packages/react/src/input/Input.tsx
+++ packages/react/src/input/Input.tsx
@@ -1,40 +1,7 @@
 import { Input as ChakraInput, InputProps as ChakraInputProps, forwardRef } from "@chakra-ui/react";
 
-export type InputProps = Omit<
-  ChakraInputProps,
-  | "colorScheme"
-  | "errorBorderColor"
-  | "focusBorderColor"
-  | "htmlSize"
-  | "isDisabled"
-  | "isInvalid"
-  | "isReadOnly"
-  | "isRequired"
-  | "size"
-  | "variant"
-> & {
-  size?: "lg" | "md" | "sm" | "xs";
-  variant?: "outline" | "filled" | "flushed" | "unstyled";
-  /**
-   * @default false
-   */
-  isRequired?: boolean;
-  /**
-   * @default false
-   */
-  isReadOnly?: boolean;
-  /**
-   * @default false
-   */
-  isInvalid?: boolean;
-  /**
-   * @default false
-   */
-  isDisabled?: boolean;
-};
-
-export const Input = forwardRef<InputProps, "input">(
-  ({ size = "md", variant, isRequired, isReadOnly, isInvalid, isDisabled, ...props }, ref) => {
+export const Input = forwardRef<ChakraInputProps, "input">(
+  ({ size, variant, isRequired, isReadOnly, isInvalid, isDisabled, ...props }, ref) => {
     return (
       <ChakraInput
         {...props}
