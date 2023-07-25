e56e8e9a9b81c1f554af5c7e5d4a54339d9389d2
ahmfar <farhad.ahmadhadi@kartverket.no>
2023-07-25 09:37:25 +0200

    Enabled InputGroup, Addon, Elements.

--- packages/react/src/input/index.ts
+++ packages/react/src/input/index.ts
@@ -1,1 +1,17 @@
 export * from "./Input";
+export {
+  InputGroup,
+  InputAddon,
+  InputLeftAddon,
+  InputRightAddon,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
+export type {
+  InputGroupProps,
+  InputAddonProps,
+  InputLeftAddonProps,
+  InputRightAddonProps,
+  InputLeftElementProps,
+  InputRightElementProps,
+} from "@chakra-ui/react";
