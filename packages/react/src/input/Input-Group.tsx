0a448a8a79c8691757b5ca40e5c4e71e3e08818c
ahmfar <farhad.ahmadhadi@kartverket.no>
2023-07-25 13:12:17 +0200

    Changed InputGroup, ele, addon export. Removed from Storybook.

--- packages/react/src/input/Input-Group.tsx
+++
@@ -1,5 +0,0 @@
-import { InputGroup as ChakraInputGroup, InputGroupProps as ChakraInputGroupProps, forwardRef } from "@chakra-ui/react";
-
-export const InputGroup = forwardRef<ChakraInputGroupProps, "div">(({ ...props }, ref) => {
-  return <ChakraInputGroup {...props} ref={ref} />;
-});
--- packages/react/src/input/Input-Left-Addon.tsx
+++
@@ -1,9 +0,0 @@
-import {
-  InputLeftAddon as ChakraInputLeftAddon,
-  InputLeftAddonProps as ChakraInputLeftAddonProps,
-  forwardRef,
-} from "@chakra-ui/react";
-
-export const InputLeftAddon = forwardRef<ChakraInputLeftAddonProps, "input">(({ children, ...props }, ref) => {
-  return <ChakraInputLeftAddon {...props} ref={ref} children={children} />;
-});
