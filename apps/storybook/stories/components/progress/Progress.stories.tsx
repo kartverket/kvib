48f4cdfd177e4dec1b013ecf30a645b935c4ce18
Adrian Flatner <adrianrflatner@gmail.com>
2023-08-14 15:44:43 +0200

    Legger til dokumentasjon for Progress

---
+++ apps/storybook/stories/components/progress/Progress.stories.tsx
@@ -0,0 +1,135 @@
+import { Progress as KvibProgress, Stack as KvibStack } from "@kvib/react/src";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof KvibProgress> = {
+  title: "Komponenter/Progress",
+  component: KvibProgress,
+  parameters: {
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: "shown" },
+    },
+  },
+  argTypes: {
+    colorScheme: {
+      description: "The visual color appearance of the component",
+      table: {
+        type: {
+          summary: '"gray" | "red" | "green" | "blue"',
+        },
+        defaultValue: { summary: "blue" },
+      },
+      options: ["gray", "red", "yellow", "green", "blue"],
+      control: {
+        type: "radio",
+      },
+    },
+
+    hasStripe: {
+      description: "If true, the progress bar will show stripe",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: false },
+      },
+      control: "boolean",
+    },
+
+    isAnimated: {
+      description: "If true, and hasStripe is true, the stripes will be animated",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: false },
+      },
+      control: "boolean",
+    },
+
+    isIndeterminate: {
+      description: "If true, the progress will be indeterminate and the value prop will be ignored",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: false },
+      },
+      control: "boolean",
+    },
+
+    max: {
+      description: "The maximum value of the progress",
+      table: {
+        type: { summary: "number" },
+        defaultValue: { summary: 100 },
+      },
+      control: "number",
+    },
+
+    min: {
+      description: "The minimum value of the progress",
+      table: {
+        type: { summary: "number" },
+        defaultValue: { summary: 0 },
+      },
+      control: "number",
+    },
+
+    size: {
+      description: "The size of the Progress",
+      table: {
+        type: { summary: '"xs" | "sm" | "md" | "lg"' },
+        defaultValue: { summary: "md" },
+      },
+      options: ["xs", "sm", "md", "lg"],
+      control: {
+        type: "radio",
+      },
+    },
+
+    value: {
+      description: "The value of the progress indicator. If undefined the progress bar will be in indeterminate state",
+      table: {
+        type: { summary: "number" },
+      },
+      control: "number",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof KvibProgress>;
+
+export const Progress: Story = {
+  args: { value: 80 },
+  render: (args) => <KvibProgress {...args} />,
+};
+
+export const ProgressStripe: Story = {
+  args: { value: 60, hasStripe: true },
+  render: (args) => <KvibProgress {...args} />,
+};
+
+export const ProgressSizes: Story = {
+  args: { value: 20, colorScheme: "green" },
+  render: (args) => (
+    <KvibStack spacing={5}>
+      <KvibProgress {...args} size="sm" />
+      <KvibProgress {...args} size="md" />
+      <KvibProgress {...args} size="lg" />
+      <KvibProgress {...args} height="32px" />
+    </KvibStack>
+  ),
+};
+
+export const ProgressColors: Story = {
+  args: { value: 40, size: "sm" },
+  render: (args) => (
+    <KvibStack spacing={5}>
+      <KvibProgress {...args} colorScheme="green" />
+      <KvibProgress {...args} colorScheme="blue" />
+      <KvibProgress {...args} colorScheme="red" />
+      <KvibProgress {...args} colorScheme="gray" />
+    </KvibStack>
+  ),
+};
+
+export const ProgressAnimated: Story = {
+  args: { isIndeterminate: true, size: "xs" },
+  render: (args) => <KvibProgress {...args} />,
+};
