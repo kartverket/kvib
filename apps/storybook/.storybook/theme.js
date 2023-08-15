24d41992a3113196e6b840a368215589a45f387d
ahmfar <farhad.ahmadhadi@kartverket.no>
2023-08-15 12:12:34 +0200

    Oppdatert theme.js. Logo URL.

--- apps/storybook/.storybook/theme.js
+++ apps/storybook/.storybook/theme.js
@@ -4,7 +4,7 @@ import logo from "./kvib_logo.svg";
 export default create({
   base: "light",
   brandTitle: "KVIB",
-  brandUrl: "https://kartverket.github.io/kvib/designsystem",
+  brandUrl: "https://kartverket.github.io/kvib/storybook",
   brandImage: logo,
   brandTarget: "_self",
 });
