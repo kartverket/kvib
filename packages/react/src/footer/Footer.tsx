3c3ccc49a299d4529e25385eed6a004cf3ac7fd1
Magnus Ramm <magnus.ramm@gmail.com>
2023-08-21 14:30:44 +0200

    KOMP-230: Legg til aria-labels

--- packages/react/src/footer/Footer.tsx
+++ packages/react/src/footer/Footer.tsx
@@ -44,19 +44,24 @@ export const Footer = ({
                 </Heading>
                 <Text>
                   Telefon:{" "}
-                  <Link fontWeight="bold" href="tel:32118000">
+                  <Link fontWeight="bold" aria-label="Ring Kartverket på telefon" href="tel:32118000">
                     {" "}
                     32 11 80 00{" "}
                   </Link>
                 </Text>
                 <Text>
                   E-post:{" "}
-                  <Link fontWeight="bold" href="mailto:post@kartverket.no">
+                  <Link fontWeight="bold" aria-label="Send e-post til Kartverket" href="mailto:post@kartverket.no">
                     {" "}
                     post@kartverket.no{" "}
                   </Link>
                 </Text>
-                <Link p={0} fontWeight="bold" href="https://kartverket.no/om-kartverket/kontakt-oss">
+                <Link
+                  p={0}
+                  fontWeight="bold"
+                  aria-label="Besøk Kartverket sin kontaktinformasjon"
+                  href="https://kartverket.no/om-kartverket/kontakt-oss"
+                >
                   {" "}
                   Kontaktinfo og adresser{" "}
                 </Link>
@@ -69,34 +74,62 @@ export const Footer = ({
             </Flex>
             <FooterToggleableFlex hidden={excludeHelp}>
               <Heading size="md"> Hjelp </Heading>
-              <Link href="http://status.kartverket.no/" fontWeight="bold">
+              <Link
+                href="http://status.kartverket.no/"
+                aria-label="Besøk Kartverket sine driftsmeldinger"
+                fontWeight="bold"
+              >
                 {" "}
                 Driftsmeldinger{" "}
               </Link>
-              <Link href="https://kartverket.no/om-kartverket/faq" fontWeight="bold">
+              <Link
+                href="https://kartverket.no/om-kartverket/faq"
+                aria-label="Besøk Kartverket sin oversikt over ofte spurte spørsmål"
+                fontWeight="bold"
+              >
                 {" "}
                 Ofte stilte spørsmål{" "}
               </Link>
-              <Link href="https://kartverket.no/eiendom/skjema/alleskjema" fontWeight="bold">
+              <Link
+                href="https://kartverket.no/eiendom/skjema/alleskjema"
+                aria-label="Besøk Kartverket sin oversikt over skjemaer"
+                fontWeight="bold"
+              >
                 {" "}
                 Skjemaer{" "}
               </Link>
             </FooterToggleableFlex>
             <FooterToggleableFlex hidden={excludeNews}>
               <Heading size="md"> Aktuelt </Heading>
-              <Link href="https://kartverket.no/om-kartverket/arrangement" fontWeight="bold">
+              <Link
+                href="https://kartverket.no/om-kartverket/arrangement"
+                aria-label="Besøk Kartverket sin oversikt over Arrangementer og kurs"
+                fontWeight="bold"
+              >
                 {" "}
                 Arrangementer og kurs{" "}
               </Link>
-              <Link href="https://kartverket.no/om-kartverket/nyheter" fontWeight="bold">
+              <Link
+                href="https://kartverket.no/om-kartverket/nyheter"
+                aria-label="Besøk Kartverket sine Nyheter"
+                fontWeight="bold"
+              >
                 {" "}
                 Nyheter{" "}
               </Link>
-              <Link href="https://kartverket.no/om-kartverket/jobb-i-kartverket" fontWeight="bold">
+              <Link
+                href="https://kartverket.no/om-kartverket/jobb-i-kartverket"
+                aria-label="Besøk Kartverket sin side med informasjon for jobb i Kartverket"
+                fontWeight="bold"
+              >
                 {" "}
                 Ledige stillinger{" "}
               </Link>
-              <Link href="https://kartverket.no/om-kartverket/presse" fontWeight="bold">
+              <Link
+                href="https://kartverket.no/om-kartverket/presse"
+                aria-label="Besøk Kartverket sin presse-side"
+                fontWeight="bold"
+              >
                 {" "}
                 Presse{" "}
               </Link>
@@ -110,32 +143,60 @@ export const Footer = ({
           ) : (
             <FooterToggleableFlex align="center" gap={3} flexWrap="wrap" flexDir="row">
               <Heading size="md"> Følg oss: </Heading>
-              <Link href="https://www.facebook.com/kartverket.no" fontWeight="bold">
+              <Link
+                href="https://www.facebook.com/kartverket.no"
+                aria-label="Besøk Kartverket sin Facebook-side"
+                fontWeight="bold"
+              >
                 Facebook
               </Link>
-              <Link href="https://twitter.com/Kartverket" fontWeight="bold">
+              <Link
+                href="https://twitter.com/Kartverket"
+                aria-label="Besøk Kartverket sin Twitter-side"
+                fontWeight="bold"
+              >
                 Twitter
               </Link>
-              <Link href="https://www.instagram.com/kartverket.no/" fontWeight="bold">
+              <Link
+                href="https://www.instagram.com/kartverket.no/"
+                aria-label="Besøk Kartverket sin Instagram-side"
+                fontWeight="bold"
+              >
                 Instagram
               </Link>
-              <Link href="https://www.linkedin.com/company/65713" fontWeight="bold">
+              <Link
+                href="https://www.linkedin.com/company/65713"
+                aria-label="Besøk Kartverket sin LinkedIn-side"
+                fontWeight="bold"
+              >
                 LinkedIn
               </Link>
-              <Link href="https://pub.dialogapi.no/s/3403d74a-8e34-4ab5-a221-1bf58c425982" fontWeight="bold">
+              <Link
+                href="https://pub.dialogapi.no/s/3403d74a-8e34-4ab5-a221-1bf58c425982"
+                aria-label="Besøk Kartverket sin side for å melde seg på nyhetsvarsler"
+                fontWeight="bold"
+              >
                 Newsletter
               </Link>
-              <Link href="https://vimeo.com/kartverket/videos" fontWeight="bold">
+              <Link
+                href="https://vimeo.com/kartverket/videos"
+                aria-label="Besøk Kartverket sin Video-side"
+                fontWeight="bold"
+              >
                 Video
               </Link>
             </FooterToggleableFlex>
           )}
           <Flex align="center" gap={3} flexWrap="wrap">
-            <Link href="https://kartverket.no/om-kartverket/personvern" fontWeight="bold">
+            <Link
+              href="https://kartverket.no/om-kartverket/personvern"
+              aria-label="Besøk Kartverket sin personvernserklæring"
+              fontWeight="bold"
+            >
               Personvern
             </Link>
             {accessibilityUrl && (
-              <Link href={accessibilityUrl} fontWeight="bold">
+              <Link href={accessibilityUrl} aria-label="Besøk denne sidens tilgjengelighetserklæring" fontWeight="bold">
                 {" "}
                 Tilgjengelighetserklæring
               </Link>
