e5cfaed6a46f8341dcc46ffb3cd64974370c8741
Sebastian Piotr Maro <marseb@kartverket.no>
2023-09-04 14:01:57 +0200

    fikset search og log på samme linje uten bug ved zoom

--- packages/react/src/header/Header.tsx
+++ packages/react/src/header/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Logo, SearchAsync } from "@kvib/react/src";
+import { Box, Icon, Logo, SearchAsync, Flex } from "@kvib/react/src";
 import { GroupBase, OptionsOrGroups } from "chakra-react-select";
 
 export type HeaderProps<T> = {
@@ -9,22 +9,28 @@ export type HeaderProps<T> = {
 
 export const Header = <T extends unknown>({ isSearch, loadOptions, onChange }: HeaderProps<T>) => {
   return (
-    <Box
+    <Flex
       bg={"white"}
       borderBottomWidth={"2px"}
       borderBottomColor={"gray.200"}
       py={"29px"}
       paddingLeft={"70px"}
-      display={"flex"}
       alignItems={"center"}
     >
-      <Logo variant={"horizontal"} size={170} />
+      <Box>
+        <Logo variant={"horizontal"} size={170} />
+      </Box>
 
       {loadOptions !== undefined && onChange !== undefined && isSearch ? (
-        <Box px={"90px"} w={500}>
-          <SearchAsync loadOptions={loadOptions} onChange={onChange} size={"lg"} />
+        <Box px={"90px"} w={700} paddingTop="10px">
+          <SearchAsync
+            loadOptions={loadOptions}
+            onChange={onChange}
+            size={"lg"}
+            dropdownIndicator={<Icon icon="search" weight={400} />}
+          />
         </Box>
       ) : null}
-    </Box>
+    </Flex>
   );
 };
