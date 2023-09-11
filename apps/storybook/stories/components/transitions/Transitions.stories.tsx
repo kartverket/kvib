61ed769218d4c7d921fc727de8cc06a97ff1b999
Sebastian Piotr Maro <marseb@kartverket.no>
2023-09-11 07:56:59 +0200

    la til flere stories

--- apps/storybook/stories/components/transitions/Transitions.stories.tsx
+++ apps/storybook/stories/components/transitions/Transitions.stories.tsx
@@ -14,9 +14,10 @@ import {
   SlideFadeProps,
   CollapseProps,
 } from "@kvib/react/src";
+import { Stack } from "@kvib/react/src";
 
 const meta: Meta<typeof KvibFade> = {
-  title: "komponenter/transitions/Fade",
+  title: "Komponenter/Transitions",
   component: KvibFade,
   parameters: {
     docs: {
@@ -29,27 +30,87 @@ const meta: Meta<typeof KvibFade> = {
 export default meta;
 type Story = StoryObj<typeof KvibFade>;
 
-const Fade = ({ ...args }: FadeProps) => {
+const FadeEx = ({ ...args }: FadeProps) => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <>
-      <Button onClick={onToggle}>Trykk for transition</Button>
+    <div>
+      <Button onClick={onToggle}>Trykk for Fade</Button>
       <KvibFade in={isOpen} {...args}>
         <Box color={"green"} w={150} h={30} bg={"pink"}>
           Tittei
         </Box>
       </KvibFade>
-    </>
+    </div>
   );
 };
-export const FadeEx: Story = {
-  args: {},
-  render: (args) => <Fade {...args}></Fade>,
-};
 
-const ScaleFade = ({ ...args }: ScaleFadeProps) => {
+export const ScaleFadeEx = ({ ...args }: ScaleFadeProps) => {
   const { isOpen, onToggle } = useDisclosure();
 
-  return;
+  return (
+    <div>
+      <Button onClick={onToggle}>Trykk for ScaleFade!</Button>
+      <KvibScaleFade initialScale={0.9} in={isOpen} {...args}>
+        <Box p="40px" color="green" mt="4" bg={"teal.500g"} rounded="md" shadow="md">
+          Fade
+        </Box>
+      </KvibScaleFade>
+    </div>
+  );
+};
+
+export const SlideEx = ({ ...args }: SlideProps) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <div>
+      <Button onClick={onToggle}>Trykk for Slide!</Button>
+      <KvibSlide direction="bottom" in={isOpen} style={{ zIndex: 10 }} {...args}>
+        <Box p="40px" color="white" mt="4" bg="teal.500" rounded="md" shadow="md">
+          Fade
+        </Box>
+      </KvibSlide>
+    </div>
+  );
+};
+
+export const SlideFadeEx = ({ ...args }: SlideFadeProps) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <div>
+      <Button onClick={onToggle}>Trykk for Slide!</Button>
+      <KvibSlideFade in={isOpen} offsetY="20px" {...args}>
+        <Box p="40px" color="white" mt="4" bg="teal.500" rounded="md" shadow="md">
+          Fade
+        </Box>
+      </KvibSlideFade>
+    </div>
+  );
+};
+
+export const CollapseEx = ({ ...args }: CollapseProps) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <div>
+      <Button onClick={onToggle}>Trykk for Slide!</Button>
+      <KvibSlide direction="bottom" in={isOpen} style={{ zIndex: 10 }} {...args}>
+        <Box p="40px" color="white" mt="4" bg="teal.500" rounded="md" shadow="md">
+          Fade
+        </Box>
+      </KvibSlide>
+    </div>
+  );
+};
+
+export const Transitions: Story = {
+  render: () => (
+    <Stack>
+      <FadeEx></FadeEx>,<ScaleFadeEx></ScaleFadeEx>
+      <SlideEx></SlideEx>
+      <SlideFadeEx></SlideFadeEx>
+    </Stack>
+  ),
 };
