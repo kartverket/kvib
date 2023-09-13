7fbb01fab5b0cd63cc602b8e2cba7a809e758f2c
Adrian Flatner <adrianrflatner@gmail.com>
2023-09-13 10:46:15 +0200

    Legger til navn til frame

--- apps/storybook/stories/components/Layout/AspectRatio/AspectRatio.stories.tsx
+++ apps/storybook/stories/components/Layout/AspectRatio/AspectRatio.stories.tsx
@@ -38,7 +38,10 @@ export const AspectRatioMap: Story = {
   args: { ratio: 16 / 9 },
   render: (args) => (
     <KvibAspectRatio {...args}>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15878.224555781148!2d10.234854769824661!3d60.16785279531992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4641075a4a064e7d%3A0x32d7e65bc9c0d372!2zSMO4bmVmb3Nz!5e0!3m2!1sno!2sno!4v1694593115187!5m2!1sno!2sno"></iframe>
+      <iframe
+        title="hønefoss kart"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15878.224555781148!2d10.234854769824661!3d60.16785279531992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4641075a4a064e7d%3A0x32d7e65bc9c0d372!2zSMO4bmVmb3Nz!5e0!3m2!1sno!2sno!4v1694593115187!5m2!1sno!2sno"
+      ></iframe>
     </KvibAspectRatio>
   ),
 };
