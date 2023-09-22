122139c72b7c5fbb3f092a60ed36fabd568e8937
Sebastian Piotr Maro <marseb@kartverket.no>
2023-09-22 08:40:36 +0200

    Ferdig med blur

--- apps/storybook/stories/design/design-tokens/Blurs.tsx
+++ apps/storybook/stories/design/design-tokens/Blurs.tsx
@@ -1,17 +1,39 @@
-import { Box, Text } from "@kvib/react/src";
+import { Box, Logo, TableContainer, Table, Thead, Tr, Th, Tbody, Td, Code, Center } from "@kvib/react/src";
+import { blur } from "@kvib/react/src/theme/tokens";
 
-// const example
+export const example = (value) => {
+  return (
+    <Box filter="auto" blur={value} w="80px">
+      <Logo size={80} />
+    </Box>
+  );
+};
+
 export const Blurs = () => {
   return (
-    <Box backdropContrast="1" w="400px" h="300px">
-      <Box
-        blur="2px"
-        w="400px"
-        h="300px"
-        background={"url(https://thispersondoesnotexist.com/) center/cover no-repeat"}
-      >
-        <Text color="white">Halla jeg legger flere ord slik at det blir lettere å se setningen</Text>
-      </Box>
-    </Box>
+    <TableContainer>
+      <Table variant="simple" width="100%">
+        <Thead textAlign="left">
+          <Tr>
+            <Th width="20%">Eksempel</Th>
+            <Th width="50%">Verdi</Th>
+            <Th width="30%">Kode</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {Object.entries(blur).map(([size, value]) => (
+            <Tr key={size}>
+              <Td backgroundColor="#F7FAFC">
+                <Center>{example(value)}</Center>
+              </Td>
+              <Td backgroundColor="white">{`${size} / ${value}`}</Td>
+              <Td backgroundColor="white">
+                <Code>{`var(--kvib-blur-${size})`}</Code>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 };
