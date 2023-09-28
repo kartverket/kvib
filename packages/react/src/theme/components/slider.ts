9532577a2c439cfd43cab3e09fa32251fd8b69de
Sebastian Piotr Maro <marseb@kartverket.no>
2023-09-28 12:29:27 +0200

    glemte å fjerne kommentert ut kode

--- packages/react/src/theme/components/slider.ts
+++ packages/react/src/theme/components/slider.ts
@@ -3,12 +3,11 @@ import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
 const baseStyle = defineStyle((props) => {
   return {
     track: {
-      bg: "gray.400", // Border color when unchecked
+      bg: "gray.400",
     },
     thumb: {
-      // borderColor: "pink.500", // Border color of the thumb
       borderWidth: "2px",
-      borderColor: `${props.colorScheme}.500`, // Border color of the thumb
+      borderColor: `${props.colorScheme}.500`,
     },
   };
 });
