0e913b3e040f18eeac8821c29ddfcb74ad71e559
Adrian Flatner <adrianrflatner@gmail.com>
2023-10-11 13:36:09 +0200

    docsmal new version

--- apps/storybook/stories/templates/DocsHeading.tsx
+++ apps/storybook/stories/templates/DocsHeading.tsx
@@ -8,15 +8,15 @@ export const DocsHeading = ({ children }: DocsHeadingProps) => {
   return (
     <Box
       backgroundColor="gray.900"
-      height="5rem"
-      borderRadius="2xl"
-      paddingLeft="3rem"
+      height="3rem"
+      borderRadius="none"
+      paddingLeft="2rem"
       alignItems="center"
       display="flex"
       marginBottom="2rem"
       marginTop="4rem"
     >
-      <Heading margin="0" color="white" size="md" as="h3">
+      <Heading margin="0" color="white" size="sm" as="h3">
         {children}
       </Heading>
     </Box>
