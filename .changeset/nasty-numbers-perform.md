690def8f5863df5a56e566ddadb954ef9aa37d60
Adrian Røstgård Flatner <42931448+adrianflatner@users.noreply.github.com>
2023-10-16 10:17:59 +0200

    Komp-288-tilgjengelighetserklæring (#560)
    * Legger til footerInline
    
    * Legger til footer på alle mdx filer
    
    * Legger til dokumentasjon
    
    * Legger til changeset

---
+++ .changeset/nasty-numbers-perform.md
@@ -0,0 +1,5 @@
+---
+"@kvib/react": minor
+---
+
+Nytt komponent: FooterInline - Footer til bunnen av en nettside som ikke skal ta hele bredden.
--- apps/storybook/.storybook/MDXContainer.jsx
+++ apps/storybook/.storybook/MDXContainer.jsx
@@ -1,12 +1,15 @@
 // .storybook/MDXWrapper.tsx
 import React from "react";
 import { KvibProvider } from "@kvib/react/src";
-import { DocsContainer } from "@storybook/blocks";
+import { DocsContainer, Story } from "@storybook/blocks";
 
 const MDXContainer = ({ children, ...props }) => {
   return (
     <KvibProvider>
-      <DocsContainer {...props}>{children}</DocsContainer>
+      <DocsContainer {...props}>
+        {children}
+        <Story id="komponenter-footer-footer-inline--footer-inline-custom" />
+      </DocsContainer>
     </KvibProvider>
   );
 };
