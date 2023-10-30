b9779cf9697a118cc64ec625a02322e62b62c25b
Adrian Røstgård Flatner <42931448+adrianflatner@users.noreply.github.com>
2023-10-30 13:20:36 +0100

    komp-337-logo (#595)
    * Endrer brandUrl til å passe nytt domene
    
    * Fjerner ubrukt kode

--- apps/storybook/.storybook/theme.js
+++ apps/storybook/.storybook/theme.js
@@ -2,15 +2,13 @@ import { create } from "@storybook/theming";
 import logo from "./kvib_logo.svg";
 import "@fontsource-variable/mulish";
 
-const isLocalhost = window.location.hostname === "localhost";
-
 export default create({
   base: "light",
   // Typography
   fontBase: '"Mulish Variable", "Open Sans", sans-serif',
   fontCode: "monospace",
   brandTitle: "Kartverkets Designsystem",
-  brandUrl: isLocalhost ? "/" : "/kvib",
+  brandUrl: "/",
   brandImage: logo,
   brandTarget: "_self",
 
