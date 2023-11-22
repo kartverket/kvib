757e9f847a6e1a673d387b86f54459abfd4a17ca
Sebastian Piotr Maro <marseb@kartverket.no>
2023-11-22 13:12:50 +0100

    gjorde litt boilerplate greier

--- apps/storybook/stories/components/spinner/Spinner.stories.tsx
+++ apps/storybook/stories/components/spinner/Spinner.stories.tsx
@@ -7,7 +7,7 @@ const meta: Meta<typeof KvibSpinner> = {
   parameters: {
     docs: {
       story: { inline: true },
-      canvas: { sourceState: "shown" },
+      canvas: { sourceState: "hidden" },
     },
   },
   argTypes: {
