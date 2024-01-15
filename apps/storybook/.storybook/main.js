6b70f745435d1bb1e3644c54dd8cc5e90c7c2fc5
Bente Lien Nilsen <bente.lien.nilsen@kartverket.no>
2024-01-15 10:07:13 +0100

    Lagt til tester med playFunction for datepicker

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     "@storybook/addon-a11y",
     {
       name: "@storybook/addon-storysource",
