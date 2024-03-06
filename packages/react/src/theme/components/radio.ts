4a18ec6fbdfe637594aeca32ed6df880b087724f
Farhad Ahmadhadi <Farhad.Ahmadhadi@kartverket.no>
2024-03-06 12:38:31 +0100

    Changed Radio and Checkbox theme

--- packages/react/src/theme/components/radio.ts
+++ packages/react/src/theme/components/radio.ts
@@ -7,9 +7,19 @@ const baseStyle = defineStyle((props) => {
   return {
     control: {
       borderColor: `${c}.500`, // Border color when unchecked
+      _checked: {
+        _hover: {
+          borderColor: `${c}.400`,
+          backgroundColor: `${c}.400`,
+        },
+      },
       _invalid: {
         _checked: {
           backgroundColor: colors.red[500],
+          _hover: {
+            borderColor: colors.red[400],
+            backgroundColor: colors.red[400],
+          },
         },
       },
     },
@@ -17,5 +27,4 @@ const baseStyle = defineStyle((props) => {
 });
 export const radioTheme = defineStyleConfig({
   baseStyle,
-  variants: {},
 });
