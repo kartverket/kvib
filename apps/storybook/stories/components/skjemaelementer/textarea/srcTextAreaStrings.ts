0499d8e980fac711be84e58683c09142dddcac79
Farhad Ahmadhadi <farhad@Farhad-sin-Mac-mini.local>
2024-03-10 14:55:36 +0100

    updated Textarea docs.

--- apps/storybook/stories/components/skjemaelementer/textarea/srcTextAreaStrings.ts
+++ apps/storybook/stories/components/skjemaelementer/textarea/srcTextAreaStrings.ts
@@ -18,3 +18,22 @@ export const TextAreaStrings = `const TextAreaControlledExample = () => {
     
     };
 `;
+
+export const TextAreaResizeStrings = `const TextareaResizeExample = () => {
+    const [resize, setResize] = useState("horizontal");
+  
+    return (
+      <>
+        <RadioGroup defaultValue={resize} onChange={setResize} mb={6}>
+          <Stack direction="row" spacing={5}>
+            <Radio value="horizontal">Horizontal</Radio>
+            <Radio value="vertical">Vertical</Radio>
+            <Radio value="none">None</Radio>
+          </Stack>
+        </RadioGroup>
+        
+        <KvibTextarea placeholder="Here is a sample placeholder" size="sm" resize={resize} />
+      </>
+    );
+  };
+`;
