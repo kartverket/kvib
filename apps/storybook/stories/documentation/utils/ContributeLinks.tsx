f4c387f0c8d5bf71b804d9d2e4662e0763960f37
SebastianPMaro <Sebastian.maro@kartverket.no>
2024-08-09 10:33:31 +0200

    Fikset på lenkene i ContributeLinks.tsx fordi de alltid har vært tomme

--- apps/storybook/stories/documentation/utils/ContributeLinks.tsx
+++ apps/storybook/stories/documentation/utils/ContributeLinks.tsx
@@ -1,33 +1,17 @@
 import { Link, Flex, Icon } from "@kvib/react/src";
 
-// export const ContributeButtons = () => {
-//   return (
-//     <Flex gap="18px">
-//       <Button variant="tertiary" size="lg" leftIcon="mail">
-//         Send en mail
-//       </Button>
-//       <Button variant="tertiary" size="lg" leftIcon="tag">
-//         Bli med i slack-kanalen
-//       </Button>
-//       <Button variant="tertiary" size="lg" leftIcon="polyline">
-//         Lag en pull-request på github
-//       </Button>
-//     </Flex>
-//   );
-// };
-
 export const ContributeLinks = () => {
   return (
     <Flex gap="18px">
-      <Link size="lg">
+      <Link href="mailto:kvib-feedbacks-aaaahwh252gnftmofucpaa47ca@kartverketgroup.slack.com">
         <Icon icon="mail" />
         Send en mail
       </Link>
-      <Link size="lg">
+      <Link href="https://slack.com/app_redirect?channel=gen-designsystem">
         <Icon icon="tag" />
         Bli med i slack-kanalen
       </Link>
-      <Link size="lg">
+      <Link href="https://github.com/kartverket/kvib">
         <Icon icon="polyline" />
         Lag en pull-request på github
       </Link>
