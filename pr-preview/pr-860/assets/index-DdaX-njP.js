3265e3342dd17800a52faebf56ee7945076a4f61
chrhein <chrhein@users.noreply.github.com>
2024-10-01 12:34:45 +0000

    Remove preview for PR 860 🛬

--- pr-preview/pr-860/assets/index-DdaX-njP.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-BwDkhjyp.js";function n(t,a=[]){const r=e.useRef(t);return e.useEffect(()=>{r.current=t}),e.useCallback((...c)=>{var u;return(u=r.current)==null?void 0:u.call(r,...c)},a)}export{n as u};
--- pr-preview/pr-860/assets/index-DjrpCY14.js
+++
@@ -1,1 +0,0 @@
-import{r as v}from"./index-BwDkhjyp.js";import{u as a}from"./index-DdaX-njP.js";function P(c){const{value:s,defaultValue:p,onChange:d,shouldUpdate:f=(t,u)=>t!==u}=c,r=a(d),l=a(f),[i,h]=v.useState(p),e=s!==void 0,o=e?s:i,m=a(t=>{const n=typeof t=="function"?t(o):t;l(o,n)&&(e||h(n),r(n))},[e,r,o,l]);return[o,m]}export{P as u};
