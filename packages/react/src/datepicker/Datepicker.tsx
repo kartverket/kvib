14004b50c53d5105ccf9c2572a8c4956b41eb5b8
Christian Hein <scheiehein@gmail.com>
2024-10-09 14:49:03 +0200

    Oppdaterer Datepicker til samme styling som før

--- packages/react/src/datepicker/Datepicker.tsx
+++ packages/react/src/datepicker/Datepicker.tsx
@@ -12,10 +12,11 @@ import {
   useBoolean,
   useTheme,
 } from "@kvib/react/src";
-import { format, isValid, parse } from "date-fns";
-import { ChangeEvent, useEffect, useId, useState } from "react";
+import { isValid, parse } from "date-fns";
+import { nb } from "date-fns/locale/nb";
+import { ChangeEvent, useEffect, useState } from "react";
 import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
+import "react-day-picker/style.css";
 
 export type DatepickerProps = Omit<InputProps, "colorScheme" | "max" | "min" | "defaultValue"> & {
   /**
@@ -124,7 +125,7 @@ const CustomDatepicker = forwardRef<DatepickerProps, "input">(
       defaultMonth,
       fromDate,
       toDate,
-      showDropdownMonthYear = true,
+      showDropdownMonthYear,
       disableNavigation,
       showOutsideDays,
       showWeekNumber,
@@ -159,10 +160,9 @@ const CustomDatepicker = forwardRef<DatepickerProps, "input">(
 
     // State for the day picker
     const [isPickerVisible, setPickerVisible] = useBoolean(false);
-    const inputId = useId();
-    const [month, setMonth] = useState(new Date());
-    const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
-    const [inputValue, setInputValue] = useState("");
+    const [selectedDate, setSelectedDate] = useState(defaultSelected ?? undefined);
+    const [month, setMonth] = useState(defaultMonth);
+    const [inputValue, setInputValue] = useState(selectedDate ? formatDate(selectedDate) : "");
 
     useEffect(() => {
       // Check if the selected date in the day picker has changed
@@ -182,30 +182,6 @@ const CustomDatepicker = forwardRef<DatepickerProps, "input">(
       }
     }, [selectedDate, setPickerVisible]);
 
-    const handleDayPickerSelect = (date: Date | undefined) => {
-      if (!date) {
-        setInputValue("");
-        setSelectedDate(undefined);
-      } else {
-        setSelectedDate(date);
-        setMonth(date);
-        setInputValue(format(date, "MM/dd/yyyy"));
-      }
-    };
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputValue(e.target.value); // keep the input value in sync
-
-      const parsedDate = parse(e.target.value, "MM/dd/yyyy", new Date());
-
-      if (isValid(parsedDate)) {
-        setSelectedDate(parsedDate);
-        setMonth(parsedDate);
-      } else {
-        setSelectedDate(undefined);
-      }
-    };
-
     // Only call onChange if the input is a valid date
     const effectiveOnChange = (event: ChangeEvent<HTMLInputElement>) => {
       // Then, call the onChange from props if it the input is valid
@@ -213,6 +189,17 @@ const CustomDatepicker = forwardRef<DatepickerProps, "input">(
       const parsedDate = parse(dateStr, "yyyy-MM-dd", new Date());
       if (isValid(parsedDate)) {
         onChange?.(event);
+      }
+    };
+
+    const handleDayPickerSelect = (date: Date | undefined) => {
+      if (!date) {
+        setInputValue("");
+        setSelectedDate(undefined);
+      } else {
+        setSelectedDate(date);
+        setMonth(date);
+        setInputValue(formatDate(date));
       }
     };
 
@@ -226,13 +213,16 @@ const CustomDatepicker = forwardRef<DatepickerProps, "input">(
         <InputGroup>
           <PopoverAnchor>
             <KVInput
+              placeholder="Velg dato"
+              value={inputValue}
               ref={ref}
               className="custom-datepicker"
               isDisabled={isDisabled}
               isInvalid={isInvalid}
               isRequired={isRequired}
-              onChange={handleInputChange}
+              onChange={effectiveOnChange}
               {...KVInputProps}
+              locale={nb}
             />
           </PopoverAnchor>
           <InputRightElement opacity={isDisabled ? 0.5 : 1} pointerEvents={isDisabled ? "none" : "auto"} height="100%">
@@ -251,15 +241,19 @@ const CustomDatepicker = forwardRef<DatepickerProps, "input">(
         <PopoverContent width="auto" padding="1rem">
           <style>{style}</style>
           <DayPicker
-            captionLayout={showDropdownMonthYear ? "dropdown-buttons" : undefined}
-            disableNavigation={disableNavigation}
-            defaultMonth={defaultMonth}
+            mode="single"
+            month={month}
+            onMonthChange={setMonth}
+            selected={selectedDate}
+            onSelect={handleDayPickerSelect}
+            classNames={{ root: uniqueClassName }}
+            locale={nb}
             showOutsideDays={showOutsideDays}
             showWeekNumber={showWeekNumber}
             disabled={disabledDays}
-            classNames={{ root: uniqueClassName }}
-            selected={selectedDate}
-            onSelect={handleDayPickerSelect}
+            disableNavigation={disableNavigation}
+            defaultMonth={defaultMonth}
+            captionLayout={showDropdownMonthYear ? "dropdown" : "label"}
           />
         </PopoverContent>
       </Popover>
@@ -288,7 +282,7 @@ function extractKVProps(props: DatepickerProps): InputProps {
 type ValidDateInput = number | Date | string;
 
 // Function to format a date to the format used by the datepicker
-function formatDate(date: ValidDateInput, format?: "no" | "en"): string {
+function formatDate(date: ValidDateInput): string {
   let dateObject: Date;
 
   if (typeof date === "number") {
@@ -307,7 +301,7 @@ function formatDate(date: ValidDateInput, format?: "no" | "en"): string {
   const y = dateObject.getFullYear().toString().padStart(4, "0");
   const m = (dateObject.getMonth() + 1).toString().padStart(2, "0");
   const d = dateObject.getDate().toString().padStart(2, "0");
-  return format === "no" ? `${d}.${m}.${y}` : `${y}-${m}-${d}`;
+  return `${d}.${m}.${y}`;
 }
 
 // Function to get the common input props the native and custom datepicker
@@ -325,12 +319,80 @@ const getCommonInputProps = (props: DatepickerProps) => {
 const css = (className: string, colorPalette: Record<number, string>) => {
   return `
  .${className} {
-  --rdp-cell-size: 40px; /* Size of the day cells. */
-  --rdp-caption-font-size: 18px; /* Font size for the caption labels. */
-  --rdp-accent-color: ${colorPalette[500]}; /* Accent color for the background of selected days. */
-  --rdp-background-color: ${colorPalette[100]}; /* Background color for the hovered/focused elements. */
-  --rdp-outline: 2px solid var(--rdp-accent-color); /* Outline border for focused elements */
-  --rdp-selected-color: #fff; /* Color of selected day text */
+
+   /* Colors */
+  --rdp-accent-color: ${colorPalette[500]}; /* The accent color used for selected days and UI elements. */
+  --rdp-accent-background-color: ${colorPalette[100]}; /* The accent background color used for selected days and UI elements. */
+
+  /* Fonts */
+  --rdp-week_number-font: 12px/1 sans-serif;
+
+  /* Day buttons */
+  --rdp-day-width: 40px; /* Width of the day cells. */
+  --rdp-day-height: 40px; /* Height of the day cells. */
+  --rdp-day_button-height: var(--rdp-day-height); /* Height of the day buttons. */
+  --rdp-day_button-width: var(--rdp-day-width); /* Width of the day buttons. */
+  --rdp-day_button-border-radius: 50%;
+  --rdp-outside-opacity: 0.6; /* Opacity of the days outside the current month. */
+
+  /* Week numbers */
+  --rdp-weekday-text-align: center;
+
+  /* Day styles */
+  .rdp-today {
+    font-weight: 700;
+  }
+
+  .rdp-day:hover:not(.rdp-selected) {
+    border-radius: var(--rdp-day_button-border-radius);
+    background-color: var(--rdp-accent-background-color);
+  }
+
+  .rdp-selected {
+    background-color: var(--rdp-accent-color);
+    color: white;
+    border-radius: var(--rdp-day_button-border-radius);
+  }
+
+  /* Navigation chevrons */
+  .rdp-chevron {
+    box-sizing: border-box;
+    padding: 8px;
+    height: var(--rdp-day_button-height);
+    width: var(--rdp-day_button-width);
+  }
+
+  .rdp-chevron:hover {
+    border-radius: var(--rdp-day_button-border-radius);
+    background-color: var(--rdp-accent-background-color);
+  }
+
+  /* Weekday and week number styles */
+  .rdp-weekday {
+    vertical-align: middle;
+    font-size: 0.75em;
+    font-weight: 700;
+    text-align: center;
+    height: var(--rdp-day_button-height);
+    text-transform: uppercase;
+  }
+
+  .rdp-week_number {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: content-box;
+    width: var(--rdp-day_button-width);
+    height: var(--rdp-day_button-height);
+  }
+
+  /* Month caption */
+  .rdp-month_caption {
+    padding: 0.25em;
+    font-weight: 700;
+    font-size: 18px;
+  }
+
 }
 `;
 };
