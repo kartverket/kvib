677de3af58f81e181a05b9723e746971b7fbccc1
chrhein <chrhein@users.noreply.github.com>
2024-10-10 13:08:34 +0000

    Deploy preview for PR 877 🛫

---
+++ pr-preview/pr-877/assets/CloseButton-jKuCFjkf.js
@@ -0,0 +1,11 @@
+import{j as o}from"./extends-CwFRzn3r.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{ae as m,af as e,ag as p}from"./index-BGnjr7g7.js";import{C as a,a as n,b as l}from"./CloseButton.stories-Cn6q25WK.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CnGWpzHb.js";import"../sb-preview/runtime.js";import"./index-Dn0f7ynd.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-lfp1ZYuW.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./close-button-uAmhqF5q.js";import"./icon-DQb5sAJm.js";import"./forward-ref-BWI-Phbn.js";import"./use-style-config--HlEOpE-.js";import"./omit-QLD0Eizt.js";import"./index-CfyPTyT-.js";import"./hooks-D04p5R0A.js";import"./use-theme-CBfqdygA.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./factory-CdGA0D9p.js";import"./cx-BDMMs1jM.js";import"./stack-Crl0JO1i.js";import"./responsive-ClFn5vzN.js";import"./children-2ZXUntH3.js";function i(r){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(m,{of:a}),`
+`,o.jsx(t.h1,{id:"close-button",children:"Close Button"}),`
+`,o.jsx(t.p,{children:"CloseButton er en knapp med et lukkeikon. Den skal brukes til å lukke funksjonalitet i komponenter."}),`
+`,o.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`import { CloseButton } from "@kvib/react";
+`})}),`
+`,o.jsx(t.h2,{id:"props",children:"Props"}),`
+`,o.jsx(e,{of:n}),`
+`,o.jsx(p,{of:n}),`
+`,o.jsx(t.h2,{id:"størrelser",children:"Størrelser"}),`
+`,o.jsx(e,{of:l})]})}function A(r={}){const{wrapper:t}={...s(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{A as default};
