633fe28fe0b3d206f9e0a5c3315c726517a4f722
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-16 12:21:03 +0000

    Deploying to gh-pages from @ kartverket/kvib@a7fcd60c20ed0bae2568527010ed1330383b5d9c 🚀

---
+++ assets/Theme-DAvH1aW7.js
@@ -0,0 +1,16 @@
+import{j as t}from"./extends-CwFRzn3r.js";import{useMDXComponents as m}from"./index-DlpNa54Y.js";import"./chunk-NUUEMKO5-OAU_NR0l.js";import{ai as i,ae as p}from"./index-BygTTvM2.js";import{F as s,D as n,a as c}from"./Feedback-D9CVkTVO.js";import{D as a}from"./DocsCanvas-BhnAGqOh.js";import{u as h}from"./use-theme-CBfqdygA.js";import{B as u}from"./box-B7h7acUJ.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CqMQJHeQ.js";import"../sb-preview/runtime.js";import"./react-18-4JAveGcY.js";import"./index-Dn0f7ynd.js";import"./index-D-8MO0q_.js";import"./isNativeReflectConstruct-lfp1ZYuW.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./image-Cz43cXZB.js";import"./forward-ref-BWI-Phbn.js";import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./factory-CdGA0D9p.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./omit-QLD0Eizt.js";import"./simple-grid-CQLrn815.js";import"./grid-zpFC2r7C.js";import"./hooks-D04p5R0A.js";import"./responsive-ClFn5vzN.js";import"./heading-D_euA-I1.js";import"./use-style-config--HlEOpE-.js";import"./index-CfyPTyT-.js";import"./cx-BDMMs1jM.js";import"./text-rUwQV1v4.js";import"./Card-DoxEvc2h.js";import"./providers-tFX8H8_I.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./context-CBKykClo.js";import"./calc-Dy10IX1y.js";import"./css-var-DE0-P1bc.js";import"./pick-DBnSFT3T.js";import"./flex-CHlq4-Ax.js";import"./Button-CQCVnziL.js";import"./button-Db-BlFJ2.js";import"./spinner-DcEBpU52.js";import"./define-styles-BSAHv3yx.js";import"./use-merge-refs-Bdxaq0s4.js";import"./attr-sHTFgF0E.js";import"./center-D672Tu8u.js";import"./h-stack-P6-XT6iQ.js";import"./stack-Crl0JO1i.js";import"./children-2ZXUntH3.js";import"./Icon-CJPLeMAE.js";/* empty css              */const x=`function Example() {
+  const theme = useTheme();
+
+  return (
+      <Box style={{backgroundColor: theme.colors.red[500]}} 
+      width={'450px'} 
+      height={'100px'} 
+      p={4} 
+      color='white'/>
+  }`,l=()=>{const o=h();return t.jsxs(a,{children:[t.jsx(u,{style:{backgroundColor:o.colors.red[500]},width:"450px",height:"100px",p:4,color:"white"}),t.jsx(i,{code:x,dark:!0})]})};function e(o){const r={code:"code",h1:"h1",p:"p",pre:"pre",strong:"strong",...m(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(p,{title:"Hooks/useTheme"}),`
+`,t.jsx(r.h1,{id:"usetheme",children:"useTheme"}),`
+`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"useTheme"})," er en tilpasset hook som brukes for å hente temaobjektet fra kontekst."]}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`import { useTheme } from "@kvib/react";
+`})}),`
+`,t.jsx(s,{component:"useTheme"}),`
+`,t.jsx(n,{children:t.jsx(c,{title:"Bruk",description:"useTheme returnerer temaobjektet.",isVertical:!0,story:t.jsx(l,{})})})]})}function ft(o={}){const{wrapper:r}={...m(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{ft as default};
