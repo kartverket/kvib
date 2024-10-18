e79d146f2e4bbab7f74b5661bacb0e02ad061d24
chrhein <chrhein@users.noreply.github.com>
2024-10-18 09:20:08 +0000

    Deploy preview for PR 886 🛫

---
+++ pr-preview/pr-886/assets/Container.stories-CZ588pbn.js
@@ -0,0 +1,11 @@
+import{j as a}from"./extends-CwFRzn3r.js";import{C as r}from"./container-CzLdjxcW.js";const i={title:"Layout/Container",component:r,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}}},e={args:{backgroundColor:"green.50",padding:"6"},render:s=>a.jsx(r,{...s,children:"There are many benefits to a joint design and development system. Not only does it bring benefits to the design team, but it also brings benefits to engineering teams. It makes sure that our experiences have a consistent look and feel, not just in our design specs, but in production"})};var n,t,o;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    backgroundColor: "green.50",
+    padding: "6"
+  },
+  render: args => <KvibContainer {...args}>
+      There are many benefits to a joint design and development system. Not only does it bring benefits to the design
+      team, but it also brings benefits to engineering teams. It makes sure that our experiences have a consistent look
+      and feel, not just in our design specs, but in production
+    </KvibContainer>
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Preview"],u=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:d,default:i},Symbol.toStringTag,{value:"Module"}));export{u as C,e as P};
