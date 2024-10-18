a2cf75463804de2d6b1a7d17175e918ea52279fb
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-18 09:30:05 +0000

    Deploying to gh-pages from @ kartverket/kvib@4555cf465fded02cf09bdb7a79d2bf8d4cceabf3 🚀

---
+++ assets/Slider-B27oC-nV.js
@@ -0,0 +1,35 @@
+import{j as r}from"./extends-CwFRzn3r.js";import{useMDXComponents as a}from"./index-DlpNa54Y.js";import{ae as n,af as t,ai as d,ag as c}from"./index-C7kypGf2.js";import{D as i,F as S,b as s,a as l}from"./Feedback-Dh6LptKD.js";import{S as u,a as h,b as x,P as m}from"./Slider.stories-DjBQ2hlm.js";import{A as k}from"./Alert-PLl3e2Qe.js";import{A as j}from"./alert-icon-B8tlVjfA.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DpCJl_jV.js";import"../sb-preview/runtime.js";import"./index-Drt3gf4w.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./box-DeqT4ixr.js";import"./factory-C6UFhaBW.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./image-BzV54DYr.js";import"./forward-ref-BWI-Phbn.js";import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./omit-QLD0Eizt.js";import"./simple-grid-inAnnyJ8.js";import"./grid-B2CMUJGq.js";import"./use-theme-CBfqdygA.js";import"./hooks-gaHRB8Pq.js";import"./responsive-BNzRyFLh.js";import"./heading--NES2npU.js";import"./use-style-config-YUvqttAg.js";import"./index-CfyPTyT-.js";import"./cx-BDMMs1jM.js";import"./text-ByHKaHOl.js";import"./Card-rgsWy52h.js";import"./providers-BwxcBeey.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./context-CBKykClo.js";import"./calc-B93s4yJe.js";import"./css-var-DE0-P1bc.js";import"./pick-DBnSFT3T.js";import"./flex-C1gIBb6o.js";import"./Link-Dp4j43jb.js";import"./slider-DqbplSP6.js";import"./slider-utils-DlyfFmdB.js";import"./owner-DF4q1r8F.js";import"./is-element-C52yPwBl.js";import"./use-callback-ref-DA6UG1mJ.js";import"./use-controllable-state-Dnial3e2.js";import"./number-D-Zd7p0h.js";import"./use-update-effect-CNg9DBIF.js";import"./use-merge-refs-Bdxaq0s4.js";import"./attr-sHTFgF0E.js";import"./call-all-DDzecCnU.js";import"./tooltip-BAhajwUq.js";import"./use-popper-DwNTykfS.js";import"./use-disclosure-CdGn7wGE.js";import"./use-event-listener-DbiM-QVt.js";import"./portal-DTuMj-Dh.js";import"./proxy-CQsau1XN.js";import"./index-DVNvNJVv.js";import"./define-styles-BSAHv3yx.js";import"./icon-Bx5s8P2X.js";import"./spinner-DfEqsPDM.js";const f=` 
+  const SliderWithTooltip = () => {
+    const [sliderValue, setSliderValue] = React.useState(5);
+    const [showTooltip, setShowTooltip] = React.useState(false);
+    return (
+    <KvibSlider
+    id="slider"
+    aria-label="sliderWithTooltip"
+    defaultValue={5}
+    onChange={(v) => setSliderValue(v)}
+    onMouseEnter={() => setShowTooltip(true)}
+    onMouseLeave={() => setShowTooltip(false)}
+  >
+    <SliderMark value={25} mt="1" ml="-2.5" fontSize="sm">
+      25%
+    </SliderMark>
+    <SliderMark value={50} mt="1" ml="-2.5" fontSize="sm">
+      50%
+    </SliderMark>
+    <SliderMark value={75} mt="1" ml="-2.5" fontSize="sm">
+      75%
+    </SliderMark>
+    <SliderTrack>
+      <SliderFilledTrack />
+    </SliderTrack>
+    <Tooltip hasArrow placement="top" isOpen={showTooltip} label={$sliderValue}>
+      <SliderThumb />
+    </Tooltip>
+  </KvibSlider>
+    )
+  }
+`;function p(o){const e={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...a(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(n,{of:u}),`
+`,r.jsxs(i,{children:[r.jsx(e.h1,{id:"slider",children:"Slider"}),r.jsx(e.p,{children:"Her er en oversikt over Slider-komponenten, og hvordan denne bygges opp av flere andre komponenter."}),r.jsxs(k,{status:"warning",children:[r.jsx(j,{}),r.jsx(e.p,{children:"En Slider skal alltid stå sammen med en Label. Dersom det mot formodning avviker fra dette må aria-label benyttes."})]}),r.jsx("br",{}),r.jsx(e.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import { Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark } from "@kvib/react";
+`})}),r.jsx(S,{component:"Slider"}),r.jsx(s,{light:!0,children:"Box props"}),r.jsx(i,{children:r.jsxs(e.p,{children:[`SliderThumb, SliderTrack og SliderFilledTrack er bygget på Box-komponenten, så alle props til Box kan benyttes i disse komponentene for å endre styling. Se
+`,r.jsx(e.a,{href:"/docs/komponenter-layout-box--docs",children:"dokumentasjonen for Box for beskrivelse og eksempler"}),"."]})}),r.jsx(l,{title:"Diskret slider med step",description:"",story:r.jsx(t,{of:h,sourceState:"hidden"}),isVertical:!0}),r.jsx(l,{title:"Slider med tooltip og marks",description:"",story:r.jsxs(r.Fragment,{children:[r.jsx(t,{of:x,sourceState:"none"}),r.jsx(d,{code:f,dark:!0})]}),isVertical:!0}),r.jsx(s,{light:!0,children:"Props"}),r.jsx(t,{of:m}),r.jsx(c,{of:m})]})]})}function Br(o={}){const{wrapper:e}={...a(),...o.components};return e?r.jsx(e,{...o,children:r.jsx(p,{...o})}):p(o)}export{Br as default};
---
+++ assets/VisuallyHidden.stories-DMoQz72C.js
@@ -0,0 +1,14 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{V as i,a as y}from"./visually-hidden-Dw_aOpzd.js";import{F as H}from"./Form-control-Dfay2bjP.js";import{F as V}from"./Form-label-B-qAuQLc.js";import{B as h}from"./Button-CXAI4A3K.js";import{I as j}from"./Icon-LAcu_LZm.js";import{B as f}from"./box-DeqT4ixr.js";import{H as I}from"./heading--NES2npU.js";const E={title:"Annet/Visually Hidden",component:i,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}}},k=({...r})=>e.jsx(e.Fragment,{children:e.jsxs(h,{children:[e.jsx(i,{...r,children:"Checkmark"}),e.jsx(j,{icon:"check"})]})}),s={args:{children:"VisuallyHidden"},render:r=>e.jsx(k,{...r})},S=({...r})=>e.jsxs(f,{children:[e.jsx(I,{children:"Tittel og beskrivelse"}),e.jsx(i,{...r,children:"Dette vil bli skjult"})]}),a={args:{children:"VisuallyHidden"},render:r=>e.jsx(S,{...r})},b=({...r})=>e.jsx(H,{children:e.jsxs(V,{children:["Visually Hidden Input",e.jsx(y,{placeholder:"Visually Hidden Input",...r,defaultChecked:!0,onChange:g=>{console.log(g.target.checked)}})]})}),n={args:{},render:r=>e.jsx(b,{...r})};var l,d,t;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: "VisuallyHidden"
+  },
+  render: args => <VisuallyHiddenExample {...args} />
+}`,...(t=(d=s.parameters)==null?void 0:d.docs)==null?void 0:t.source}}};var o,c,u;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: "VisuallyHidden"
+  },
+  render: args => <VisuallyHiddenText {...args} />
+}`,...(u=(c=a.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var m,p,x;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {},
+  render: args => <VisuallyHiddenInputExample {...args} />
+}`,...(x=(p=n.parameters)==null?void 0:p.docs)==null?void 0:x.source}}};const v=["Preview","VisuallyHiddenTextExample","VisuallyHiddenInput"],z=Object.freeze(Object.defineProperty({__proto__:null,Preview:s,VisuallyHiddenInput:n,VisuallyHiddenTextExample:a,__namedExportsOrder:v,default:E},Symbol.toStringTag,{value:"Module"}));export{s as P,z as V};
