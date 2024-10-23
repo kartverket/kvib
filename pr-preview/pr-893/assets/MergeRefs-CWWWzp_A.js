be9a4beeb36f0d277cad8e02c50831122b1362b2
chrhein <chrhein@users.noreply.github.com>
2024-10-23 10:35:58 +0000

    Deploy preview for PR 893 🛫

---
+++ pr-preview/pr-893/assets/MergeRefs-CWWWzp_A.js
@@ -0,0 +1,41 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{useMDXComponents as c}from"./index-DlpNa54Y.js";import"./chunk-NUUEMKO5-wHdwVLK2.js";import{ae as g,ai as p}from"./index-BYXiNgyR.js";import{F as j,D as k,b as M}from"./Feedback-C8oPMuBk.js";import{R as f}from"./index-BwDkhjyp.js";import{D as a}from"./DocsCanvas-DLlOGo4n.js";import{u as l}from"./use-merge-refs-Bdxaq0s4.js";import{u as _}from"./use-popper-DwNTykfS.js";import{u as b}from"./use-outside-click-DJDnsvpj.js";import{u as C}from"./use-disclosure-CdGn7wGE.js";import{B as y}from"./box-Cs-tJ6EH.js";import"./iframe-B9TI63e6.js";import"../sb-preview/runtime.js";import"./react-18-CyFAdspd.js";import"./index-CS4BWtGh.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./image-CU6Rr-ts.js";import"./forward-ref-BWI-Phbn.js";import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./factory-lWLPzty5.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./omit-QLD0Eizt.js";import"./simple-grid-Dx05c2KX.js";import"./grid-DbabZME7.js";import"./use-theme-CBfqdygA.js";import"./hooks-CUXMK5kD.js";import"./responsive-BbUtuhhZ.js";import"./heading-CZK5ruZ7.js";import"./use-style-config-C4sZEIFb.js";import"./index-CfyPTyT-.js";import"./cx-BDMMs1jM.js";import"./text-CypcOzo0.js";import"./Card-CjsZQyOM.js";import"./providers-BQbExJqd.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./context-CBKykClo.js";import"./calc-tjRj3nBR.js";import"./css-var-DE0-P1bc.js";import"./pick-DBnSFT3T.js";import"./flex-DUW0W68E.js";import"./Link-B6mGcy8v.js";import"./stack-C3a7yBZW.js";import"./children-2ZXUntH3.js";import"./use-callback-ref-DA6UG1mJ.js";const n=({ref:o,...r})=>{const t=f.useRef(),s=l(t,o);return e.jsx(a,{children:e.jsx("div",{...r,ref:s,children:"A div with multiple refs."})})},i=({ref:o,...r})=>{const t=f.useRef(),{isOpen:s,onOpen:u,onClose:d}=C(),{popperRef:h,referenceRef:R}=_();b({ref:t,handler:()=>s&&d()});const x=l(t,R);return e.jsx(a,{children:e.jsxs("div",{children:[e.jsx("button",{ref:x,onClick:u,children:"Click me to see the popover"}),s&&e.jsx(y,{ref:h,bg:"green",children:"Click outside to close me"})]})})};try{n.displayName="MergeRefsExample",n.__docgenInfo={description:"",displayName:"MergeRefsExample",props:{ref:{defaultValue:null,description:"",name:"ref",required:!0,type:{name:"any"}}}}}catch{}try{i.displayName="MergeRefsOtherExample",i.__docgenInfo={description:"",displayName:"MergeRefsOtherExample",props:{ref:{defaultValue:null,description:"",name:"ref",required:!0,type:{name:"any"}}}}}catch{}const O=`function Example({ ref, ...props }) {
+  const internalRef = React.useRef()
+  const refs = useMergeRefs(internalRef, ref)
+
+  return (
+    <div {...props} ref={refs}>
+      A div with multiple refs.
+    </div>
+  )
+}`,v=`function Example({ ref, ...props }) {
+  const outsideRef = React.useRef()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { popperRef, referenceRef } = usePopper()
+
+  useOutsideClick({
+    ref: outsideRef,
+    handler: () => isOpen && onClose(),
+  })
+
+  const buttonEl = useMergeRefs(outsideRef, referenceRef)
+
+  return (
+    <>
+      <button ref={buttonEl} onClick={onOpen}>
+        Click me to see the popover
+      </button>
+      {isOpen && (
+        <Box ref={popperRef} bg='green'>
+          Click outside to close me
+        </Box>
+      )}
+    </>
+  )
+}`;function m(o){const r={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...c(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(g,{title:"Hooks/useMergeRefs"}),`
+`,e.jsx(r.h1,{id:"usemergerefs",children:"useMergeRefs"}),`
+`,e.jsxs(r.p,{children:[e.jsx(r.strong,{children:"useMergeRefs"})," er en tilpasset hook som brukes til å slå sammen flere React-refs til en enkelt ref."]}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { useMergeRefs } from "@kvib/react";
+`})}),`
+`,e.jsx(j,{component:"useMergeRefs"}),`
+`,e.jsxs(k,{children:[e.jsxs(r.p,{children:[e.jsx(M,{light:!0,children:"Return value"}),`
+useMergeRefs hooken returnerer en funksjon som mottar elementet og tildeler verdien til de gitte React-refs-ene.`]}),e.jsx(r.h2,{id:"bruk",children:"Bruk"}),e.jsx(n,{}),e.jsx(p,{code:O,dark:!0}),e.jsx(r.h2,{id:"bruk-i-kombinasjon-med-andre-hooks",children:"Bruk i kombinasjon med andre hooks"}),e.jsx(i,{}),e.jsx(p,{code:v,dark:!0})]})]})}function Oe(o={}){const{wrapper:r}={...c(),...o.components};return r?e.jsx(r,{...o,children:e.jsx(m,{...o})}):m(o)}export{Oe as default};
