5fc6e8bc1ef69fdf8c251d62789bdf1cd2b1f490
chrhein <chrhein@users.noreply.github.com>
2024-10-24 06:37:32 +0000

    Deploy preview for PR 893 🛫

---
+++ pr-preview/pr-893/assets/LinkOverlay-DM-NXVhr.js
@@ -0,0 +1,29 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{ae as a,af as n,ag as m,ai as p}from"./index-DZBJQxXM.js";import{L as l,P as o,a as d}from"./LinkOverlay.stories-BD9X-EP0.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-WV1SlPsl.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./forward-ref-BWI-Phbn.js";import"./factory-lWLPzty5.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./cx-BDMMs1jM.js";import"./heading-CZK5ruZ7.js";import"./use-style-config-C4sZEIFb.js";import"./omit-QLD0Eizt.js";import"./index-CfyPTyT-.js";import"./hooks-CUXMK5kD.js";import"./use-theme-CBfqdygA.js";import"./text-CypcOzo0.js";import"./box-Cs-tJ6EH.js";function t(i){const r={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(r.h1,{id:"link-overlay",children:"Link Overlay"}),`
+`,e.jsx(r.p,{children:"Link Overlay er et semantisk komponent som brukes til å wrappe elementer sammen i en link."}),`
+`,e.jsx(r.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { LinkBox, LinkOverlay } from "@kvib/react";
+`})}),`
+`,e.jsx(r.h2,{id:"props",children:"Props"}),`
+`,e.jsx(n,{of:o}),`
+`,e.jsx(m,{of:o}),`
+`,e.jsx(r.h2,{id:"nested-link",children:"Nested link"}),`
+`,e.jsx(n,{of:d}),`
+`,e.jsx(r.h2,{id:"link-overlay-med-routing-library",children:"Link Overlay med Routing Library"}),`
+`,e.jsxs(r.p,{children:["For å legge til støtte for biblioteker som Next.js eller gatsby Link må du wrappe de rundt ",e.jsx(r.code,{children:"LinkOverlay"})," eller bruke de som en prop."]}),`
+`,e.jsx(p,{code:`const RoutingExample = () => {
+  return (
+    <LinkBox as="article">
+      <h2>
+        <LinkOverlay as={NextLink} href="#">
+          Some blog post
+        </LinkOverlay>
+      </h2>
+      <p>
+        As a side note, using quotation marks around an attribute value is required only if this value is not a valid
+        identifier.
+      </p>
+      <a href="#">Some inner link</a>
+    </LinkBox>
+  );
+};`,dark:!0})]})}function A(i={}){const{wrapper:r}={...s(),...i.components};return r?e.jsx(r,{...i,children:e.jsx(t,{...i})}):t(i)}export{A as default};
---
+++ pr-preview/pr-893/assets/LinkOverlay.stories-BD9X-EP0.js
@@ -0,0 +1,26 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{f as b}from"./forward-ref-BWI-Phbn.js";import{c as g}from"./factory-lWLPzty5.js";import{c as u}from"./cx-BDMMs1jM.js";import{H as h}from"./heading-CZK5ruZ7.js";import{T as f}from"./text-CypcOzo0.js";import{B as d}from"./box-Cs-tJ6EH.js";const l=b(function(a,i){const{isExternal:o,target:t,rel:K,className:j,...L}=a;return e.jsx(g.a,{...L,ref:i,className:u("chakra-linkbox__overlay",j),rel:o?"noopener noreferrer":K,target:o?"_blank":t,__css:{position:"static","&::before":{content:"''",cursor:"inherit",display:"block",position:"absolute",top:0,left:0,zIndex:0,width:"100%",height:"100%"}}})}),y=b(function(a,i){const{className:o,...t}=a;return e.jsx(g.div,{ref:i,position:"relative",...t,className:u("chakra-linkbox",o),__css:{"a[href]:not(.chakra-linkbox__overlay), abbr[title]":{position:"relative",zIndex:1}}})}),B={title:"Navigasjon/LinkOverlay",component:l,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}},argTypes:{isExternal:{description:"If true, the link will open in new tab",table:{type:{summary:"Boolean"},defaultValue:{summary:"false"}},control:"boolean"}}},n={render:r=>e.jsxs(y,{as:"article",maxW:"sm",p:"5",borderWidth:"1px",rounded:"md",children:[e.jsx(h,{size:"md",my:"2",children:e.jsx(l,{...r,href:"#",children:"Denne kan trykkes på"})}),e.jsx(f,{children:"Hele komponenten er en knapp"})]})},s={render:r=>e.jsxs(y,{as:"article",maxW:"sm",p:"5",borderWidth:"1px",rounded:"md",children:[e.jsx(d,{children:"13 timer siden"}),e.jsx(h,{size:"md",my:"2",children:e.jsx(l,{...r,href:"#",children:"Ikke gå deg vill på bærtur"})}),e.jsx(f,{mb:"3",children:'Bær- og soppsesongen er i full gang til skogs og fjells. Last ned Kartverkets gratis mobil-app "Hvor?", så vet du hvor du er.'}),e.jsx(d,{as:"a",color:"green",href:"#",fontWeight:"bold",children:"En ekstra link"})]})};var c,m,p;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: args => <KvibLinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
+      <KvibHeading size="md" my="2">
+        <KvibLinkOverlay {...args} href="#">
+          Denne kan trykkes på
+        </KvibLinkOverlay>
+      </KvibHeading>
+      <KvibText>Hele komponenten er en knapp</KvibText>
+    </KvibLinkBox>
+}`,...(p=(m=n.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var v,k,x;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: args => <KvibLinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
+      <KvibBox>13 timer siden</KvibBox>
+      <KvibHeading size="md" my="2">
+        <KvibLinkOverlay {...args} href="#">
+          Ikke gå deg vill på bærtur
+        </KvibLinkOverlay>
+      </KvibHeading>
+      <KvibText mb="3">
+        Bær- og soppsesongen er i full gang til skogs og fjells. Last ned Kartverkets gratis mobil-app "Hvor?", så vet
+        du hvor du er.
+      </KvibText>
+      <KvibBox as="a" color="green" href="#" fontWeight="bold">
+        En ekstra link
+      </KvibBox>
+    </KvibLinkBox>
+}`,...(x=(k=s.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};const _=["Preview","LinkOverlayNested"],E=Object.freeze(Object.defineProperty({__proto__:null,LinkOverlayNested:s,Preview:n,__namedExportsOrder:_,default:B},Symbol.toStringTag,{value:"Module"}));export{E as L,n as P,s as a};
