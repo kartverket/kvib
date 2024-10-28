3b68351c9a97d78e03fbd8544220d35fcf7d6579
chrhein <chrhein@users.noreply.github.com>
2024-10-28 07:08:14 +0000

    Remove preview for PR 894 🛬

--- pr-preview/pr-894/assets/responsive-BbUtuhhZ.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./factory-lWLPzty5.js";const i=Object.freeze(["base","sm","md","lg","xl","2xl"]);function c(n,r){return Array.isArray(n)?n.map(t=>t===null?null:r(t)):a(n)?Object.keys(n).reduce((t,e)=>(t[e]=r(n[e]),t),{}):n!=null?r(n):null}function f(n,r=i){const t={};return n.forEach((e,s)=>{const u=r[s];e!=null&&(t[u]=e)}),t}export{f as a,i as b,c as m};
