c76048cd8ead3fba013c28c3b0ba2300176f0e9a
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-28 07:07:59 +0000

    Deploying to gh-pages from @ kartverket/kvib@eb3e0ec1b34f58e773be7bfb01deda599e7f1e90 🚀

---
+++ assets/Z-index-DcQzgBEE.js
@@ -0,0 +1,19 @@
+import{j as o}from"./extends-CwFRzn3r.js";import{useMDXComponents as r}from"./index-DlpNa54Y.js";import"./chunk-NUUEMKO5-B85kuFc0.js";import{ae as i,ai as m}from"./index-BdeBcD3D.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Aivc9q2u.js";import"../sb-preview/runtime.js";import"./react-18-CyFAdspd.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";const s=`
+export const zIndices = {
+  hide: -1,
+  auto: "auto",
+  base: 0,
+  docked: 10,
+  dropdown: 1000,
+  sticky: 1100,
+  banner: 1200,
+  overlay: 1300,
+  modal: 1400,
+  popover: 1500,
+  skipLink: 1600,
+  toast: 1700,
+  tooltip: 1800,
+};
+`;function e(t){const n={h2:"h2",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Komponenter/Tokens/Z-index"}),`
+`,o.jsx(n.h2,{id:"z-index",children:"Z-index"}),`
+`,o.jsx(m,{code:s,dark:!0})]})}function X(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{X as default};
