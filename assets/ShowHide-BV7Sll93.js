0430c0b6fd4472fccd3da491c6a6918e5d3b95c7
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-28 15:15:07 +0000

    Deploying to gh-pages from @ kartverket/kvib@b711a543a79848479bda1ce391b9d31974e8980d 🚀

---
+++ assets/ShowHide-BV7Sll93.js
@@ -0,0 +1,9 @@
+import{j as r}from"./extends-CwFRzn3r.js";import{useMDXComponents as n}from"./index-DlpNa54Y.js";import{ae as s,af as m,ag as p}from"./index-CPKov55y.js";import{S as a,P as i}from"./ShowHide.stories-DEvIpYSZ.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-COYsYg2r.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./use-theme-CBfqdygA.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./use-media-query-DZQeksUW.js";import"./env-B2QgRHdN.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./box-Cs-tJ6EH.js";import"./factory-lWLPzty5.js";function t(o){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...n(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:a}),`
+`,r.jsx(e.h1,{id:"show-hide",children:"Show-Hide"}),`
+`,r.jsx(e.p,{children:"Show og Hide er wrapperkomponenter som render eller ikke render child-elementer hvis media query som er spesifisert passer."}),`
+`,r.jsx(e.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import { Show, Hide } from "@kvib/react";
+`})}),`
+`,r.jsx(e.h2,{id:"props",children:"Props"}),`
+`,r.jsx(m,{of:i}),`
+`,r.jsx(p,{of:i})]})}function q(o={}){const{wrapper:e}={...n(),...o.components};return e?r.jsx(e,{...o,children:r.jsx(t,{...o})}):t(o)}export{q as default};
