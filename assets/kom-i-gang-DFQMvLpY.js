91588477af5ad4d6a0a71d84236de04c3ead8063
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-28 15:09:42 +0000

    Deploying to gh-pages from @ kartverket/kvib@56dcef45d03061586a742d673b941d4d9627b12a 🚀

---
+++ assets/kom-i-gang-DFQMvLpY.js
@@ -0,0 +1,88 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{useMDXComponents as o}from"./index-DlpNa54Y.js";import"./chunk-NUUEMKO5-Cf3yRRFi.js";import{ae as m}from"./index-CfPSepdb.js";import{F as p}from"./flex-DUW0W68E.js";import{L as n}from"./Link-B6mGcy8v.js";import{I as a}from"./Icon-COWGDXDO.js";import{T as c}from"./text-CypcOzo0.js";import{B as i}from"./box-Cs-tJ6EH.js";import{I as l}from"./image-CU6Rr-ts.js";import{B as d}from"./Button-BPVxvY9E.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DPjcFLBO.js";import"../sb-preview/runtime.js";import"./react-18-CyFAdspd.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./forward-ref-BWI-Phbn.js";import"./factory-lWLPzty5.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./use-style-config-C4sZEIFb.js";import"./omit-QLD0Eizt.js";import"./index-CfyPTyT-.js";import"./hooks-CUXMK5kD.js";import"./use-theme-CBfqdygA.js";import"./cx-BDMMs1jM.js";/* empty css              */import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./button-lwGmIUkK.js";import"./context-CBKykClo.js";import"./spinner-CCN1jjNR.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./define-styles-BSAHv3yx.js";import"./use-merge-refs-Bdxaq0s4.js";import"./attr-sHTFgF0E.js";import"./center-Bg6uA6YZ.js";import"./h-stack-ecaAvnDk.js";import"./stack-C3a7yBZW.js";import"./responsive-BbUtuhhZ.js";import"./children-2ZXUntH3.js";const s=r=>e.jsxs(p,{gap:"18px",...r,children:[e.jsxs(n,{href:"mailto:kvib-feedbacks-aaaahwh252gnftmofucpaa47ca@kartverketgroup.slack.com",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(a,{icon:"mail"}),"Send en mail"]}),e.jsxs(n,{href:"https://slack.com/app_redirect?channel=gen-designsystem",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(a,{icon:"tag"}),"Bli med i slack-kanalen"]}),e.jsxs(n,{href:"https://github.com/kartverket/kvib",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(a,{size:"40",icon:"polyline"}),"Lag en pull-request på github"]})]});try{s.displayName="ContributeLinks",s.__docgenInfo={description:"",displayName:"ContributeLinks",props:{_hover:{defaultValue:null,description:"",name:"_hover",required:!1,type:{name:"SystemStyleObject | undefined"}},_active:{defaultValue:null,description:"",name:"_active",required:!1,type:{name:"SystemStyleObject | undefined"}},_focus:{defaultValue:null,description:"",name:"_focus",required:!1,type:{name:"SystemStyleObject | undefined"}},_highlighted:{defaultValue:null,description:"",name:"_highlighted",required:!1,type:{name:"SystemStyleObject | undefined"}},_focusWithin:{defaultValue:null,description:"",name:"_focusWithin",required:!1,type:{name:"SystemStyleObject | undefined"}},_focusVisible:{defaultValue:null,description:"",name:"_focusVisible",required:!1,type:{name:"SystemStyleObject | undefined"}},_disabled:{defaultValue:null,description:"",name:"_disabled",required:!1,type:{name:"SystemStyleObject | undefined"}},_readOnly:{defaultValue:null,description:"",name:"_readOnly",required:!1,type:{name:"SystemStyleObject | undefined"}},_before:{defaultValue:null,description:"",name:"_before",required:!1,type:{name:"SystemStyleObject | undefined"}},_after:{defaultValue:null,description:"",name:"_after",required:!1,type:{name:"SystemStyleObject | undefined"}},_empty:{defaultValue:null,description:"",name:"_empty",required:!1,type:{name:"SystemStyleObject | undefined"}},_expanded:{defaultValue:null,description:"",name:"_expanded",required:!1,type:{name:"SystemStyleObject | undefined"}},_checked:{defaultValue:null,description:"",name:"_checked",required:!1,type:{name:"SystemStyleObject | undefined"}},_grabbed:{defaultValue:null,description:"",name:"_grabbed",required:!1,type:{name:"SystemStyleObject | undefined"}},_pressed:{defaultValue:null,description:"",name:"_pressed",required:!1,type:{name:"SystemStyleObject | undefined"}},_invalid:{defaultValue:null,description:"",name:"_invalid",required:!1,type:{name:"SystemStyleObject | undefined"}},_valid:{defaultValue:null,description:"",name:"_valid",required:!1,type:{name:"SystemStyleObject | undefined"}},_loading:{defaultValue:null,description:"",name:"_loading",required:!1,type:{name:"SystemStyleObject | undefined"}},_selected:{defaultValue:null,description:"",name:"_selected",required:!1,type:{name:"SystemStyleObject | undefined"}},_hidden:{defaultValue:null,description:"",name:"_hidden",required:!1,type:{name:"SystemStyleObject | undefined"}},_autofill:{defaultValue:null,description:"",name:"_autofill",required:!1,type:{name:"SystemStyleObject | undefined"}},_even:{defaultValue:null,description:"",name:"_even",required:!1,type:{name:"SystemStyleObject | undefined"}},_odd:{defaultValue:null,description:"",name:"_odd",required:!1,type:{name:"SystemStyleObject | undefined"}},_first:{defaultValue:null,description:"",name:"_first",required:!1,type:{name:"SystemStyleObject | undefined"}},_firstLetter:{defaultValue:null,description:"",name:"_firstLetter",required:!1,type:{name:"SystemStyleObject | undefined"}},_last:{defaultValue:null,description:"",name:"_last",required:!1,type:{name:"SystemStyleObject | undefined"}},_notFirst:{defaultValue:null,description:"",name:"_notFirst",required:!1,type:{name:"SystemStyleObject | undefined"}},_notLast:{defaultValue:null,description:"",name:"_notLast",required:!1,type:{name:"SystemStyleObject | undefined"}},_visited:{defaultValue:null,description:"",name:"_visited",required:!1,type:{name:"SystemStyleObject | undefined"}},_activeLink:{defaultValue:null,description:"",name:"_activeLink",required:!1,type:{name:"SystemStyleObject | undefined"}},_activeStep:{defaultValue:null,description:"",name:"_activeStep",required:!1,type:{name:"SystemStyleObject | undefined"}},_indeterminate:{defaultValue:null,description:"",name:"_indeterminate",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupOpen:{defaultValue:null,description:"",name:"_groupOpen",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupClosed:{defaultValue:null,description:"",name:"_groupClosed",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupHover:{defaultValue:null,description:"",name:"_groupHover",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerHover:{defaultValue:null,description:"",name:"_peerHover",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupFocus:{defaultValue:null,description:"",name:"_groupFocus",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerFocus:{defaultValue:null,description:"",name:"_peerFocus",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupFocusVisible:{defaultValue:null,description:"",name:"_groupFocusVisible",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerFocusVisible:{defaultValue:null,description:"",name:"_peerFocusVisible",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupActive:{defaultValue:null,description:"",name:"_groupActive",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerActive:{defaultValue:null,description:"",name:"_peerActive",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupDisabled:{defaultValue:null,description:"",name:"_groupDisabled",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerDisabled:{defaultValue:null,description:"",name:"_peerDisabled",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupInvalid:{defaultValue:null,description:"",name:"_groupInvalid",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerInvalid:{defaultValue:null,description:"",name:"_peerInvalid",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupChecked:{defaultValue:null,description:"",name:"_groupChecked",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerChecked:{defaultValue:null,description:"",name:"_peerChecked",required:!1,type:{name:"SystemStyleObject | undefined"}},_groupFocusWithin:{defaultValue:null,description:"",name:"_groupFocusWithin",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerFocusWithin:{defaultValue:null,description:"",name:"_peerFocusWithin",required:!1,type:{name:"SystemStyleObject | undefined"}},_peerPlaceholderShown:{defaultValue:null,description:"",name:"_peerPlaceholderShown",required:!1,type:{name:"SystemStyleObject | undefined"}},_placeholder:{defaultValue:null,description:"",name:"_placeholder",required:!1,type:{name:"SystemStyleObject | undefined"}},_placeholderShown:{defaultValue:null,description:"",name:"_placeholderShown",required:!1,type:{name:"SystemStyleObject | undefined"}},_fullScreen:{defaultValue:null,description:"",name:"_fullScreen",required:!1,type:{name:"SystemStyleObject | undefined"}},_selection:{defaultValue:null,description:"",name:"_selection",required:!1,type:{name:"SystemStyleObject | undefined"}},_rtl:{defaultValue:null,description:"",name:"_rtl",required:!1,type:{name:"SystemStyleObject | undefined"}},_ltr:{defaultValue:null,description:"",name:"_ltr",required:!1,type:{name:"SystemStyleObject | undefined"}},_mediaDark:{defaultValue:null,description:"",name:"_mediaDark",required:!1,type:{name:"SystemStyleObject | undefined"}},_mediaReduceMotion:{defaultValue:null,description:"",name:"_mediaReduceMotion",required:!1,type:{name:"SystemStyleObject | undefined"}},_dark:{defaultValue:null,description:"",name:"_dark",required:!1,type:{name:"SystemStyleObject | undefined"}},_light:{defaultValue:null,description:"",name:"_light",required:!1,type:{name:"SystemStyleObject | undefined"}},_horizontal:{defaultValue:null,description:"",name:"_horizontal",required:!1,type:{name:"SystemStyleObject | undefined"}},_vertical:{defaultValue:null,description:"",name:"_vertical",required:!1,type:{name:"SystemStyleObject | undefined"}},_open:{defaultValue:null,description:"",name:"_open",required:!1,type:{name:"SystemStyleObject | undefined"}},_closed:{defaultValue:null,description:"",name:"_closed",required:!1,type:{name:"SystemStyleObject | undefined"}},_complete:{defaultValue:null,description:"",name:"_complete",required:!1,type:{name:"SystemStyleObject | undefined"}},_incomplete:{defaultValue:null,description:"",name:"_incomplete",required:!1,type:{name:"SystemStyleObject | undefined"}},_current:{defaultValue:null,description:"",name:"_current",required:!1,type:{name:"SystemStyleObject | undefined"}}}}}catch{}function u(r){const t={h1:"h1",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...o(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Kom i gang/For designere"}),`
+`,e.jsx(c,{as:"bold",fontSize:"2xl",children:"For designere"}),`
+`,e.jsx(t.h1,{id:"kom-i-gang-med-design",children:"Kom i gang med design"}),`
+`,e.jsx(t.p,{children:`Kartverkets Designsystem skal hjelpe deg med å bygge digitale løsninger og inneholder alt som er felles på tvers av team og produkter.
+Ved å følge designsystemet sikrer vi at alle våre produkter har et enhetlig uttrykk og god brukeropplevelse.
+Nedenfor finner du en liten guide for å komme i gang med design i Kartverket.`}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"bli-kjent",children:"Bli kjent"}),`
+`,e.jsxs(t.p,{children:[`Før du begynner å designe løsninger for Kartverket, bør du bli kjent med vår
+`,e.jsx(n,{href:"https://www.kartverket.no/om-kartverket/kartverkets-identitet",children:"visuelle identitet"}),`
+for å lære mer om Kartverkets mål, visjon og designprinsipper.`]}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"figma",children:"Figma"}),`
+`,e.jsx(t.p,{children:`Vi bruker Figma som hovedverktøy. Opprett en Figmabruker når du har fått tildelt e-post fra Kartverket.
+Dersom du ønsker Editor-tilgang kan du sende en forespørsel via Figma eller ta kontakt med @Wanni på slack, eller mail @Wanwisa Wanpake Johnsrud.`}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Full editor-tilgang:"})," Primært for designere. Begrenset antall lisenser."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Dev-mode:"})," For utviklere som vil jobbe direkte ut fra skissene."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"Viewer-restricted:"})," For utviklere og andre som ønsker å se skissene."]}),`
+`]}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"opprett-prosjekt",children:"Opprett prosjekt"}),`
+`,e.jsx(t.p,{children:"Bruk vår cover template når du oppretter et nytt prosjekt i Figma."}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsxs(t.p,{children:[`Kopier template til ditt prosjekt. Denne finner du i
+`,e.jsx(n,{href:"https://design.kartverket.no",isExternal:!1,children:"Kartverkets Designsystem"}),"."]}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Legg til prosjektnavn og eier av prosjektet"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(t.p,{children:"Høyreklikk på cover og trykk “set as thumbnail”"}),`
+`]}),`
+`]}),`
+`,e.jsx(l,{src:"assets/FigmaOpprettProsjekt.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"aktiver-biblioteket",children:"Aktiver biblioteket"}),`
+`,e.jsxs(t.ol,{children:[`
+`,e.jsx(t.li,{children:"Gå til panelet på venstre siden trykk på assets"}),`
+`,e.jsx(t.li,{children:"Åpne Libraires"}),`
+`,e.jsx(t.li,{children:"Huk av på Kartverkets Designsystem"}),`
+`,e.jsx(t.li,{children:"Nå er det bare å ta i bruk de komponentene du trenger til dine skisser"}),`
+`]}),`
+`,e.jsx(l,{src:"assets/FigmaAktiverBibliotek.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"komponentbiblioteket",children:"Komponentbiblioteket"}),`
+`,e.jsx(t.p,{children:`Kartverket sitt bibliotek i Figma er en fil med alle våre UI-komponenter som er klare til bruk.
+Ved å koble seg til biblioteket får du tilgang til universale komponenter som farger, typografi, knapper, inputfelter, navigasjon og lignende.
+Slik kommer du raskt og effektivt i gang med å sette opp brukergrensensitt til ditt prosjekt.`}),`
+`,e.jsx(l,{src:"assets/FigmaKomponentbiblioteket.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"design-i-figma",children:"Design i Figma"}),`
+`,e.jsxs(t.p,{children:[`Før du setter i gang er det et par ting som er verdt å tenke på.
+Når du har aktivert biblioteket har du nå tilgang til en rekke stiler i designsystemet (design tokens) som vi ønsker at dere bruker,
+istedenfor egendefinerte verdier på f.eks farger og typografi.
+Å bruke
+`,e.jsx(n,{href:"https://help.figma.com/hc/en-us/articles/15339657135383-Guide-to-variables-in-Figma",children:"variables"}),`
+gjør det enkelt å oppdatere hele systemet, og vil også gjøre at du benytter deg av korrekte farger.`]}),`
+`,e.jsx(l,{src:"assets/FigmaVariables.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(t.p,{children:`Kartverket sine hovedfarger er grønn og blå og er lagt inn som to “themes” i Figma.
+Dette gjør det enkelt å f.eks skifte fargene på alle komponenter i et større designelement.`}),`
+`,e.jsx(l,{src:"assets/FigmaThemes.svg",width:"926px",margin:"auto"}),`
+`,e.jsxs(t.p,{children:[`Komponentene er satt opp med
+`,e.jsx(n,{href:"https://help.figma.com/hc/en-us/articles/5731482952599-Add-auto-layout-to-a-design",children:"autolayout"}),`
+og
+`,e.jsx(n,{href:"https://help.figma.com/hc/en-us/articles/360056440594-Create-and-use-variants",children:"variants"}),`
+Vi anbefaler alle som bruker Figma regelmessig å gjøre seg kjent med AutoLayout, dette vil redusere tiden du bruker på å lage detaljerte skisser.`]}),`
+`,e.jsx(l,{src:"assets/FigmaAutolayout.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"nyttige-lenker",children:"Nyttige lenker"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(n,{href:"https://design.kartverket.no",children:"Storybook"}),": vår dokumentasjonsside, hvor du finner alt som tilhører vårt designsystem."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(n,{href:"https://kartverket.atlassian.net/jira/software/projects/KOMP/boards/80?atlOrigin=eyJpIjoiYjlmOGFmN2E5ODkwNGNmMjhjYTZkNTRhOTVlZGYwMDciLCJwIjoiaiJ9",children:"Jira"}),": vi bruker Jira til å legge opp oppgaver for hele teamet."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(n,{href:"https://miro.com/app/dashboard/",children:"Miro"}),": vårt samhandlingsverktøy. Benyttes i retro, felles planleging, idemyldring og fasilitering."]}),`
+`,e.jsxs(t.li,{children:[e.jsx(n,{href:"https://github.com/kartverket/kvib",children:"Github"}),": dokumentasjonssiden til designsystemet i Storybook og komponentbiblioteket i React."]}),`
+`,e.jsx(t.li,{children:"Slack:  #gen-designsystem, #designsystem-status, #laug-design"}),`
+`,e.jsxs(t.li,{children:[e.jsx(n,{href:"https://kartverket.sharepoint.com",children:"Innsida"}),": Vi bruker Innsida til å synligjøre designsystemet for hele organisasjonen."]}),`
+`]}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(d,{rightIcon:"chevron_right",mb:"9px",w:"926px",size:"lg",children:"Les mer om hvorda du kan bidra med design"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(d,{rightIcon:"chevron_right",mt:"9px",w:"926px",size:"lg",children:"Les om Universell utforming"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(t.h2,{id:"vil-du-bidra",children:"Vil du bidra?"}),`
+`,e.jsx(t.p,{children:"Vi setter pris på alle tilbakemeldinger, ideer og innspill, så ikke nøl med å ta kontakt."}),`
+`,e.jsx(s,{}),`
+`,e.jsx(i,{mb:"87px"})]})}function se(r={}){const{wrapper:t}={...o(),...r.components};return t?e.jsx(t,{...r,children:e.jsx(u,{...r})}):u(r)}export{se as default};
