4ad7a51458979bec6a814ae9171a299aa42b3d5f
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-29 14:46:01 +0000

    Deploying to gh-pages from @ kartverket/kvib@586578d31d86bf6b85a06bff662fe83c617e27ed 🚀

---
+++ assets/IconButton-BjI56xT-.js
@@ -0,0 +1,17 @@
+import{j as o}from"./extends-CwFRzn3r.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{ae as p,af as n,ag as m}from"./index-BawzhkRu.js";import{I as a,P as e,a as c,b as u,c as d,d as l}from"./IconButton.stories-cu508SjP.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CUnIEOa8.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./IconButton-uz9h808H.js";import"./forward-ref-BWI-Phbn.js";import"./button-lwGmIUkK.js";import"./context-CBKykClo.js";import"./factory-lWLPzty5.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./cx-BDMMs1jM.js";import"./spinner-CCN1jjNR.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./use-style-config-C4sZEIFb.js";import"./omit-QLD0Eizt.js";import"./index-CfyPTyT-.js";import"./hooks-CUXMK5kD.js";import"./use-theme-CBfqdygA.js";import"./define-styles-BSAHv3yx.js";import"./use-merge-refs-Bdxaq0s4.js";import"./attr-sHTFgF0E.js";import"./Icon-D-x3rA5J.js";/* empty css              */import"./h-stack-ecaAvnDk.js";import"./stack-C3a7yBZW.js";import"./responsive-BbUtuhhZ.js";import"./children-2ZXUntH3.js";import"./v-stack-k8hvxFKi.js";import"./stack-divider-Bj3JUBnu.js";import"./ButtonGroup-DIYU158p.js";import"./Button-JOqyI9DN.js";import"./center-Bg6uA6YZ.js";function i(r){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:a}),`
+`,o.jsx(t.h1,{id:"icon-button",children:"Icon Button"}),`
+`,o.jsx(t.p,{children:"Her er en oversikt over utvalget av ikonknapper i Kartverkets Designbibliotek. Disse er blant annet bygget opp ved bruk av design tokens fra biblioteket, eksempelvis farger og ikoner. I dokumentasjonen følger beskrivelse av komponenten og bruksområde, sammen med kodeeksempler."}),`
+`,o.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`import { IconButton } from "@kvib/react";
+`})}),`
+`,o.jsx(t.h2,{id:"props",children:"Props"}),`
+`,o.jsx(n,{of:e}),`
+`,o.jsx(m,{of:e}),`
+`,o.jsx(t.h2,{id:"størrelser",children:"Størrelser"}),`
+`,o.jsx(n,{of:c}),`
+`,o.jsx(t.h2,{id:"varianter",children:"Varianter"}),`
+`,o.jsx(n,{of:u}),`
+`,o.jsx(t.h2,{id:"states",children:"States"}),`
+`,o.jsx(n,{of:d}),`
+`,o.jsx(t.h2,{id:"gruppering-av-iconbuttons-buttongroup",children:"Gruppering av IconButtons (ButtonGroup)"}),`
+`,o.jsx(n,{of:l})]})}function no(r={}){const{wrapper:t}={...s(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{no as default};
