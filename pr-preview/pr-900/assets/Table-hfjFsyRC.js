0a2443c9294a3ef1326c2889fd88fdbd9fe410c3
chrhein <chrhein@users.noreply.github.com>
2024-10-29 14:38:11 +0000

    Deploy preview for PR 900 🛫

---
+++ pr-preview/pr-900/assets/Table-hfjFsyRC.js
@@ -0,0 +1,75 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{useMDXComponents as n}from"./index-DlpNa54Y.js";import{ae as d,af as o,ai as i,ag as c}from"./index-BX8hYPxt.js";import{D as l,F as p,a as T,b as h}from"./Feedback-C8oPMuBk.js";import{T as b,a as x,b as j,P as s}from"./Table.stories-BXWOOR8K.js";import{T as m}from"./text-CypcOzo0.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bvu1xO5Z.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./box-Cs-tJ6EH.js";import"./factory-lWLPzty5.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./image-CU6Rr-ts.js";import"./forward-ref-BWI-Phbn.js";import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./omit-QLD0Eizt.js";import"./simple-grid-Dx05c2KX.js";import"./grid-DbabZME7.js";import"./use-theme-CBfqdygA.js";import"./hooks-CUXMK5kD.js";import"./responsive-BbUtuhhZ.js";import"./heading-CZK5ruZ7.js";import"./use-style-config-C4sZEIFb.js";import"./index-CfyPTyT-.js";import"./cx-BDMMs1jM.js";import"./Card-CjsZQyOM.js";import"./providers-BQbExJqd.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./context-CBKykClo.js";import"./calc-tjRj3nBR.js";import"./css-var-DE0-P1bc.js";import"./pick-DBnSFT3T.js";import"./flex-DUW0W68E.js";import"./Link-B6mGcy8v.js";import"./tr-Ck5ZJHg6.js";const u=`
+<TableContainer>
+  <Table variant="striped" colorScheme="green">
+    <TableCaption>Imperial to metric conversion factors</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td isNumeric>25.4</Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td isNumeric>30.48</Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td isNumeric>0.91444</Td>
+      </Tr>
+    </Tbody>
+    <Tfoot>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Tfoot>
+  </Table>
+</TableContainer>;`,f=`
+<TableContainer>
+  <Table size="sm">
+    <TableCaption>Imperial to metric conversion factors</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td isNumeric>25.4</Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td isNumeric>30.48</Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td isNumeric>0.91444</Td>
+      </Tr>
+    </Tbody>
+    <Tfoot>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Tfoot>
+  </Table>
+</TableContainer>;`;function a(t){const r={code:"code",h1:"h1",p:"p",pre:"pre",...n(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:b}),`
+`,e.jsxs(l,{children:[e.jsx(r.h1,{id:"table",children:"Table"}),e.jsxs(r.p,{children:["Table brukes til å organiserer og vise frem data på en effektiv måte. Den render et ",e.jsx(r.code,{children:"<table>"})," element som standard."]}),e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from "@kvib/react";
+`})}),e.jsx(p,{component:"Table"}),e.jsx(T,{isVertical:!0,title:"Varianter",description:e.jsxs(m,{children:["Table kommer med tre varianter: ",e.jsx(r.code,{children:"simple"}),", ",e.jsx(r.code,{children:"striped"})," og ",e.jsx(r.code,{children:"unstyled"}),". Eksempel på",e.jsx(r.code,{children:"striped"})," table:"]}),story:e.jsxs(e.Fragment,{children:[e.jsx(o,{of:x,sourceState:"none"}),e.jsx(i,{code:u,dark:!0})]})}),e.jsx(T,{isVertical:!0,title:"Størrelser",description:e.jsxs(m,{children:["Table kommer med tre forskjellige størrelser: ",e.jsx(r.code,{children:"sm"}),", ",e.jsx(r.code,{children:"md"})," og ",e.jsx(r.code,{children:"lg"}),". Eksempel på",e.jsx(r.code,{children:"sm"})," table:"]}),story:e.jsxs(e.Fragment,{children:[e.jsx(o,{of:j,sourceState:"none"}),e.jsx(i,{code:f,dark:!0})]})}),e.jsx(h,{light:!0,children:"Props"}),e.jsx(o,{of:s,sourceState:"hidden"}),e.jsx(c,{of:s})]})]})}function de(t={}){const{wrapper:r}={...n(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(a,{...t})}):a(t)}export{de as default};
---
+++ pr-preview/pr-900/assets/entry-preview-BNw46hPe.js
@@ -0,0 +1,1 @@
+import{d as i,m as d,p as n,r as c,a as f}from"./chunk-OOL6AVF7-baj-DpE7.js";import"./iframe-Bvu1xO5Z.js";import"../sb-preview/runtime.js";import"./chunk-H6MOWX77-DTQOW814.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CS4BWtGh.js";export{i as decorators,d as mount,n as parameters,c as render,f as renderToCanvas};
