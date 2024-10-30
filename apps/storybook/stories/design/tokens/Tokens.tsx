e64eec7810c8fdaad8d4556b4d77973b14e1e862
Christian Hein <scheiehein@gmail.com>
2024-10-30 12:09:21 +0100

    Oppdaterer andre dok-sider til å bruke nye komponenter

--- apps/storybook/stories/design/tokens/Tokens.tsx
+++ apps/storybook/stories/design/tokens/Tokens.tsx
@@ -1,6 +1,7 @@
-import { ListItem, Text, UnorderedList } from "@kvib/react/src";
 import { Dokumentasjonsside } from "../../documentation/utils/Dokumentasjonsside";
 import { Feedback } from "../../documentation/utils/Feedback";
+import { Liste } from "../../documentation/utils/Liste";
+import { Listeelement } from "../../documentation/utils/Listeelement";
 import { Tekstblokk } from "../../documentation/utils/Tekstblokk";
 
 export const Tokens = () => (
@@ -9,32 +10,22 @@ export const Tokens = () => (
     beskrivelse="Design tokens er fundamentale byggeklosser i et designsystem, som definerer stil og designprinsipper vår designsystem. De fungerer som en felles kilde mellom design og utvikling, og sikrer at både designere og utviklere følger de samme retningslinjene."
   >
     <Tekstblokk tittel="Fordeler med design tokens">
-      <Text fontSize={"lg"} fontWeight={400} m="0" p="0">
-        Tokens gjør det lettere å implementere endringer på en konsistent måte. For eksempel, når vi ønsker å oppdatere
-        en farge eller justere typografi, kan vi enkelt gjøre dette i tokenene, og endringene vil automatisk reflekteres
-        i alle komponentene som bruker dem.
-      </Text>
-      <Text fontSize={"lg"} fontWeight={400} m="0" p="0" mt="1.5rem">
-        Vi har også etablert variabler som er tema-baserte. Dette betyr at vi tar utgangspunkt i hovedfargene til
-        Kartverket, som er grønn og blå, for å forenkle tilpasningen av stiler i skissene dine.
-      </Text>
+      Tokens gjør det lettere å implementere endringer på en konsistent måte. For eksempel, når vi ønsker å oppdatere en
+      farge eller justere typografi, kan vi enkelt gjøre dette i tokenene, og endringene vil automatisk reflekteres i
+      alle komponentene som bruker dem.
+      <br />
+      <br />
+      Vi har også etablert variabler som er tema-baserte. Dette betyr at vi tar utgangspunkt i hovedfargene til
+      Kartverket, som er grønn og blå, for å forenkle tilpasningen av stiler i skissene dine.
     </Tekstblokk>
 
     <Tekstblokk tittel="Eksempler på design tokens">
-      <UnorderedList pl="1.75rem" m="0">
-        <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-          <b>Farger:</b> Primær- og sekundærfarger.
-        </ListItem>
-        <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-          <b>Typografi:</b> Skrifttyper, størrelser og linjehøyder.
-        </ListItem>
-        <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-          <b>Avstander:</b> Marginer og paddinger.
-        </ListItem>
-        <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-          <b>Former:</b> Radiuser, skygger og andre visuelle effekter.
-        </ListItem>
-      </UnorderedList>
+      <Liste>
+        <Listeelement tittel="Farger">Primær- og sekundærfarger.</Listeelement>
+        <Listeelement tittel="Typografi">Skrifttyper, størrelser og linjehøyder.</Listeelement>
+        <Listeelement tittel="Avstander">Marginer og paddinger.</Listeelement>
+        <Listeelement tittel="Former">Radiuser, skygger og andre visuelle effekter.</Listeelement>
+      </Liste>
     </Tekstblokk>
 
     <Feedback />
--- apps/storybook/stories/documentation/designere/bidra.tsx
+++ apps/storybook/stories/documentation/designere/bidra.tsx
@@ -1,6 +1,7 @@
-import { ListItem, Text, UnorderedList } from "@kvib/react/src";
 import { Dokumentasjonsside } from "../utils/Dokumentasjonsside";
 import { Feedback } from "../utils/Feedback";
+import { Liste } from "../utils/Liste";
+import { Listeelement } from "../utils/Listeelement";
 import { Tekstblokk } from "../utils/Tekstblokk";
 
 export const BidraMedDesign = () => (
@@ -10,47 +11,38 @@ export const BidraMedDesign = () => (
     grensesnitt som følger Kartverkets visuelle profil og oppfyller kravene til universell utforming."
   >
     <Tekstblokk tittel="Samarbeid">
-      <Text fontSize={"lg"} fontWeight={400} m="0" p="0">
-        Designsystemet er et levende produkt som kontinuerlig oppdateres og vedlikeholdes. Alle som ønsker å påvirke
-        utviklingen eller gi tilbakemeldinger, er velkomne til å bidra. Målet er at designsystemet skal forbedre
-        produktutviklingen hos Kartverket. I stedet for at Team Designsystem skal fungere som et bestillingssystem,
-        ønsker vi å oppmuntre til godt samarbeid og aktiv deltakelse fra alle produktteamene.
-      </Text>
+      Designsystemet er et levende produkt som kontinuerlig oppdateres og vedlikeholdes. Alle som ønsker å påvirke
+      utviklingen eller gi tilbakemeldinger, er velkomne til å bidra. Målet er at designsystemet skal forbedre
+      produktutviklingen hos Kartverket. I stedet for at Team Designsystem skal fungere som et bestillingssystem, ønsker
+      vi å oppmuntre til godt samarbeid og aktiv deltakelse fra alle produktteamene.
     </Tekstblokk>
 
     <Tekstblokk tittel="Sandkassen i Figma">
-      <Text fontSize={"lg"} fontWeight={400} m="0" p="0">
-        I et designsystem er samarbeid og deling av ideer nøkkelen til suksess. Figma-sandkassen gir alle muligheten til
-        å bidra med sine innspill og inspirasjon for å skape et mer helhetlig system. I sandkassen kan alle dele sine
-        forslag til hva de ønsker skal være en del av designsystemet. Det er også et sted for å dele skisser, som kan
-        være til inspirasjon for andre designere.
-      </Text>
+      I et designsystem er samarbeid og deling av ideer nøkkelen til suksess. Figma-sandkassen gir alle muligheten til å
+      bidra med sine innspill og inspirasjon for å skape et mer helhetlig system. I sandkassen kan alle dele sine
+      forslag til hva de ønsker skal være en del av designsystemet. Det er også et sted for å dele skisser, som kan være
+      til inspirasjon for andre designere.
     </Tekstblokk>
 
     <Tekstblokk tittel="Foreslå nytt design eller forbedringer">
-      <Text fontSize={"lg"} fontWeight={400} m="0" p="0">
-        Kartverkets designsystem utvikles og forbedres kontinuerlig gjennom flere tilnærminger. Når nye komponenter
-        foreslås, gjennomgår vi en vurderingsprosess der vi ser på problemet komponenten skal løse, samt verdien den
-        tilfører. Vi vurderer også om komponenten samsvarer med designprinsippene, om den kan gjøres fleksibel nok, og
-        om den passer inn i helheten.
-        <UnorderedList>
-          <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-            <b>Enkle endringer som flere team trenger:</b> Når flere team etterspør små justeringer eller nye
-            komponenter, følger vi opp ved å designe komponenten i Figma og legge den til i komponentbiblioteket, klar
-            til bruk.
-          </ListItem>
-          <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-            <b>Komponenter som er spesifikke for ett team:</b> Hvis en komponent kun er relevant for ett team,
-            oppfordrer vi teamet til å utvikle den lokalt. Vi følger likevel med for å se om lignende behov oppstår hos
-            andre produktteam.
-          </ListItem>
-          <ListItem fontSize={"lg"} fontWeight={400} m="0" p="0">
-            <b>Avanserte endringer eller nye komponenter som flere team trenger:</b> For større endringer eller nye
-            komponenter som er relevante for flere team, samarbeider vi med produktteamene for å utvikle en felles
-            løsning som dekker behovene på tvers av alle team.
-          </ListItem>
-        </UnorderedList>
-      </Text>
+      Kartverkets designsystem utvikles og forbedres kontinuerlig gjennom flere tilnærminger. Når nye komponenter
+      foreslås, gjennomgår vi en vurderingsprosess der vi ser på problemet komponenten skal løse, samt verdien den
+      tilfører. Vi vurderer også om komponenten samsvarer med designprinsippene, om den kan gjøres fleksibel nok, og om
+      den passer inn i helheten.
+      <Liste>
+        <Listeelement tittel="Enkle endringer som flere team trenger">
+          Når flere team etterspør små justeringer eller nye komponenter, følger vi opp ved å designe komponenten i
+          Figma og legge den til i komponentbiblioteket, klar til bruk.
+        </Listeelement>
+        <Listeelement tittel="Komponenter som er spesifikke for ett team">
+          Hvis en komponent kun er relevant for ett team, oppfordrer vi teamet til å utvikle den lokalt. Vi følger
+          likevel med for å se om lignende behov oppstår hos andre produktteam.
+        </Listeelement>
+        <Listeelement tittel="Avanserte endringer eller nye komponenter som flere team trenger">
+          For større endringer eller nye komponenter som er relevante for flere team, samarbeider vi med produktteamene
+          for å utvikle en felles løsning som dekker behovene på tvers av alle team.
+        </Listeelement>
+      </Liste>
     </Tekstblokk>
 
     <Feedback />
