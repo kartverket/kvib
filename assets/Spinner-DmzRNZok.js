c9053d78a0fed5b6f0ba63bb51762a51ab24db47
chrhein <31273371+chrhein@users.noreply.github.com>
2024-10-30 12:19:05 +0000

    Deploying to gh-pages from @ kartverket/kvib@818b69cf46228a99c0089f5e7d5727b18f282260 🚀

---
+++ assets/Spinner-DmzRNZok.js
@@ -0,0 +1,33 @@
+import{j as e}from"./extends-CwFRzn3r.js";import{useMDXComponents as m}from"./index-DlpNa54Y.js";import{ae as a,af as s,ah as t,ag as d}from"./index-I8hmiOKM.js";import{D as i,F as c,a as g,b as o}from"./Feedback-C8oPMuBk.js";import{S as j,a as h,b as x,P as l}from"./Spinner.stories-CHnuOFkW.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-S8uZIyLZ.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./box-Cs-tJ6EH.js";import"./factory-lWLPzty5.js";import"./emotion-styled.browser.esm-NI0tOKWg.js";import"./image-CU6Rr-ts.js";import"./forward-ref-BWI-Phbn.js";import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./omit-QLD0Eizt.js";import"./simple-grid-Dx05c2KX.js";import"./grid-DbabZME7.js";import"./use-theme-CBfqdygA.js";import"./hooks-CUXMK5kD.js";import"./responsive-BbUtuhhZ.js";import"./heading-CZK5ruZ7.js";import"./use-style-config-C4sZEIFb.js";import"./index-CfyPTyT-.js";import"./cx-BDMMs1jM.js";import"./text-CypcOzo0.js";import"./Card-CjsZQyOM.js";import"./providers-BQbExJqd.js";import"./emotion-react.browser.esm-Hqbkbmab.js";import"./context-CBKykClo.js";import"./calc-tjRj3nBR.js";import"./css-var-DE0-P1bc.js";import"./pick-DBnSFT3T.js";import"./flex-DUW0W68E.js";import"./Link-B6mGcy8v.js";import"./spinner-CCN1jjNR.js";import"./stack-C3a7yBZW.js";import"./children-2ZXUntH3.js";function p(n){const r={code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:j}),`
+`,e.jsx(r.h1,{id:"spinner",children:"Spinner"}),`
+`,e.jsx(r.p,{children:"Spinner gir et visuelt signal om at en oppgave prosseseres og vil gi en endring eller et resultat."}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Spinner } from "@kvib/react";
+`})}),`
+`,e.jsxs(i,{children:[e.jsx(r.p,{children:e.jsx(r.strong,{children:"Egnet til:"})}),e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Vise lasting eller ubestemt ventetid"}),`
+`]}),`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Indikere lasting som tar mer enn ett sekund"}),`
+`]}),`
+`]}),e.jsx(r.p,{children:e.jsx(r.strong,{children:"Uegnet til:"})}),e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Vise framgang i prosesser"}),`
+`]}),`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Indikere lasting kortere enn ett sekund"}),`
+`]}),`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Indikere over 5 minutters ventetid"}),`
+`]}),`
+`]}),e.jsx(c,{component:"Spinner"})]}),`
+`,e.jsx(i,{children:e.jsx(g,{title:"Utseende",description:`Fargene burde bli valgt med tanke på kontrast. Velg size og thickness slik at den gir mening til komponentet eller innholdet som skal lastes..
+Farten kan justeres for å gjenspeile estimert ventetid. `,story:e.jsxs(s,{children:[e.jsx(t,{of:h}),e.jsx(t,{of:x})]})})}),`
+`,e.jsx(o,{light:!0,children:"Retningslinjer"}),`
+`,e.jsxs(i,{children:[e.jsx(r.h3,{id:"spinner-vs-progress",children:"Spinner vs. Progress"}),e.jsx(r.p,{children:`Progress burde velges over spinner hvis man kan se prosentvis framgang, som f.eks. upload/download av filer, eller skjema med flere steg
+(Der kan stepper vurderes også). Spinner burde brukes over Progress uansett, så lenge ventetiden er minimal.`}),e.jsx(r.h3,{id:"ventetid",children:"Ventetid"}),e.jsx(r.p,{children:`Om bruker må vente, vil spinner bidra til å redusere usikkerhet. Det tar ikke lang tid før folk blir
+utålmodige og/eller forvirra over at ingenting skjer. Hvis ventetiden overstiger 9 sekunder, bør bruker se en
+forklaring sammen med Progress, og tekst som forklarer hva maskinen jobber med.`})]}),`
+`,e.jsx(o,{light:!0,children:"Props"}),`
+`,e.jsx(s,{of:l}),`
+`,e.jsx(d,{of:l})]})}function me(n={}){const{wrapper:r}={...m(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(p,{...n})}):p(n)}export{me as default};
