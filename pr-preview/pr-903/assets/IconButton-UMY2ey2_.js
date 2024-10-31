a0e45b277ffef5f78f401ebf60a2fd8820b7b960
chrhein <chrhein@users.noreply.github.com>
2024-10-31 12:50:04 +0000

    Deploy preview for PR 903 🛫

---
+++ pr-preview/pr-903/assets/IconButton-UMY2ey2_.js
@@ -0,0 +1,17 @@
+import{j as o}from"./extends-CwFRzn3r.js";import{useMDXComponents as s}from"./index-DlpNa54Y.js";import{ae as p,af as n,ag as m}from"./index-D7Kl20qE.js";import{I as a,P as e,a as c,b as u,c as d,d as l}from"./IconButton.stories-DNvE_rb1.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DxPpY7qe.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./IconButton-Bj25AnSx.js";import"./forward-ref-BWI-Phbn.js";import"./button-CcUc8H6T.js";import"./context-CBKykClo.js";import"./factory-DBKMNJMh.js";import"./emotion-styled.browser.esm-KUABWefE.js";import"./emotion-element-5486c51c.browser.esm-xLAo67gU.js";import"./cx-BDMMs1jM.js";import"./spinner-CI5qPk1o.js";import"./emotion-react.browser.esm-0Zac8NuU.js";import"./use-style-config-B2siLCbt.js";import"./omit-QLD0Eizt.js";import"./index-CfyPTyT-.js";import"./hooks-Dk7VeZ_r.js";import"./use-theme-CeOC8r1f.js";import"./define-styles-BSAHv3yx.js";import"./use-merge-refs-Bdxaq0s4.js";import"./attr-sHTFgF0E.js";import"./Icon-AWJq4l__.js";/* empty css              */import"./h-stack-DtOTlfD9.js";import"./stack-BdCvkUuU.js";import"./responsive-CwLgsqRj.js";import"./children-2ZXUntH3.js";import"./v-stack-D7XFool3.js";import"./stack-divider-BE2Yhewy.js";import"./ButtonGroup-CPfUJfpb.js";import"./Button-yx5TWCfg.js";import"./center-Ds0pdlwh.js";function i(r){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:a}),`
+`,o.jsx(t.h1,{id:"icon-button",children:"Icon Button"}),`
+`,o.jsx(t.p,{children:"Her er en oversikt over utvalget av ikonknapper i Kartverkets Designbibliotek. Disse er blant annet bygget opp ved bruk av design tokens fra biblioteket, eksempelvis farger og ikoner. I dokumentasjonen følger beskrivelse av komponenten og bruksområde, sammen med kodeeksempler."}),`
+`,o.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`import { IconButton } from "@kvib/react";
+`})}),`
+`,o.jsx(t.h2,{id:"props",children:"Props"}),`
+`,o.jsx(n,{of:e}),`
+`,o.jsx(m,{of:e}),`
+`,o.jsx(t.h2,{id:"størrelser",children:"Størrelser"}),`
+`,o.jsx(n,{of:c}),`
+`,o.jsx(t.h2,{id:"varianter",children:"Varianter"}),`
+`,o.jsx(n,{of:u}),`
+`,o.jsx(t.h2,{id:"states",children:"States"}),`
+`,o.jsx(n,{of:d}),`
+`,o.jsx(t.h2,{id:"gruppering-av-iconbuttons-buttongroup",children:"Gruppering av IconButtons (ButtonGroup)"}),`
+`,o.jsx(n,{of:l})]})}function eo(r={}){const{wrapper:t}={...s(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{eo as default};
---
+++ pr-preview/pr-903/assets/Toast-OblwgAU3.js
@@ -0,0 +1,39 @@
+import{j as t}from"./extends-CwFRzn3r.js";import{useMDXComponents as a}from"./index-DlpNa54Y.js";import{ae as l,ah as i,ai as e,ag as c}from"./index-D7Kl20qE.js";import{D as d}from"./Feedback-oujG440w.js";import{T as x,a as u,b as h,P as n}from"./Toast.stories-oFPx_UAP.js";import{C as s,a as p}from"./Card-jUBPBQ-A.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DxPpY7qe.js";import"../sb-preview/runtime.js";import"./index-CS4BWtGh.js";import"./index-D-8MO0q_.js";import"./assertThisInitialized-DSoh8oXT.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DF3CPj03.js";import"./index-DrFu-skq.js";import"./box-CwIaBbUI.js";import"./factory-DBKMNJMh.js";import"./emotion-styled.browser.esm-KUABWefE.js";import"./emotion-element-5486c51c.browser.esm-xLAo67gU.js";import"./image-GsZWH-VT.js";import"./forward-ref-BWI-Phbn.js";import"./use-image-rNdzB_Ea.js";import"./use-safe-layout-effect-BnICyLBj.js";import"./omit-QLD0Eizt.js";import"./simple-grid-ZnRIKZLi.js";import"./grid-BNFEBO0r.js";import"./use-theme-CeOC8r1f.js";import"./hooks-Dk7VeZ_r.js";import"./responsive-CwLgsqRj.js";import"./heading-05dRngNe.js";import"./use-style-config-B2siLCbt.js";import"./index-CfyPTyT-.js";import"./cx-BDMMs1jM.js";import"./text-D14Jyc6W.js";import"./flex-v5DFvFs4.js";import"./Link-D6DR4S49.js";import"./use-toast-Cxhsjrgt.js";import"./toast.provider-Ccs5t8hm.js";import"./proxy-NyFW0ziH.js";import"./use-update-effect-CNg9DBIF.js";import"./use-callback-ref-DA6UG1mJ.js";import"./alert-icon-DyJ3kpUV.js";import"./define-styles-BSAHv3yx.js";import"./icon-BmokjFuu.js";import"./spinner-CI5qPk1o.js";import"./emotion-react.browser.esm-0Zac8NuU.js";import"./context-CBKykClo.js";import"./alert-title-C6vt8Fh3.js";import"./alert-description-Ce3B-u4s.js";import"./close-button-C2STa05D.js";import"./portal-BlJtTGys.js";import"./index-BMuBlvl2.js";import"./stack-BdCvkUuU.js";import"./children-2ZXUntH3.js";import"./h-stack-DtOTlfD9.js";import"./center-Ds0pdlwh.js";import"./Button-yx5TWCfg.js";import"./button-CcUc8H6T.js";import"./use-merge-refs-Bdxaq0s4.js";import"./attr-sHTFgF0E.js";import"./Icon-AWJq4l__.js";/* empty css              */import"./providers-BmY41TRh.js";import"./calc-C4XOv3Qc.js";import"./css-var-DE0-P1bc.js";import"./pick-DBnSFT3T.js";const j=`
+const ToastExample = (props) => {
+const toast = useToast()
+  
+  return (
+    <Button
+      colorScheme="green"
+      onClick={() => toast({...props})
+      }
+    >
+    Trykk for tilbakemelding
+    </Button>
+)
+<ToastExample
+    title="Skjema innsendt"
+    description="Takk for opplysningene"
+    status="success"
+    duration=9000
+    icon="check">
+</ToastExample>
+`,k=`
+<ToastExample
+    duration={4000}
+    title="Error"
+    description="Noe gikk galt"
+    status="error"
+    >Trykk for toast
+</ToastExample>
+`,f=`
+<ToastExample
+    position="top-left"
+    title="Top-left"
+    duration="5000"
+    >Top-left
+</ToastExample>
+`;function m(r){const o={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...a(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(l,{of:x}),`
+`,t.jsxs(d,{children:[t.jsx(o.h1,{id:"toast",children:"Toast"}),t.jsx(o.p,{children:`Toast brukes for å gi brukeren tilbakemelding etter en handling. Toast skal forsvinne enten når den blir lukket,
+eller av seg selv etterhvert. Flere toasts kan være på skjermen samtidig.`}),t.jsxs(o.p,{children:["På ",t.jsx(o.a,{href:"https://chakra-ui.com/docs/components/toast",rel:"nofollow",children:"Chakra-ui.com"})," finner du noen niche brukseksempler."]}),t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`import { useToast } from "@kvib/react";
+`})}),t.jsx(o.h2,{id:"status",children:"Status"}),t.jsx(s,{variant:"outline",children:t.jsx(p,{children:t.jsx(i,{of:u})})}),t.jsx(e,{code:k,dark:!0}),t.jsx(o.h2,{id:"positions",children:"Positions"}),t.jsx(s,{variant:"outline",children:t.jsx(p,{children:t.jsx(i,{of:h})})}),t.jsx(e,{code:f,dark:!0}),t.jsx(o.h2,{id:"props",children:"Props"}),t.jsx(s,{variant:"outline",children:t.jsx(p,{children:t.jsx(i,{of:n})})}),t.jsx(e,{code:j,dark:!0}),t.jsx(c,{of:n})]})]})}function Ft(r={}){const{wrapper:o}={...a(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(m,{...r})}):m(r)}export{Ft as default};
