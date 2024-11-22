d909b5990a61bc04875a000a817ad6a21d975d0c
chrhein <chrhein@users.noreply.github.com>
2024-11-22 17:44:59 +0000

    Deploy preview for PR 912 🛫

--- pr-preview/pr-912/assets/IconButton-FADoYxsn.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as i}from"./index-BRkUXrbv.js";import{ae as s,af as m,ag as p}from"./index-BsHmpHtr.js";import{I as a,P as t}from"./IconButton.stories-DpbP2bCp.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-bb1pd3wT.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./IconButton-URh82nUA.js";import"./icon-button-CQVMxZrp.js";import"./button-Cbvbv_aU.js";import"./create-recipe-context-AlCFSakd.js";import"./factory-Dr_T95xI.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";import"./absolute-center-CNroa-V3.js";import"./spinner-DP07Pf0W.js";import"./Icon-Clsndq3d.js";/* empty css              */function n(r){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:a}),`
-`,e.jsx(o.h1,{id:"icon-button",children:"Icon Button"}),`
-`,e.jsx(o.p,{children:"Her er en oversikt over utvalget av ikonknapper i Kartverkets Designbibliotek. Disse er blant annet bygget opp ved bruk av design tokens fra biblioteket, eksempelvis farger og ikoner. I dokumentasjonen følger beskrivelse av komponenten og bruksområde, sammen med kodeeksempler."}),`
-`,e.jsx(o.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`import { IconButton } from "@kvib/react";
-`})}),`
-`,e.jsx(o.h2,{id:"props",children:"Props"}),`
-`,e.jsx(m,{of:t}),`
-`,e.jsx(p,{of:t})]})}function F(r={}){const{wrapper:o}={...i(),...r.components};return o?e.jsx(o,{...r,children:e.jsx(n,{...r})}):n(r)}export{F as default};
--- pr-preview/pr-912/assets/Portal-BUjefqy8.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as i}from"./index-BRkUXrbv.js";import{ae as s,af as a,ag as m}from"./index-BsHmpHtr.js";import{P as p,a as e}from"./Portal.stories-BSOalMAI.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-bb1pd3wT.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./portal-6abwbB-C.js";import"./index-DXXQsaeK.js";import"./use-environment-context-DqdFjJyw.js";import"./create-context-DPShtm6F.js";function n(o){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:p}),`
-`,r.jsx(t.h1,{id:"portal",children:"Portal"}),`
-`,r.jsx(t.p,{children:"Portal bruker ReactDOM.createPortal-APIet for å legge til et element på slutten av document.body eller en spesifikk container."}),`
-`,r.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,r.jsx(t.pre,{children:r.jsx(t.code,{className:"language-jsx",children:`import { Portal } from "@kvib/react";
-`})}),`
-`,r.jsx(t.h2,{id:"props",children:"Props"}),`
-`,r.jsx(a,{of:e}),`
-`,r.jsx(m,{of:e})]})}function X(o={}){const{wrapper:t}={...i(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(n,{...o})}):n(o)}export{X as default};
