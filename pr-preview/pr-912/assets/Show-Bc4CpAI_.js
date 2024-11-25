86e92855541e2010ecb5256cd21d7be4c83b7df2
chrhein <chrhein@users.noreply.github.com>
2024-11-25 11:22:27 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Show-Bc4CpAI_.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as n}from"./index-BRkUXrbv.js";import{ae as m,af as p,ag as s}from"./index-r_TlHl_k.js";import{S as a,P as e}from"./Show.stories-B1xKfjVO.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BlfnZkop.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./show-DskmVrvv.js";import"./Button-Crc1h0hb.js";import"./button-Cbvbv_aU.js";import"./create-recipe-context-AlCFSakd.js";import"./factory-Dr_T95xI.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";import"./absolute-center-CNroa-V3.js";import"./spinner-DP07Pf0W.js";import"./span-DwCpyh4Z.js";import"./center-7hWxgEBL.js";import"./h-stack-CjVo9nnl.js";import"./stack-Bh3oWNRR.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./Icon-Clsndq3d.js";/* empty css              */import"./text-DJW8V014.js";import"./box-C9nK2UO9.js";function i(t){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...n(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(m,{of:a}),`
+`,r.jsx(o.h1,{id:"show",children:"Show"}),`
+`,r.jsx(o.p,{children:"Show er en wrapperkomponent som render eller ikke render child-elementer hvis en gitt betingelse er sann."}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-jsx",children:`
+## Ta i bruk
+
+\`\`\`jsx
+import { Show } from "@kvib/react";
+`})}),`
+`,r.jsx(o.h2,{id:"props",children:"Props"}),`
+`,r.jsx(p,{of:e}),`
+`,r.jsx(s,{of:e})]})}function B(t={}){const{wrapper:o}={...n(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(i,{...t})}):i(t)}export{B as default};
