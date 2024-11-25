04002ef3f1a9309fa09b84c5b107d46fa8854f06
chrhein <chrhein@users.noreply.github.com>
2024-11-25 10:09:54 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Code-xYmIPCQT.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as m}from"./index-BRkUXrbv.js";import{ae as a,af as t,ag as d}from"./index-JDlEWREV.js";import{D as s,F as p,a as c,b as x}from"./Feedback-DT-gPCEI.js";import{C as j,a as h,P as n}from"./Code.stories-O4m6sC-r.js";import{B as k}from"./box-C9nK2UO9.js";import{T as i}from"./text-DJW8V014.js";import{C as g}from"./code-BXkiuTb0.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-T9L_VKZd.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./image-CB2SsSea.js";import"./factory-Dr_T95xI.js";import"./simple-grid-BMz7U_wE.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./grid-BtoyrwbI.js";import"./heading-D7346gVA.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";import"./card-sQbwxR_z.js";import"./create-slot-recipe-context-CYF_0Bn9.js";import"./flex-DKwYg45R.js";import"./Link-CzKHhpgD.js";import"./link-DWtvkXlG.js";import"./stack-Bh3oWNRR.js";function l(o){const r={code:"code",h1:"h1",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:j}),`
+`,e.jsx(r.h1,{id:"code",children:"Code"}),`
+`,e.jsx(r.p,{children:"Code er et komponent som skal brukes for å presentere inline kode."}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Code } from "@kvib/react";
+`})}),`
+`,e.jsxs(s,{children:[e.jsx(r.p,{children:e.jsx(r.strong,{children:"Egnet til:"})}),e.jsxs(r.ul,{children:[`
+`,e.jsx(r.li,{children:"Presentere kode i en tekst"}),`
+`]}),e.jsx(r.p,{children:e.jsx(r.strong,{children:"Uegnet til:"})}),e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:["Varsling av brukeren - må ikke forveksles med ",e.jsx(r.code,{children:"Alert"})]}),`
+`]}),e.jsx(p,{component:"Code"})]}),`
+`,e.jsx(s,{children:e.jsx(c,{title:"Utseende og vektlegging",description:e.jsxs(k,{children:[e.jsx(i,{children:"Code har tre varianter: solid, subtle og outline. Bruk disse for å vektlegge hvor viktig det er at brukeren får med seg kodelinja. Subtle brukes som regel i et avsnitt, mens solid og outline fint kan stå alene."}),e.jsxs(i,{children:["Bruken av colorScheme skal passe kontekst. Det kan også utnyttes til å vektlegge viktighet. For eksempel så vil",e.jsx(g,{children:'variant="solid"'})," med rødt colorscheme lett fange oppmerksomheten til brukeren."]})]}),story:e.jsx(t,{of:h})})}),`
+`,e.jsx(x,{light:!0,children:"Props"}),`
+`,e.jsx(t,{of:n}),`
+`,e.jsx(d,{of:n})]})}function W(o={}){const{wrapper:r}={...m(),...o.components};return r?e.jsx(r,{...o,children:e.jsx(l,{...o})}):l(o)}export{W as default};
---
+++ pr-preview/pr-912/assets/input-CopjlPMb.js
@@ -0,0 +1,1 @@
+import{d as e}from"./config-BzBMZny9.js";const o=e({variants:{outline:{field:{borderColor:"gray.400"}},flushed:{field:{borderColor:"gray.400"}}}});export{o as i};
