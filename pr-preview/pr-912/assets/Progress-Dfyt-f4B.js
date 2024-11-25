e00267e7af933697855d755bf2a17633a381a4f9
chrhein <chrhein@users.noreply.github.com>
2024-11-25 14:26:21 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Progress-Dfyt-f4B.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as n}from"./index-BRkUXrbv.js";import{ae as m,af as p,ag as l}from"./index-CuPk2Mqc.js";import{b as o,D as a}from"./Feedback-CUsScRJr.js";import{P as d,a as t}from"./Progress.stories-Dyypow-4.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CmCyc0pH.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./box-C9nK2UO9.js";import"./factory-Dr_T95xI.js";import"./image-CB2SsSea.js";import"./simple-grid-BMz7U_wE.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./grid-BtoyrwbI.js";import"./heading-D7346gVA.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";import"./text-DJW8V014.js";import"./card-sQbwxR_z.js";import"./create-slot-recipe-context-CYF_0Bn9.js";import"./flex-DKwYg45R.js";import"./Link-DqK-vYp6.js";import"./link-DWtvkXlG.js";import"./toggle-tip-01wGuvyz.js";import"./index-v24uTv2s.js";import"./iconBase-CTl-8GCs.js";import"./portal-B73Xo__z.js";import"./index-51odREz2.js";import"./use-environment-context-DqdFjJyw.js";import"./create-context-DPShtm6F.js";import"./icon-button-CQVMxZrp.js";import"./button-Cbvbv_aU.js";import"./popover-D_nxhwA9.js";import"./split-presence-props-DcTE3k_W.js";import"./create-split-props-u5h9OPvL.js";import"./use-presence-context-B6qRt-aB.js";import"./index-DkshEfrg.js";import"./index-Ctx6eFvH.js";import"./index-Bdoxc4SK.js";import"./use-event-CSfbiBcH.js";import"./index-DI7p0YkP.js";import"./index-BRNWFSEf.js";import"./index-nlDNkuRb.js";import"./floating-ui.dom-BfjzW660.js";import"./focus-trap.esm-D1qb2f5l.js";import"./index-IUjCT-0R.js";import"./index-CcwapMGp.js";import"./index-33_OKIP6.js";import"./use-locale-context-CnLnjqE7.js";import"./factory-BHlL6AhM.js";import"./progress-s8iT_AqN.js";import"./progress-value-text-a4PLof_7.js";import"./index-BqQfDmyD.js";function s(i){const e={code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...n(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(m,{of:d}),`
+`,r.jsx(e.h1,{id:"progress",children:"Progress"}),`
+`,r.jsx(e.p,{children:"Progress brukes til å vise frem status på fremdrift for en oppgave som tar tid eller består av flere steg."}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import { Progress } from "@kvib/react";
+`})}),`
+`,r.jsx(e.p,{children:r.jsx(e.strong,{children:"Egnet til:"})}),`
+`,r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Vise framgang i en prosess"}),`
+`]}),`
+`,r.jsx(e.p,{children:r.jsx(e.strong,{children:"Uegnet til:"})}),`
+`,r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Vise lasting som nesten er umiddelbar"}),`
+`,r.jsx(e.li,{children:"vise lasting for prosesser som ikke er så viktige for brukeropplevelsen"}),`
+`]}),`
+`,r.jsx(o,{light:!0,children:"Retningslinjer"}),`
+`,r.jsxs(a,{children:[r.jsx(e.h3,{id:"progress-vs-spinner",children:"Progress vs. Spinner"}),r.jsx(e.p,{children:`Progress burde velges over spinner hvis man kan se prosentvis framgang, som f.eks. upload/download av filer, eller skjema med flere steg
+(Der kan stepper vurderes også). Hvis ventetiden er minimal, burde man ta spinner istedenfor.`})]}),`
+`,r.jsx(o,{light:!0,children:"Props"}),`
+`,r.jsx(p,{of:t,className:"fullwidth"}),`
+`,r.jsx(l,{of:t})]})}function ur(i={}){const{wrapper:e}={...n(),...i.components};return e?r.jsx(e,{...i,children:r.jsx(s,{...i})}):s(i)}export{ur as default};
