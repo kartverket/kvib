806b8ebccab1b30d723ea1c52753394d8ab8d98e
chrhein <chrhein@users.noreply.github.com>
2024-11-25 10:57:43 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/NumberInput-C4-9w0KB.js
@@ -0,0 +1,15 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as o}from"./index-BRkUXrbv.js";import{ae as p,af as m,ag as s}from"./index-BAvm3b5y.js";import{D as l,b as c}from"./Feedback-DT-gPCEI.js";import{N as a,P as i}from"./NumberInput.stories-BapzYdgJ.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Dk5iRNio.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./box-C9nK2UO9.js";import"./factory-Dr_T95xI.js";import"./image-CB2SsSea.js";import"./simple-grid-BMz7U_wE.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./grid-BtoyrwbI.js";import"./heading-D7346gVA.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";import"./text-DJW8V014.js";import"./card-sQbwxR_z.js";import"./create-slot-recipe-context-CYF_0Bn9.js";import"./flex-DKwYg45R.js";import"./Link-CzKHhpgD.js";import"./link-DWtvkXlG.js";import"./number-input-rPUDxFd5.js";import"./icons-CXhDYdRi.js";import"./create-split-props-u5h9OPvL.js";import"./factory-BHlL6AhM.js";import"./index-DkshEfrg.js";import"./create-context-DPShtm6F.js";import"./index-Bl9vfJtp.js";import"./index-BRNWFSEf.js";import"./index-33_OKIP6.js";import"./index-51odREz2.js";import"./index-DBzJ7FiP.js";import"./index-Bdoxc4SK.js";import"./index-Ctx6eFvH.js";import"./use-environment-context-DqdFjJyw.js";import"./use-locale-context-CnLnjqE7.js";import"./use-event-CSfbiBcH.js";import"./use-field-context-CcEUgBGI.js";function n(t){const e={code:"code",h1:"h1",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...o(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(p,{of:a}),`
+`,r.jsxs(l,{children:[r.jsx(e.h1,{id:"numberinput",children:"NumberInput"}),r.jsx(e.p,{children:"NumberInput ligner på Input-komponenten, men den har kontroller for å øke eller redusere numeriske verdier."}),r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@kvib/react";
+`})}),r.jsx(e.p,{children:r.jsx(e.strong,{children:"Egnet til:"})}),r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Svar"}),`
+`,r.jsx(e.li,{children:"Tall med eller uten interaktive knapper"}),`
+`]}),r.jsx(e.p,{children:r.jsx(e.strong,{children:"Uegnet til:"})}),r.jsxs(e.ul,{children:[`
+`,r.jsxs(e.li,{children:["Søk (bruk heller ",r.jsx(e.code,{children:"Search"})," eller ",r.jsx(e.code,{children:"SearchAsync"}),")"]}),`
+`,r.jsxs(e.li,{children:["Interaktiv datepicker (bruk heller ",r.jsx(e.code,{children:"DatePicker"}),")"]}),`
+`]}),r.jsx(c,{light:!0,children:"Props"}),r.jsx(m,{of:i,className:"sb-unstyled"}),r.jsx(s,{of:i})]})]})}function or(t={}){const{wrapper:e}={...o(),...t.components};return e?r.jsx(e,{...t,children:r.jsx(n,{...t})}):n(t)}export{or as default};
--- pr-preview/pr-912/assets/input-CopjlPMb.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./config-BzBMZny9.js";const o=e({variants:{outline:{field:{borderColor:"gray.400"}},flushed:{field:{borderColor:"gray.400"}}}});export{o as i};
