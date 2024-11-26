1a65a0cc4ac1dade434f72248f656ff920ebeb71
chrhein <chrhein@users.noreply.github.com>
2024-11-26 11:26:44 +0000

    Deploy preview for PR 912 🛫

--- pr-preview/pr-912/assets/Portal.stories-DKwoDhB4.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-sfY7k0Xq.js";import{P as c}from"./portal-B_Lxu86P.js";const d={title:"Komponenter/Portal",component:c,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}},argTypes:{appendToParentPortal:{description:"If true, the portal will check if it is within a parent portal and append itself to the parent's portal node. This provides nesting for portals. If false, the portal will always append to `document.body` regardless of nesting. It is used to opt out of portal nesting.",table:{type:{summary:"boolean"},defaultValue:{summary:"true"}},control:"boolean"},containerRef:{description:"The ref to the component where the portal will be attached to.",table:{type:{summary:"RefObject<HTMLElement | null>"}},control:"object"}}},r={render:t=>o.jsx(e,{...t})},e=({...t})=>o.jsx(c,{...t,children:o.jsx("div",{children:"Portal-innhold"})});var a,n,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: (args: PortalProps) => <PortalExample {...args} />
-}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var l,p,i;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`({
-  ...args
-}) => {
-  return <Portal {...args}>
-      <div>Portal-innhold</div>
-    </Portal>;
-}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};const m=["Preview","PortalExample"],f=Object.freeze(Object.defineProperty({__proto__:null,PortalExample:e,Preview:r,__namedExportsOrder:m,default:d},Symbol.toStringTag,{value:"Module"}));export{f as P,r as a};
---
+++ pr-preview/pr-912/assets/kom-i-gang-Dt82WRUT.js
@@ -0,0 +1,88 @@
+import{j as e}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as p}from"./index-BRkUXrbv.js";import"./chunk-NUUEMKO5-DToT1C_J.js";import{ae as g}from"./index-CcYMoPog.js";import{F as m}from"./flex-DKwYg45R.js";import{L as t}from"./Link-DqK-vYp6.js";import{I as o}from"./Icon-CKFCJN9k.js";import{T as h}from"./text-DJW8V014.js";import{B as i}from"./box-C9nK2UO9.js";import{I as s}from"./image-CB2SsSea.js";import{B as a}from"./Button-BPW1m5nd.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DnPxoUV6.js";import"../sb-preview/runtime.js";import"./react-18-ChRDcVY1.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./factory-Dr_T95xI.js";import"./use-recipe-CWoigbDD.js";import"./link-DWtvkXlG.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";/* empty css              */import"./button-Cbvbv_aU.js";import"./center-7hWxgEBL.js";import"./spinner-DP07Pf0W.js";import"./h-stack-CjVo9nnl.js";import"./stack-Bh3oWNRR.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";const l=n=>e.jsxs(m,{gap:"18px",...n,children:[e.jsxs(t,{href:"mailto:kvib-feedbacks-aaaahwh252gnftmofucpaa47ca@kartverketgroup.slack.com",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(o,{icon:"mail"}),"Send en mail"]}),e.jsxs(t,{href:"https://slack.com/app_redirect?channel=gen-designsystem",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(o,{icon:"tag"}),"Bli med i slack-kanalen"]}),e.jsxs(t,{href:"https://github.com/kartverket/kvib",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(o,{size:"40",icon:"polyline"}),"Lag en pull-request på github"]})]});try{l.displayName="ContributeLinks",l.__docgenInfo={description:"",displayName:"ContributeLinks",props:{}}}catch{}function d(n){const r={h1:"h1",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...p(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(g,{title:"Kom i gang/For designere"}),`
+`,e.jsx(h,{as:"bold",fontSize:"2xl",children:"For designere"}),`
+`,e.jsx(r.h1,{id:"kom-i-gang-med-design",children:"Kom i gang med design"}),`
+`,e.jsx(r.p,{children:`Kartverkets Designsystem skal hjelpe deg med å bygge digitale løsninger og inneholder alt som er felles på tvers av team og produkter.
+Ved å følge designsystemet sikrer vi at alle våre produkter har et enhetlig uttrykk og god brukeropplevelse.
+Nedenfor finner du en liten guide for å komme i gang med design i Kartverket.`}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"bli-kjent",children:"Bli kjent"}),`
+`,e.jsxs(r.p,{children:[`Før du begynner å designe løsninger for Kartverket, bør du bli kjent med vår
+`,e.jsx(t,{href:"https://www.kartverket.no/om-kartverket/kartverkets-identitet",children:"visuelle identitet"}),`
+for å lære mer om Kartverkets mål, visjon og designprinsipper.`]}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"figma",children:"Figma"}),`
+`,e.jsx(r.p,{children:`Vi bruker Figma som hovedverktøy. Opprett en Figmabruker når du har fått tildelt e-post fra Kartverket.
+Dersom du ønsker Editor-tilgang kan du sende en forespørsel via Figma eller ta kontakt med @Wanni på slack, eller mail @Wanwisa Wanpake Johnsrud.`}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"Full editor-tilgang:"})," Primært for designere. Begrenset antall lisenser."]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"Dev-mode:"})," For utviklere som vil jobbe direkte ut fra skissene."]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"Viewer-restricted:"})," For utviklere og andre som ønsker å se skissene."]}),`
+`]}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"opprett-prosjekt",children:"Opprett prosjekt"}),`
+`,e.jsx(r.p,{children:"Bruk vår cover template når du oppretter et nytt prosjekt i Figma."}),`
+`,e.jsxs(r.ol,{children:[`
+`,e.jsxs(r.li,{children:[`
+`,e.jsxs(r.p,{children:[`Kopier template til ditt prosjekt. Denne finner du i
+`,e.jsx(t,{href:"https://design.kartverket.no",isExternal:!1,children:"Kartverkets Designsystem"}),"."]}),`
+`]}),`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Legg til prosjektnavn og eier av prosjektet"}),`
+`]}),`
+`,e.jsxs(r.li,{children:[`
+`,e.jsx(r.p,{children:"Høyreklikk på cover og trykk “set as thumbnail”"}),`
+`]}),`
+`]}),`
+`,e.jsx(s,{src:"assets/FigmaOpprettProsjekt.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"aktiver-biblioteket",children:"Aktiver biblioteket"}),`
+`,e.jsxs(r.ol,{children:[`
+`,e.jsx(r.li,{children:"Gå til panelet på venstre siden trykk på assets"}),`
+`,e.jsx(r.li,{children:"Åpne Libraires"}),`
+`,e.jsx(r.li,{children:"Huk av på Kartverkets Designsystem"}),`
+`,e.jsx(r.li,{children:"Nå er det bare å ta i bruk de komponentene du trenger til dine skisser"}),`
+`]}),`
+`,e.jsx(s,{src:"assets/FigmaAktiverBibliotek.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"komponentbiblioteket",children:"Komponentbiblioteket"}),`
+`,e.jsx(r.p,{children:`Kartverket sitt bibliotek i Figma er en fil med alle våre UI-komponenter som er klare til bruk.
+Ved å koble seg til biblioteket får du tilgang til universale komponenter som farger, typografi, knapper, inputfelter, navigasjon og lignende.
+Slik kommer du raskt og effektivt i gang med å sette opp brukergrensensitt til ditt prosjekt.`}),`
+`,e.jsx(s,{src:"assets/FigmaKomponentbiblioteket.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"design-i-figma",children:"Design i Figma"}),`
+`,e.jsxs(r.p,{children:[`Før du setter i gang er det et par ting som er verdt å tenke på.
+Når du har aktivert biblioteket har du nå tilgang til en rekke stiler i designsystemet (design tokens) som vi ønsker at dere bruker,
+istedenfor egendefinerte verdier på f.eks farger og typografi.
+Å bruke
+`,e.jsx(t,{href:"https://help.figma.com/hc/en-us/articles/15339657135383-Guide-to-variables-in-Figma",children:"variables"}),`
+gjør det enkelt å oppdatere hele systemet, og vil også gjøre at du benytter deg av korrekte farger.`]}),`
+`,e.jsx(s,{src:"assets/FigmaVariables.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(r.p,{children:`Kartverket sine hovedfarger er grønn og blå og er lagt inn som to “themes” i Figma.
+Dette gjør det enkelt å f.eks skifte fargene på alle komponenter i et større designelement.`}),`
+`,e.jsx(s,{src:"assets/FigmaThemes.svg",width:"926px",margin:"auto"}),`
+`,e.jsxs(r.p,{children:[`Komponentene er satt opp med
+`,e.jsx(t,{href:"https://help.figma.com/hc/en-us/articles/5731482952599-Add-auto-layout-to-a-design",children:"autolayout"}),`
+og
+`,e.jsx(t,{href:"https://help.figma.com/hc/en-us/articles/360056440594-Create-and-use-variants",children:"variants"}),`
+Vi anbefaler alle som bruker Figma regelmessig å gjøre seg kjent med AutoLayout, dette vil redusere tiden du bruker på å lage detaljerte skisser.`]}),`
+`,e.jsx(s,{src:"assets/FigmaAutolayout.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"nyttige-lenker",children:"Nyttige lenker"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(t,{href:"https://design.kartverket.no",children:"Storybook"}),": vår dokumentasjonsside, hvor du finner alt som tilhører vårt designsystem."]}),`
+`,e.jsxs(r.li,{children:[e.jsx(t,{href:"https://kartverket.atlassian.net/jira/software/projects/KOMP/boards/80?atlOrigin=eyJpIjoiYjlmOGFmN2E5ODkwNGNmMjhjYTZkNTRhOTVlZGYwMDciLCJwIjoiaiJ9",children:"Jira"}),": vi bruker Jira til å legge opp oppgaver for hele teamet."]}),`
+`,e.jsxs(r.li,{children:[e.jsx(t,{href:"https://miro.com/app/dashboard/",children:"Miro"}),": vårt samhandlingsverktøy. Benyttes i retro, felles planleging, idemyldring og fasilitering."]}),`
+`,e.jsxs(r.li,{children:[e.jsx(t,{href:"https://github.com/kartverket/kvib",children:"Github"}),": dokumentasjonssiden til designsystemet i Storybook og komponentbiblioteket i React."]}),`
+`,e.jsx(r.li,{children:"Slack:  #gen-designsystem, #designsystem-status, #laug-design"}),`
+`,e.jsxs(r.li,{children:[e.jsx(t,{href:"https://kartverket.sharepoint.com",children:"Innsida"}),": Vi bruker Innsida til å synligjøre designsystemet for hele organisasjonen."]}),`
+`]}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(a,{rightIcon:"chevron_right",mb:"9px",w:"926px",size:"lg",children:"Les mer om hvorda du kan bidra med design"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(a,{rightIcon:"chevron_right",mt:"9px",w:"926px",size:"lg",children:"Les om Universell utforming"}),`
+`,e.jsx(i,{py:"41px"}),`
+`,e.jsx(r.h2,{id:"vil-du-bidra",children:"Vil du bidra?"}),`
+`,e.jsx(r.p,{children:"Vi setter pris på alle tilbakemeldinger, ideer og innspill, så ikke nøl med å ta kontakt."}),`
+`,e.jsx(l,{}),`
+`,e.jsx(i,{mb:"87px"})]})}function Y(n={}){const{wrapper:r}={...p(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(d,{...n})}):d(n)}export{Y as default};
