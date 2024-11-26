388cd74a18bb071ed9f4e430eae1c4a7eb2b1632
chrhein <chrhein@users.noreply.github.com>
2024-11-26 13:26:59 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Portal-CG_tcYXj.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as i}from"./index-BRkUXrbv.js";import{ae as m,af as p,ag as s}from"./index-D-zBApB4.js";import{P as a,a as e}from"./Portal.stories-Dyrj0wZJ.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Dm3ZDKCz.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./portal-B_Lxu86P.js";import"./index-C5caIrke.js";import"./use-environment-context-DqdFjJyw.js";import"./create-context-DPShtm6F.js";import"./box-C9nK2UO9.js";import"./factory-Dr_T95xI.js";import"./text-DJW8V014.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";function n(o){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(m,{of:a}),`
+`,r.jsx(t.h1,{id:"portal",children:"Portal"}),`
+`,r.jsx(t.p,{children:"Portal bruker ReactDOM.createPortal-APIet for å legge til et element på slutten av document.body eller en spesifikk container."}),`
+`,r.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,r.jsx(t.pre,{children:r.jsx(t.code,{className:"language-jsx",children:`import { Portal } from "@kvib/react";
+`})}),`
+`,r.jsx(t.h2,{id:"props",children:"Props"}),`
+`,r.jsx(p,{of:e}),`
+`,r.jsx(s,{of:e})]})}function E(o={}){const{wrapper:t}={...i(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(n,{...o})}):n(o)}export{E as default};
---
+++ pr-preview/pr-912/assets/Z-index-DVm-vvXS.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as r}from"./index-BRkUXrbv.js";import"./chunk-NUUEMKO5-GS7wjuqD.js";import{ae as i,ai as s}from"./index-D-zBApB4.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-Dm3ZDKCz.js";import"../sb-preview/runtime.js";import"./react-18-ChRDcVY1.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";const m=`
+export const zIndices = {
+  hide: -1,
+  auto: "auto",
+  base: 0,
+  docked: 10,
+  dropdown: 1000,
+  sticky: 1100,
+  banner: 1200,
+  overlay: 1300,
+  modal: 1400,
+  popover: 1500,
+  skipLink: 1600,
+  toast: 1700,
+  tooltip: 1800,
+};
+`;function e(t){const n={h2:"h2",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Komponenter/Tokens/Z-index"}),`
+`,o.jsx(n.h2,{id:"z-index",children:"Z-index"}),`
+`,o.jsx(s,{code:m,dark:!0})]})}function z(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{z as default};
