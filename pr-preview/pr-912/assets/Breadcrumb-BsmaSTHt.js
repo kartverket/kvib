a580dccb5cdd1af9723581941354e1148219bfcd
chrhein <chrhein@users.noreply.github.com>
2024-11-26 12:43:40 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Breadcrumb-BsmaSTHt.js
@@ -0,0 +1,10 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as i}from"./index-BRkUXrbv.js";import{ae as s,af as m,ag as a}from"./index-C-eDq7uL.js";import{B as p,P as t}from"./Breadcrumb.stories-CSL6Tzte.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DZueWxJB.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./breadcrumb-B1uAdMvL.js";import"./create-slot-recipe-context-CYF_0Bn9.js";import"./factory-Dr_T95xI.js";import"./empty-DSVal0bC.js";import"./icons-CXhDYdRi.js";function n(o){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:p}),`
+`,r.jsx(e.h1,{id:"breadcrumb",children:"Breadcrumb"}),`
+`,r.jsxs(e.p,{children:["Breadcrumb er et navigasjonsmønster som hjelper brukerne å forstå hierarkiet på en nettside. Legg til ",r.jsx(e.code,{children:"ìsCurrentPage"}),"-proppen på ",r.jsx(e.code,{children:"BreacrumbItem"}),` brukeren står i.
+Breadcrumb inneholder `,r.jsx(e.code,{children:"Box"})," så det er mulig å bruke alle style props derfra."]}),`
+`,r.jsx(e.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "@kvib/react";
+`})}),`
+`,r.jsx(e.h2,{id:"props",children:"Props"}),`
+`,r.jsx(m,{of:t}),`
+`,r.jsx(a,{of:t})]})}function L(o={}){const{wrapper:e}={...i(),...o.components};return e?r.jsx(e,{...o,children:r.jsx(n,{...o})}):n(o)}export{L as default};
---
+++ pr-preview/pr-912/assets/LinkOverlay.stories-uG_Vv1Ul.js
@@ -0,0 +1,24 @@
+import{j as e}from"./jsx-runtime-sfY7k0Xq.js";import{L as i,a as p,b as c}from"./index-Djumft7z.js";import{H as v}from"./heading-D7346gVA.js";import{T as k}from"./text-DJW8V014.js";import{B as x}from"./box-C9nK2UO9.js";const b={title:"Komponenter/Link Overlay",component:i,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}}},r={render:s=>e.jsxs(p,{as:"article",maxW:"sm",p:"5",borderWidth:"1px",rounded:"md",children:[e.jsx(v,{size:"md",my:"2",children:e.jsx(i,{...s,href:"#",children:"Denne kan trykkes på"})}),e.jsx(k,{children:"Hele komponenten er en knapp"})]})},n={render:s=>e.jsxs(p,{as:"article",maxW:"sm",p:"5",borderWidth:"1px",rounded:"md",children:[e.jsx(x,{children:"13 timer siden"}),e.jsx(v,{size:"md",my:"2",children:e.jsx(i,{...s,href:"#",children:"Ikke gå deg vill på bærtur"})}),e.jsx(k,{mb:"3",children:'Bær- og soppsesongen er i full gang til skogs og fjells. Last ned Kartverkets gratis mobil-app "Hvor?", så vet du hvor du er.'}),e.jsx(c,{href:"#",children:"En ekstra link"})]})};var a,o,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => <KvibLinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
+      <KvibHeading size="md" my="2">
+        <KvibLinkOverlay {...args} href="#">
+          Denne kan trykkes på
+        </KvibLinkOverlay>
+      </KvibHeading>
+      <KvibText>Hele komponenten er en knapp</KvibText>
+    </KvibLinkBox>
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var d,l,m;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => <KvibLinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
+      <KvibBox>13 timer siden</KvibBox>
+      <KvibHeading size="md" my="2">
+        <KvibLinkOverlay {...args} href="#">
+          Ikke gå deg vill på bærtur
+        </KvibLinkOverlay>
+      </KvibHeading>
+      <KvibText mb="3">
+        Bær- og soppsesongen er i full gang til skogs og fjells. Last ned Kartverkets gratis mobil-app "Hvor?", så vet
+        du hvor du er.
+      </KvibText>
+      <Link href="#">En ekstra link</Link>
+    </KvibLinkBox>
+}`,...(m=(l=n.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const g=["Preview","LinkOverlayNested"],y=Object.freeze(Object.defineProperty({__proto__:null,LinkOverlayNested:n,Preview:r,__namedExportsOrder:g,default:b},Symbol.toStringTag,{value:"Module"}));export{y as L,r as P,n as a};
