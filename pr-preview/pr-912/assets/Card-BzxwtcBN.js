3e1fe190f84c00e12be2060535023d32f2907269
chrhein <chrhein@users.noreply.github.com>
2024-11-26 14:29:48 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Card-BzxwtcBN.js
@@ -0,0 +1,22 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as d}from"./index-BRkUXrbv.js";import{ae as l,af as a,ag as m}from"./index-CXv2PlT9.js";import{D as o,F as p,b as i}from"./Feedback-DTSfR72I.js";import{C as c,P as t}from"./Card.stories-8r7zVhqD.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-B3dtEGQo.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./box-C9nK2UO9.js";import"./factory-Dr_T95xI.js";import"./image-CB2SsSea.js";import"./simple-grid-BMz7U_wE.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./grid-BtoyrwbI.js";import"./heading-D7346gVA.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";import"./use-recipe-CWoigbDD.js";import"./text-DJW8V014.js";import"./card-sQbwxR_z.js";import"./create-slot-recipe-context-CYF_0Bn9.js";import"./flex-DKwYg45R.js";import"./Link-BR11G8qQ.js";import"./link-DWtvkXlG.js";function s(n){const e={code:"code",h1:"h1",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...d(),...n.components};return r.jsxs(r.Fragment,{children:[r.jsx(l,{of:c}),`
+`,r.jsx(e.h1,{id:"card",children:"Card"}),`
+`,r.jsx(e.p,{children:"Card er et fleksibelt komponent som brukes til å gruppere og vise innhold i et klart og konsist format."}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import { Card, CardHeader, CardBody, CardFooter } from "@kvib/react";
+`})}),`
+`,r.jsxs(o,{children:[r.jsx(e.p,{children:r.jsx(e.strong,{children:"Egnet til:"})}),r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Gruppere innhold"}),`
+`,r.jsx(e.li,{children:"Gruppere innhold og handlinger som hører sammen"}),`
+`,r.jsx(e.li,{children:"Inngangspunkt til mer kompleks og detaljert informasjon"}),`
+`]}),r.jsx(e.p,{children:r.jsx(e.strong,{children:"Uegnet til:"})}),r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Kompleks og detaljert informasjon som krever mye plass"}),`
+`]}),r.jsx(p,{component:"Card"})]}),`
+`,r.jsx(i,{light:!0,children:"Kontekst"}),`
+`,r.jsxs(o,{children:[r.jsx(e.p,{children:r.jsx(e.strong,{children:"Card er bygget opp av fire komponenter:"})}),r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Card"}),`
+`,r.jsx(e.li,{children:"CardHeader"}),`
+`,r.jsx(e.li,{children:"CardBody"}),`
+`,r.jsx(e.li,{children:"CardFooter."}),`
+`]}),r.jsxs(e.p,{children:[r.jsx(e.code,{children:"Card"})," skal alltid være ytterst. ",r.jsx(e.code,{children:"CardBody"})," skal inneholde hoveddelen av innholdet. ",r.jsx(e.code,{children:"CardHeader"})," og ",r.jsx(e.code,{children:"CardFooter"})," er valgfrie."]})]}),`
+`,r.jsx(i,{light:!0,children:"Props"}),`
+`,r.jsx(a,{of:t}),`
+`,r.jsx(m,{of:t})]})}function z(n={}){const{wrapper:e}={...d(),...n.components};return e?r.jsx(e,{...n,children:r.jsx(s,{...n})}):s(n)}export{z as default};
---
+++ pr-preview/pr-912/assets/Link.stories-DupXUfUo.js
@@ -0,0 +1,7 @@
+import{j as i}from"./jsx-runtime-sfY7k0Xq.js";import{L as a}from"./Link-BR11G8qQ.js";const s={title:"Komponenter/Link",component:a,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}},argTypes:{variant:{description:"The visual style of the component.",table:{type:{summary:"string"}},defaultValue:{summary:"underline | plain"},options:["underline","plain"],control:{type:"radio"}},children:{description:"This is the linktext",table:{type:{summary:"string"}},control:"text"},external:{description:"If true, an icon will be included.",table:{type:{summary:"Boolean"},defaultValue:{summary:"false"}},control:"boolean"},href:{description:"This is the link.",table:{type:{summary:"string"}},control:"text"}}},e={args:{children:"Dette er en lenke",href:"/?path=/"},render:r=>i.jsx(a,{...r,children:r.children})};var t,n,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: "Dette er en lenke",
+    href: "/?path=/"
+  },
+  render: args => <KvibLink {...args}>{args.children}</KvibLink>
+}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const l=["Preview"],d=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{d as L,e as P};
