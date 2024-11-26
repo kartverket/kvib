de08640925216a5f768dc6699e71c605f954ad8c
chrhein <chrhein@users.noreply.github.com>
2024-11-26 09:05:59 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/DateRangePicker.stories-pyPzLj5n.js
@@ -0,0 +1,131 @@
+import{j as t}from"./jsx-runtime-sfY7k0Xq.js";import{r as b}from"./index-CcKhGcwW.js";import{D as g,n as m}from"./style-CiF8_und.js";import{c as y}from"./colors-DLx86jGi.js";const a=({...e})=>{const r=f("kvib-datepicker"),l=y[e.colorPalette??"green"],d=Object.fromEntries(Object.entries(l).map(([s,u])=>[s,u.value])),n=h(r,d);return t.jsxs(t.Fragment,{children:[t.jsx("style",{children:n}),t.jsx(g,{classNames:{root:r},locale:m,captionLayout:e.showDropdownMonthYear?"dropdown":"label",...e,mode:"range"})]})},h=(e,r)=>`
+ .${e} {
+
+   /* Colors */
+  --rdp-accent-color: ${r[500]}; /* The accent color used for selected days and UI elements. */
+  --rdp-accent-background-color: ${r[100]}; /* The accent background color used for selected days and UI elements. */
+  --rdp-day_button-border: none; /* The border of the day buttons. */
+  --rdp-range_start-date-background-color: var(--rdp-accent-color); /* The background color of the start date in a range. */
+  --rdp-range_end-date-background-color: var(--rdp-accent-color); /* The background color of the end date in a range. */
+  --rdp-range_start-color: #fff;
+  --rdp-range_end-color: #fff;
+  --rdp-selected-border: 1px solid var(--rdp-accent-color); /* The border color of the selected days. */
+
+  /* Fonts */
+  --rdp-week_number-font: 12px/1 sans-serif;
+
+  /* Day buttons */
+  --rdp-day-width: 42px; /* Width of the day cells. */
+  --rdp-day-height: 42px; /* Height of the day cells. */
+  --rdp-day_button-height: calc(var(--rdp-day-height) - 2px); /* Height of the day buttons. */
+  --rdp-day_button-width: calc(var(--rdp-day-width) - 1px); /* Width of the day buttons. */
+  --rdp-day_button-border-radius: 0.375rem; /* Border radius of the day buttons. */
+  --rdp-outside-opacity: 0.4; /* Opacity of the days outside the current month. */
+  --rdp-disabled-opacity: 0.25; /* Opacity of the disabled days. */
+
+  /* Week numbers */
+  --rdp-weekday-text-align: center;
+
+  .rdp-selected {
+  font-weight: 400;
+  font-size: 16px;
+}
+
+  /* Day styles */
+  .rdp-today {
+    font-weight: 700;
+  }
+
+  /* Navigation buttons */
+  .rdp-button_next,
+  .rdp-button_previous {
+    box-sizing: border-box;
+    height: var(--rdp-day_button-height);
+    width: var(--rdp-day_button-width);
+  }
+
+  .rdp-button_next:not(:disabled):hover,
+  .rdp-button_previous:not(:disabled):hover {
+    border-radius: var(--rdp-day_button-border-radius);
+    background-color: var(--rdp-accent-background-color);
+  }
+
+  .rdp-button_next:disabled,
+  .rdp-button_previous:disabled,
+  .rdp-day_button:disabled
+   {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  /* Weekday and week number styles */
+  .rdp-weekday {
+    vertical-align: middle;
+    font-size: 0.75em;
+    font-weight: 700;
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  .rdp-week_number {
+    vertical-align: baseline;
+    transform: translateY(50%);
+    text-align: center;
+    height: var(--rdp-day_button-height);
+    width: var(--rdp-day_button-width);
+    line-height: 2px;
+  }
+
+  /* Month caption */
+  .rdp-month_caption {
+    padding: 0.5em;
+    font-weight: 700;
+    font-size: 18px;
+    text-transform: capitalize;
+  }
+
+  .rdp-footer {
+  padding: 0.5em;
+  padding-left: 0.75em;
+    font-weight: 400;
+    font-size: 14px;
+  }
+
+  .rdp-dropdowns {
+    gap: 0.5em;
+  }
+
+  /* Range styles */
+  .rdp-range_middle, .rdp-range_start, .rdp-range_end {
+    background-color: var(--rdp-accent-background-color);
+  }
+
+  .rdp-range_start {
+    border-top-left-radius: var(--rdp-day_button-border-radius);
+    border-bottom-left-radius: var(--rdp-day_button-border-radius);
+  }
+
+  .rdp-range_end {
+    border-top-right-radius: var(--rdp-day_button-border-radius);
+    border-bottom-right-radius: var(--rdp-day_button-border-radius);
+  }
+
+  .rdp-day_button {
+    margin: -1px;
+  }
+
+  .rdp-day:hover:not(.rdp-selected):not(.rdp-disabled) {
+    background-color: var(--rdp-accent-background-color);
+    border-radius: var(--rdp-day_button-border-radius);
+  }
+
+  .rdp-outside.rdp-range_start,
+  .rdp-outside.rdp-range_middle,
+  .rdp-outside.rdp-range_end {
+    opacity: 1;
+  }
+
+
+`,f=e=>{const r=Math.random().toString(36).substring(2,7);return`${e}-${r}`};try{a.displayName="DateRangePicker",a.__docgenInfo={description:"",displayName:"DateRangePicker",props:{colorPalette:{defaultValue:null,description:"",name:"colorPalette",required:!1,type:{name:"enum",value:[{value:"undefined"},{value:'"blue"'},{value:'"green"'}]}},showDropdownMonthYear:{defaultValue:null,description:"",name:"showDropdownMonthYear",required:!1,type:{name:"boolean | undefined"}}}}}catch{}const _={title:"Komponenter/DateRangePicker",component:a,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"hidden"}}},args:{showOutsideDays:!1,showWeekNumber:!1,showDropdownMonthYear:!1,colorPalette:"green"},argTypes:{showWeekNumber:{description:"Vis ukenummer",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}},control:"boolean"},showOutsideDays:{description:"Vis datoer utenfor valgt måned",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}},control:"boolean"},showDropdownMonthYear:{description:"Vis dropdown for å velge måned og år",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}},control:"boolean"},colorPalette:{description:"Fargepalett",table:{type:{summary:"green | blue"},defaultValue:{summary:"green"}},options:["green","blue"],control:{type:"radio"}},footer:{description:"Tekst som vises nederst i datovelgeren",table:{type:{summary:"string"}},control:"text"},selected:{description:"Valgt periode",table:{type:{summary:"DateRange"}},control:!1},onSelect:{description:"Callback som kalles når en periode velges",table:{type:{summary:"OnSelectHandler<DateRange | undefined>"}},control:!1}}},v=e=>{const{selected:r,onSelect:l,...d}=e,[n,s]=b.useState(void 0);return t.jsx(a,{selected:n,onSelect:s,...d,mode:"range"})},o={render:e=>t.jsx(v,{...e})};var i,c,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: args => <DateRangePickerExample {...args} />
+}`,...(p=(c=o.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const w=["Preview"],S=Object.freeze(Object.defineProperty({__proto__:null,Preview:o,__namedExportsOrder:w,default:_},Symbol.toStringTag,{value:"Module"}));export{S as D,o as P};
---
+++ pr-preview/pr-912/assets/LinkOverlay-7FwuAuTB.js
@@ -0,0 +1,29 @@
+import{j as r}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as e}from"./index-BRkUXrbv.js";import{ae as n,af as t,ag as s,ai as a}from"./index-efyjSUwp.js";import{L as l,P as m,a as d}from"./LinkOverlay.stories-DzVGP4ux.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BmKvAOMd.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./index-DmkSCyH4.js";import"./stat-DGy9Wjvm.js";import"./index-DC8FWG5U.js";import"./use-locale-context-CnLnjqE7.js";import"./create-context-DPShtm6F.js";import"./create-slot-recipe-context-CYF_0Bn9.js";import"./factory-Dr_T95xI.js";import"./empty-DSVal0bC.js";import"./icons-CXhDYdRi.js";import"./index-BzqNc4xl.js";import"./config-BzBMZny9.js";import"./is-D1VN0Vzs.js";import"./walk-object-MOVJDcbI.js";import"./index-DykOT1nN.js";import"./index-BRNWFSEf.js";import"./index-33_OKIP6.js";import"./index-51odREz2.js";import"./index-DkshEfrg.js";import"./index-Ctx6eFvH.js";import"./index-DMDAIQL1.js";import"./focus-trap.esm-D1qb2f5l.js";import"./index-IUjCT-0R.js";import"./index-CcwapMGp.js";import"./index-LZ0jFNZH.js";import"./field.anatomy-BZ5UhGuB.js";import"./index-C-jMJD2l.js";import"./index-DLirhTLq.js";import"./index-nlDNkuRb.js";import"./floating-ui.dom-BfjzW660.js";import"./index-DI7p0YkP.js";import"./index-Dvita17N.js";import"./index-DVhUfsZv.js";import"./index-BR8t6gqn.js";import"./index-DBzJ7FiP.js";import"./index-FS-qFuuY.js";import"./index-8LG5O2iL.js";import"./index-x1Ien6QS.js";import"./index-CMT2VdI4.js";import"./checkbox.anatomy-WeIGH50-.js";import"./index-Bl9vfJtp.js";import"./index-Bn68IcI8.js";import"./index-BqQfDmyD.js";import"./show-DskmVrvv.js";import"./create-recipe-context-AlCFSakd.js";import"./use-recipe-CWoigbDD.js";import"./select-B21ydTxy.js";import"./create-split-props-u5h9OPvL.js";import"./factory-BHlL6AhM.js";import"./split-presence-props-DcTE3k_W.js";import"./use-presence-context-B6qRt-aB.js";import"./index-Bdoxc4SK.js";import"./use-event-CSfbiBcH.js";import"./use-environment-context-DqdFjJyw.js";import"./use-field-context-CcEUgBGI.js";import"./accordion-DD1KsAPV.js";import"./render-strategy-B0jBahTc.js";import"./alert-CRdwOIqR.js";import"./aspect-ratio-B-7bOwGa.js";import"./avatar-DCbuXzB8.js";import"./badge-a-vhsMzP.js";import"./box-C9nK2UO9.js";import"./circle-DyiK1bSb.js";import"./file-upload-CSnJSLdf.js";import"./breadcrumb-B1uAdMvL.js";import"./button-Cbvbv_aU.js";import"./icon-button-CQVMxZrp.js";import"./card-sQbwxR_z.js";import"./center-7hWxgEBL.js";import"./absolute-center-CNroa-V3.js";import"./checkbox-C5JVDQ7f.js";import"./attr-C3Jtgmrz.js";import"./code-BXkiuTb0.js";import"./container-CLLJd9-b.js";import"./dialog-ieJ52i25.js";import"./editable-BIAdAEvl.js";import"./field-CfLG99pn.js";import"./icon-C_lHd8gp.js";import"./flex-DKwYg45R.js";import"./highlight-Czu-ouLi.js";import"./grid-BtoyrwbI.js";import"./grid-item-DjXkgC50.js";import"./simple-grid-BMz7U_wE.js";import"./group-DiEFhqFk.js";import"./image-CB2SsSea.js";import"./input-CxVsKUJ9.js";import"./input-element-CpSc6Iej.js";import"./kbd-dPfSTdWe.js";import"./link-DWtvkXlG.js";import"./list-Cdms5FJ_.js";import"./menu-DBzePu0A.js";import"./number-input-rPUDxFd5.js";import"./pin-input-DuByOv7l.js";import"./popover-D_nxhwA9.js";import"./progress-s8iT_AqN.js";import"./progress-value-text-a4PLof_7.js";import"./progress-circle-Rp6f7CJw.js";import"./radio-group-BjPrHSdT.js";import"./separator-CmGcrybP.js";import"./skeleton-SUkWNsh_.js";import"./skip-nav-link-CeTS-0SG.js";import"./slider-DAPuFuCP.js";import"./spinner-DP07Pf0W.js";import"./h-stack-CjVo9nnl.js";import"./stack-Bh3oWNRR.js";import"./v-stack-bWf0p04U.js";import"./stack-separator-D2gEZy64.js";import"./steps-BXXnF25n.js";import"./switch-DCyE2lhi.js";import"./table-BFO0IH09.js";import"./tabs-CWHnEAgK.js";import"./tag-BSWy3RGp.js";import"./textarea-C4rszewK.js";import"./toast-B9cUKr-S.js";import"./heading-D7346gVA.js";import"./text-DJW8V014.js";import"./visually-hidden-CkDcAbek.js";import"./index-BteWwxSb.js";import"./iconBase-CTl-8GCs.js";/* empty css              */import"./style-CiF8_und.js";import"./index-6zURGglY.js";import"./index-v24uTv2s.js";import"./react-select-async.esm-YUGiyMWY.js";import"./portal-B73Xo__z.js";import"./useDateSegment-B7dS2BNP.js";import"./parse-BEM0nVXK.js";function p(o){const i={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...e(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(n,{of:l}),`
+`,r.jsx(i.h1,{id:"link-overlay",children:"Link Overlay"}),`
+`,r.jsx(i.p,{children:"Link Overlay er et semantisk komponent som brukes til å wrappe elementer sammen i en link."}),`
+`,r.jsx(i.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,r.jsx(i.pre,{children:r.jsx(i.code,{className:"language-jsx",children:`import { LinkBox, LinkOverlay } from "@kvib/react";
+`})}),`
+`,r.jsx(i.h2,{id:"props",children:"Props"}),`
+`,r.jsx(t,{of:m}),`
+`,r.jsx(s,{of:m}),`
+`,r.jsx(i.h2,{id:"nested-link",children:"Nested link"}),`
+`,r.jsx(t,{of:d}),`
+`,r.jsx(i.h2,{id:"link-overlay-med-routing-library",children:"Link Overlay med Routing Library"}),`
+`,r.jsxs(i.p,{children:["For å legge til støtte for biblioteker som Next.js eller gatsby Link må du wrappe de rundt ",r.jsx(i.code,{children:"LinkOverlay"})," eller bruke de som en prop."]}),`
+`,r.jsx(a,{code:`const RoutingExample = () => {
+  return (
+    <LinkBox as="article">
+      <h2>
+        <LinkOverlay as={NextLink} href="#">
+          Some blog post
+        </LinkOverlay>
+      </h2>
+      <p>
+        As a side note, using quotation marks around an attribute value is required only if this value is not a valid
+        identifier.
+      </p>
+      <a href="#">Some inner link</a>
+    </LinkBox>
+  );
+};`,dark:!0})]})}function Gi(o={}){const{wrapper:i}={...e(),...o.components};return i?r.jsx(i,{...o,children:r.jsx(p,{...o})}):p(o)}export{Gi as default};
