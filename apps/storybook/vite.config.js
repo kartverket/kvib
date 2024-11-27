2350ff154eb3b7f56e2cf5dbc5ebb1e4c4672432
Christian Hein <scheiehein@gmail.com>
2024-11-27 15:56:55 +0100

    Vite js

--- apps/storybook/vite.config.js
+++ apps/storybook/vite.config.js
@@ -2,7 +2,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
-// Convert __dirname for ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
