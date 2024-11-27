309bfffd1a733bbf42af0d750898639d156eb13d
chrhein <chrhein@users.noreply.github.com>
2024-11-27 12:40:21 +0000

    Deploy preview for PR 912 🛫

--- pr-preview/pr-912/assets/CloseButton-B1r55m2F.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-sfY7k0Xq.js";import{useMDXComponents as i}from"./index-BRkUXrbv.js";import{ae as s,af as m,ag as p}from"./index-B3M8Pt-V.js";import{C as a,P as e}from"./CloseButton.stories-DjJoKnnE.js";import"./index-CcKhGcwW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-CbJ7Kje9.js";import"../sb-preview/runtime.js";import"./index-DI5DFbIx.js";import"./index-D-8MO0q_.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./close-button-D6hU4W82.js";import"./index-BteWwxSb.js";import"./iconBase-CTl-8GCs.js";import"./use-recipe-CWoigbDD.js";import"./factory-Dr_T95xI.js";import"./icon-button-CQVMxZrp.js";import"./button-Cbvbv_aU.js";import"./create-recipe-context-AlCFSakd.js";import"./empty-DSVal0bC.js";function n(r){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:a}),`
-`,o.jsx(t.h1,{id:"close-button",children:"Close Button"}),`
-`,o.jsx(t.p,{children:"CloseButton er en knapp med et lukkeikon. Den skal brukes til å lukke funksjonalitet i komponenter."}),`
-`,o.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`import { CloseButton } from "@kvib/react";
-`})}),`
-`,o.jsx(t.h2,{id:"props",children:"Props"}),`
-`,o.jsx(m,{of:e}),`
-`,o.jsx(p,{of:e})]})}function E(r={}){const{wrapper:t}={...i(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(n,{...r})}):n(r)}export{E as default};
