3a81f9f01b9213ad8a1788e4a1b3bfdad53ac2f5
Christian Hein <scheiehein@gmail.com>
2024-11-28 12:23:26 +0100

    Endrer path til index

--- apps/storybook/stories/documentation/Migration.tsx
+++ apps/storybook/stories/documentation/Migration.tsx
@@ -4,6 +4,18 @@ import { Liste } from "./utils/Liste";
 import { Listeelement } from "./utils/Listeelement";
 import { Tekstblokk } from "./utils/Tekstblokk";
 
+const nyeKomponenter = [
+  "Rating",
+  "Select",
+  "PasswordInput",
+  "FileUpload",
+  "CheckboxCard",
+  "Segment Control",
+  "Nested Menu",
+  "Color Swatch",
+  "Color Picker",
+];
+
 export const Migration = () => (
   <Dokumentasjonsside
     tittel="Kvib 5"
@@ -12,6 +24,17 @@ export const Migration = () => (
     <Alert status="info" size="lg">
       Dokumentasjonen vår vil være litt utdatert i en tid fremover etter oppdateringen.
     </Alert>
+    <Tekstblokk tittel="Syntaksendringer">
+      <Liste>
+        <Listeelement tittel="Dot-notasjon for komponentene">
+          Man kan nå importere én rot-komponent og få tilgang til alle underkomponentene gjennom dot-notasjon. Dette er
+          standarden i Chakra fra nå, men for å unngå enorme mengder med breaking changes har vi valgt å fortsatt tilby
+          komponentene våre som enkeltstående importer, i tillegg til dot-notasjon. Dette tas i bruk ved å importere{" "}
+          <Code>Kvib*</Code>-komponentene, som f.eks. <Code>KvibAccordion</Code>. Dette gir tilgang til
+          underkomponentene gjennom dot-notasjon, som f.eks. <Code>KvibAccordion.Item</Code>.
+        </Listeelement>
+      </Liste>
+    </Tekstblokk>
     <Tekstblokk tittel="Komponenter med nye navn">
       Flere av komponentene har fått nye navn. Dette byr på breaking changes for de som oppgraderer fra tidligere
       versjoner av Kvib.
@@ -32,30 +55,28 @@ export const Migration = () => (
       </Text>
       <Liste>
         <Listeelement tittel="is*">
-          Alle Chakra-props som tidligere har blitt kalt noe som starter på <Code>is*</Code>, som f.eks.{" "}
-          <Code>isRequired</Code>, vil fra nå kun hete <Code>required</Code>.
+          Alle Chakra-props som tidligere har blitt kalt noe som starter på <Code>{`is<X>`}</Code>, som f.eks.{" "}
+          <Code>isRequired</Code>, vil fra nå kun hete <Code>{`<x>`}</Code>, altså <Code>required</Code>. Det samme
+          gjelder <Code>defaultIsOpen</Code>, som nå heter <Code>defaultOpen</Code>.
+        </Listeelement>
+        <Listeelement tittel="colorScheme">
+          Heter nå <Code>colorPalette</Code>. Man kan også bruke variantene av en fargepalett slik{" "}
+          <Code>{`<Box bg="colorPalette.400">...</Box>`}</Code>
         </Listeelement>
         <Listeelement tittel="spacing">
           Har blitt forenklet slik at alle komponenter bruker <Code>gap</Code>. Tidligere har det vært noen som bruker
           den ene og noen som bruker den andre, men nå er det kun <Code>gap</Code> som gjelder.
         </Listeelement>
+        <Listeelement tittel="sx">
+          <Code>sx</Code> har nå bli erstattet med <Code>css</Code>.
+        </Listeelement>
       </Liste>
     </Tekstblokk>
     <Tekstblokk tittel="Nye komponenter">
       <Liste>
-        <Listeelement tittel="ColorPicker"></Listeelement>
-        <Listeelement tittel="ColorSwatch"></Listeelement>
-      </Liste>
-    </Tekstblokk>
-    <Tekstblokk tittel="Syntaksendringer">
-      <Liste>
-        <Listeelement tittel="Dot-notasjon for komponentene">
-          Man kan nå importere én rot-komponent og få tilgang til alle underkomponentene gjennom dot-notasjon. Dette er
-          standarden i Chakra fra nå, men for å unngå enorme mengder med breaking changes har vi valgt å fortsatt tilby
-          komponentene våre som enkeltstående importer, i tillegg til dot-notasjon. Dette tas i bruk ved å importere{" "}
-          <Code>Kvib*</Code>-komponentene, som f.eks. <Code>KvibAccordion</Code>. Dette gir tilgang til
-          underkomponentene gjennom dot-notasjon, som f.eks. <Code>KvibAccordion.Item</Code>.
-        </Listeelement>
+        {nyeKomponenter.sort().map(komponent => (
+          <Listeelement key={komponent} tittel={komponent} />
+        ))}
       </Liste>
     </Tekstblokk>
   </Dokumentasjonsside>
