e7d9199e7946714487bfbd7dcf9e4c1966e3af33
chrhein <chrhein@users.noreply.github.com>
2024-11-28 08:39:35 +0000

    Deploy preview for PR 912 🛫

---
+++ pr-preview/pr-912/assets/Spinner-7d7-vVU-.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as p}from"./index-C2WH5l5l.js";import{ae as d,af as i,ah as t,ag as m}from"./index-scZTFslL.js";import{D as s,F as c,a as g,b as o}from"./Feedback-Ddcknu-S.js";import{S as j,a as h,b as x,P as l}from"./Spinner.stories-CemH2fog.js";import"./index-Wp2u197Z.js";import"./iframe-P7f-nCkU.js";import"../sb-preview/runtime.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./box-BV7_-RZx.js";import"./factory-DbxLU_Fv.js";import"./image-CQDJLaH5.js";import"./simple-grid-Bsi4EGdg.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./grid-D5vdwI11.js";import"./heading-CoXJvVOu.js";import"./create-recipe-context-uxbFyZx7.js";import"./empty-DSVal0bC.js";import"./use-recipe-Bg1t1VCq.js";import"./text-DjVM_lrW.js";import"./card-CN8xBwHc.js";import"./create-slot-recipe-context-Ct7AJ-u_.js";import"./flex-lNX0IHYU.js";import"./Link-BrjUlTjh.js";import"./link-BIDHNzfj.js";import"./spinner-Ddxj2mVP.js";import"./stack-BOCWF6Sh.js";function a(r){const n={code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...p(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:j}),`
+`,e.jsx(n.h1,{id:"spinner",children:"Spinner"}),`
+`,e.jsx(n.p,{children:"Spinner gir et visuelt signal om at en oppgave prosseseres og vil gi en endring eller et resultat."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { Spinner } from "@kvib/react";
+`})}),`
+`,e.jsxs(s,{children:[e.jsx(n.p,{children:e.jsx(n.strong,{children:"Egnet til:"})}),e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Vise lasting eller ubestemt ventetid"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Indikere lasting som tar mer enn ett sekund"}),`
+`]}),`
+`]}),e.jsx(n.p,{children:e.jsx(n.strong,{children:"Uegnet til:"})}),e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Vise framgang i prosesser"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Indikere lasting kortere enn ett sekund"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Indikere over 5 minutters ventetid"}),`
+`]}),`
+`]}),e.jsx(c,{component:"Spinner"})]}),`
+`,e.jsx(s,{children:e.jsx(g,{title:"Utseende",description:`Fargene burde bli valgt med tanke på kontrast. Velg size og thickness slik at den gir mening til komponentet eller innholdet som skal lastes..
+Farten kan justeres for å gjenspeile estimert ventetid. `,story:e.jsxs(i,{children:[e.jsx(t,{of:h}),e.jsx(t,{of:x})]})})}),`
+`,e.jsx(o,{light:!0,children:"Retningslinjer"}),`
+`,e.jsxs(s,{children:[e.jsx(n.h3,{id:"spinner-vs-progress",children:"Spinner vs. Progress"}),e.jsx(n.p,{children:`Progress burde velges over spinner hvis man kan se prosentvis framgang, som f.eks. upload/download av filer, eller skjema med flere steg
+(Der kan stepper vurderes også). Spinner burde brukes over Progress uansett, så lenge ventetiden er minimal.`}),e.jsx(n.h3,{id:"ventetid",children:"Ventetid"}),e.jsx(n.p,{children:`Om bruker må vente, vil spinner bidra til å redusere usikkerhet. Det tar ikke lang tid før folk blir
+utålmodige og/eller forvirra over at ingenting skjer. Hvis ventetiden overstiger 9 sekunder, bør bruker se en
+forklaring sammen med Progress, og tekst som forklarer hva maskinen jobber med.`})]}),`
+`,e.jsx(o,{light:!0,children:"Props"}),`
+`,e.jsx(i,{of:l}),`
+`,e.jsx(m,{of:l})]})}function K(r={}){const{wrapper:n}={...p(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(a,{...r})}):a(r)}export{K as default};
