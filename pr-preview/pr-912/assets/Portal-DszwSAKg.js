4ad589d7ba51cecda993a26d7fde8216858690ca
chrhein <chrhein@users.noreply.github.com>
2024-11-28 08:07:12 +0000

    Deploy preview for PR 912 🛫

--- pr-preview/pr-912/assets/Portal-DszwSAKg.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as i}from"./index-C2WH5l5l.js";import{ae as s,af as a,ag as m}from"./index-mQkRMU9y.js";import{P as p,a as e}from"./Portal.stories-CGwxHDEX.js";import"./index-Wp2u197Z.js";import"./iframe-BceXYb2z.js";import"../sb-preview/runtime.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./portal-DnyyLHII.js";import"./index-C5caIrke.js";import"./use-environment-context-CJHKuyB_.js";import"./create-context-9KvyJVA-.js";import"./box-BV7_-RZx.js";import"./factory-DbxLU_Fv.js";import"./text-DjVM_lrW.js";import"./create-recipe-context-uxbFyZx7.js";import"./empty-DSVal0bC.js";import"./use-recipe-Bg1t1VCq.js";function n(o){const t={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:p}),`
-`,r.jsx(t.h1,{id:"portal",children:"Portal"}),`
-`,r.jsx(t.p,{children:"Portal bruker ReactDOM.createPortal-APIet for å legge til et element på slutten av document.body eller en spesifikk container."}),`
-`,r.jsx(t.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,r.jsx(t.pre,{children:r.jsx(t.code,{className:"language-jsx",children:`import { Portal } from "@kvib/react";
-`})}),`
-`,r.jsx(t.h2,{id:"props",children:"Props"}),`
-`,r.jsx(a,{of:e}),`
-`,r.jsx(m,{of:e})]})}function _(o={}){const{wrapper:t}={...i(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(n,{...o})}):n(o)}export{_ as default};
---
+++ pr-preview/pr-912/assets/Progress-DFu6LpoO.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as n}from"./index-C2WH5l5l.js";import{ae as m,af as p,ag as l}from"./index-CjlpKdiy.js";import{b as o,D as a}from"./Feedback-Ddcknu-S.js";import{P as d,a as t}from"./Progress.stories-BxCXJnFv.js";import"./index-Wp2u197Z.js";import"./iframe-D76ad2zw.js";import"../sb-preview/runtime.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./box-BV7_-RZx.js";import"./factory-DbxLU_Fv.js";import"./image-CQDJLaH5.js";import"./simple-grid-Bsi4EGdg.js";import"./walk-object-MOVJDcbI.js";import"./is-D1VN0Vzs.js";import"./grid-D5vdwI11.js";import"./heading-CoXJvVOu.js";import"./create-recipe-context-uxbFyZx7.js";import"./empty-DSVal0bC.js";import"./use-recipe-Bg1t1VCq.js";import"./text-DjVM_lrW.js";import"./card-CN8xBwHc.js";import"./create-slot-recipe-context-Ct7AJ-u_.js";import"./flex-lNX0IHYU.js";import"./Link-BrjUlTjh.js";import"./link-BIDHNzfj.js";import"./toggle-tip-BBA6gFvc.js";import"./index-Bokjg-wG.js";import"./iconBase-ur5FuG4R.js";import"./portal-DnyyLHII.js";import"./index-C5caIrke.js";import"./use-environment-context-CJHKuyB_.js";import"./create-context-9KvyJVA-.js";import"./icon-button-Bnp4VcWh.js";import"./button-CYFvIhue.js";import"./popover-D5ryemgo.js";import"./split-presence-props-DcTE3k_W.js";import"./create-split-props-u5h9OPvL.js";import"./use-presence-context-CTMwjttg.js";import"./index-Cq1GvU5P.js";import"./index-Ctx6eFvH.js";import"./index-CPV3pteC.js";import"./use-event-BQKQuVfd.js";import"./index-D5MGdGv7.js";import"./index-BRNWFSEf.js";import"./index-CSN3utww.js";import"./floating-ui.dom-BfjzW660.js";import"./focus-trap.esm-CVCU0Y7G.js";import"./index-Cbd61W9O.js";import"./index-D_kLChQY.js";import"./index-3UVeZ5B9.js";import"./use-locale-context-DoP-38uM.js";import"./factory-DzReEOQo.js";import"./progress-Cm-s0-jH.js";import"./progress-value-text-D4QkzDnp.js";import"./index-DJhKCpBo.js";function s(i){const e={code:"code",h1:"h1",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...n(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(m,{of:d}),`
+`,r.jsx(e.h1,{id:"progress",children:"Progress"}),`
+`,r.jsx(e.p,{children:"Progress brukes til å vise frem status på fremdrift for en oppgave som tar tid eller består av flere steg."}),`
+`,r.jsx(e.pre,{children:r.jsx(e.code,{className:"language-jsx",children:`import { Progress } from "@kvib/react";
+`})}),`
+`,r.jsx(e.p,{children:r.jsx(e.strong,{children:"Egnet til:"})}),`
+`,r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Vise framgang i en prosess"}),`
+`]}),`
+`,r.jsx(e.p,{children:r.jsx(e.strong,{children:"Uegnet til:"})}),`
+`,r.jsxs(e.ul,{children:[`
+`,r.jsx(e.li,{children:"Vise lasting som nesten er umiddelbar"}),`
+`,r.jsx(e.li,{children:"vise lasting for prosesser som ikke er så viktige for brukeropplevelsen"}),`
+`]}),`
+`,r.jsx(o,{light:!0,children:"Retningslinjer"}),`
+`,r.jsxs(a,{children:[r.jsx(e.h3,{id:"progress-vs-spinner",children:"Progress vs. Spinner"}),r.jsx(e.p,{children:`Progress burde velges over spinner hvis man kan se prosentvis framgang, som f.eks. upload/download av filer, eller skjema med flere steg
+(Der kan stepper vurderes også). Hvis ventetiden er minimal, burde man ta spinner istedenfor.`})]}),`
+`,r.jsx(o,{light:!0,children:"Props"}),`
+`,r.jsx(p,{of:t,className:"fullwidth"}),`
+`,r.jsx(l,{of:t})]})}function xr(i={}){const{wrapper:e}={...n(),...i.components};return e?r.jsx(e,{...i,children:r.jsx(s,{...i})}):s(i)}export{xr as default};
