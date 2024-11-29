c987f3d1559b30f5f9a5e066cfc14071c4b98fd0
chrhein <31273371+chrhein@users.noreply.github.com>
2024-11-29 06:48:27 +0000

    Deploying to gh-pages from @ kartverket/kvib@10278efa2f96febdcb15b1f51eb1fc4750205873 🚀

---
+++ assets/Badge-D51y4XLN.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as a}from"./index-C2WH5l5l.js";import{T as l}from"./index-C7Oi3TOS.js";import{ae as d,af as c,ag as m}from"./index-BWd7GHpp.js";import{D as s,F as g,b as t}from"./Feedback-CIV5ZGAV.js";import{B as j,P as i}from"./Badge.stories-eKEkunZ_.js";import"./index-Wp2u197Z.js";import"./index-C2tM1_0x.js";import"./iframe-DplJhCfS.js";import"../sb-preview/runtime.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";function o(n){const r={code:"code",h1:"h1",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...a(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:j}),`
+`,e.jsxs(s,{children:[e.jsx(r.h1,{id:"badge",children:"Badge"}),e.jsx(r.p,{children:"Et Badge brukes til å fremheve elementers status slik at brukeren raskt gjenkjenner tilstanden."}),e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Badge } from "@kvib/react";
+`})}),e.jsx(r.p,{children:e.jsx(r.strong,{children:"Egnet til:"})}),e.jsxs(r.ul,{children:[`
+`,e.jsx(r.li,{children:"Vise statusen til et element"}),`
+`]}),e.jsx(r.p,{children:e.jsx(r.strong,{children:"Uegnet til:"})}),e.jsxs(r.ul,{children:[`
+`,e.jsx(r.li,{children:"Kategorisering av elementer (bruk heller tag)"}),`
+`]}),e.jsx(g,{component:"Badge"})]}),`
+`,e.jsxs(s,{children:[e.jsx(t,{light:!0,children:"Retningslinjer"}),e.jsx(l,{paddingLeft:"1rem",children:e.jsx(r.p,{children:`Ikke bli forvirret av Badge og Tag. Badge skal brukes for å raskt kunne se statusen på noe. Bruk grønn for positive og
+rød for negative statuser. Solid variant brukes for å fremheve Badgen mer. Vær konsekvent med fargebruk.`})}),e.jsx(t,{light:!0,children:"Props"}),e.jsx(c,{of:i}),e.jsx(m,{of:i})]})]})}function E(n={}){const{wrapper:r}={...a(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(o,{...n})}):o(n)}export{E as default};
--- assets/call-all-DDzecCnU.js
+++
@@ -1,1 +0,0 @@
-function a(...c){return function(...l){c.forEach(r=>r==null?void 0:r(...l))}}function o(...c){return function(l){c.some(r=>(r==null||r(l),l==null?void 0:l.defaultPrevented))}}export{a,o as c};
