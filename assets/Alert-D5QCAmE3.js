4c6bb200fce52a44270c3956ad93a07d5c8efbc3
chrhein <31273371+chrhein@users.noreply.github.com>
2024-11-29 10:38:36 +0000

    Deploying to gh-pages from @ kartverket/kvib@ca50464193ee728c789d0a2bc6f4b4b7ccba4df7 🚀

--- assets/Alert-D5QCAmE3.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as s}from"./index-C2WH5l5l.js";import{ae as i,af as m,ag as a}from"./index-BKQyuJ86.js";import{b as l}from"./Feedback-CIV5ZGAV.js";import{C as p}from"./ComponentsBanner-zMf2MTPF.js";import{A as c,P as t}from"./Alert.stories-BeczWq-1.js";import"./index-Wp2u197Z.js";import"./iframe-QvgwYF6E.js";import"../sb-preview/runtime.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./index-C7Oi3TOS.js";function n(r){const o={code:"code",pre:"pre",...s(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
-`,e.jsx(p,{title:"Alert",description:"Alert brukes til meldinger som kommuniserer viktig informasjon sammen med, eller i, andre komponenter som brukeren interagerer med, for eksempel underveis i utfylling av skjema.",hasCodeBelow:!0}),`
-`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`import { Alert, AlertIcon, AlertTitle, AlertDescription } from "@kvib/react";
-`})}),`
-`,e.jsx("br",{}),`
-`,e.jsx("br",{}),`
-`,e.jsx(l,{light:!0,children:"Props"}),`
-`,e.jsx(m,{of:t}),`
-`,e.jsx(a,{of:t})]})}function M(r={}){const{wrapper:o}={...s(),...r.components};return o?e.jsx(o,{...r,children:e.jsx(n,{...r})}):n(r)}export{M as default};
--- assets/entry-preview-BaahBh9T.js
+++
@@ -1,1 +0,0 @@
-import{d as s,m as i,p as d,r as n,a as c}from"./chunk-H7CJXHDS-CtWrZ-zP.js";import"./iframe-QvgwYF6E.js";import"../sb-preview/runtime.js";import"./chunk-XP5HYGXS-BGCqD1aY.js";import"./index-Wp2u197Z.js";import"./index-C2tM1_0x.js";export{s as decorators,i as mount,d as parameters,n as render,c as renderToCanvas};
