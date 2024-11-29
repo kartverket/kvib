e026948c3aac08048c0d1155b7c34188c243f6cd
chrhein <31273371+chrhein@users.noreply.github.com>
2024-11-29 09:38:19 +0000

    Deploying to gh-pages from @ kartverket/kvib@68be17f19a1505b09a68f1aec542eab85f8d17db 🚀

---
+++ assets/Transitions-wqAeBrIg.js
@@ -0,0 +1,35 @@
+import{j as o}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as i}from"./index-C2WH5l5l.js";import"./chunk-NUUEMKO5-BQyrOgXO.js";import{ae as s,ai as e}from"./index-BKQyuJ86.js";import"./index-Wp2u197Z.js";import"./iframe-QvgwYF6E.js";import"../sb-preview/runtime.js";import"./react-18-B9NBEw6j.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";const a=`
+const transitionProperty = {
+  common: "background-color, border-color, color, fill, stroke, opacity, box-shadow, transform",
+  colors: "background-color, border-color, color, fill, stroke",
+  dimensions: "width, height",
+  position: "left, right, top, bottom",
+  background: "background-color, background-image, background-position",
+};
+
+const transitionTimingFunction = {
+  "ease-in": "cubic-bezier(0.4, 0, 1, 1)",
+  "ease-out": "cubic-bezier(0, 0, 0.2, 1)",
+  "ease-in-out": "cubic-bezier(0.4, 0, 0.2, 1)",
+};
+
+const transitionDuration = {
+  "ultra-fast": "50ms",
+  faster: "100ms",
+  fast: "150ms",
+  normal: "200ms",
+  slow: "300ms",
+  slower: "400ms",
+  "ultra-slow": "500ms",
+};
+
+const transition = {
+  property: transitionProperty,
+  easing: transitionTimingFunction,
+  duration: transitionDuration,
+};
+
+export default transition;
+`;function r(t){const n={h2:"h2",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Komponenter/Tokens/Transitions"}),`
+`,o.jsx(n.h2,{id:"transitions",children:"Transitions"}),`
+`,o.jsx(e,{code:a})]})}function k(t={}){const{wrapper:n}={...i(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(r,{...t})}):r(t)}export{k as default};
