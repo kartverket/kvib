9405f4e962f280209c9113f7a1aa2ef1e6ee230b
chrhein <31273371+chrhein@users.noreply.github.com>
2024-12-02 08:14:36 +0000

    Deploying to gh-pages from @ kartverket/kvib@7909221f2db5a3a86d276960a728700c97bf715f 🚀

---
+++ assets/DocsRenderer-CFRXHY34-DRi6XdAb.js
@@ -0,0 +1,1 @@
+import{D as f,d as D}from"./chunk-NUUEMKO5-6SjkC0jy.js";import"./iframe-B5w9eKFh.js";import"../sb-preview/runtime.js";import"./index-Wp2u197Z.js";import"./index-0_rPXZYS.js";import"./jsx-runtime-CQsLhzk5.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./react-18-B9NBEw6j.js";export{f as DocsRenderer,D as defaultComponents};
---
+++ assets/IconButton-DkjpgKeo.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as s}from"./index-C2WH5l5l.js";import{ae as i,af as a,ag as m}from"./index-0_rPXZYS.js";import{I as p,P as r}from"./IconButton.stories-OCuZpM2p.js";import"./index-Wp2u197Z.js";import"./iframe-B5w9eKFh.js";import"../sb-preview/runtime.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./index-CyhnlmWi.js";function t(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:p}),`
+`,e.jsx(n.h1,{id:"icon-button",children:"Icon Button"}),`
+`,e.jsx(n.p,{children:"Her er en oversikt over utvalget av ikonknapper i Kartverkets Designbibliotek. Disse er blant annet bygget opp ved bruk av design tokens fra biblioteket, eksempelvis farger og ikoner. I dokumentasjonen følger beskrivelse av komponenten og bruksområde, sammen med kodeeksempler."}),`
+`,e.jsx(n.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { IconButton } from "@kvib/react";
+`})}),`
+`,e.jsx(n.h2,{id:"props",children:"Props"}),`
+`,e.jsx(a,{of:r}),`
+`,e.jsx(m,{of:r})]})}function v(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{v as default};
