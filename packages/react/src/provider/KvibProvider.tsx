7c1c6d2d0816b7d4936529fd386001eea649e143
Christian Hein <scheiehein@gmail.com>
2024-12-02 09:04:34 +0100

    Fjerner muligheten for å sette dark mode

--- packages/react/src/provider/KvibProvider.tsx
+++ packages/react/src/provider/KvibProvider.tsx
@@ -1,18 +1,7 @@
 import { Provider } from "@/components/ui/provider";
-import { Theme } from "@/layout/theme";
 import "@fontsource/mulish/400.css";
 import "@fontsource/mulish/600.css";
 import "@fontsource/mulish/700.css";
 import "material-symbols";
-import React from "react";
 
-export interface KvibProviderProps {
-  colorPalette?: "green" | "blue";
-  children?: React.ReactNode;
-}
-
-export const KvibProvider = ({ colorPalette = "green", children }: KvibProviderProps) => (
-  <Provider>
-    <Theme colorPalette={colorPalette}>{children}</Theme>
-  </Provider>
-);
+export const KvibProvider = ({ children }: { children: React.ReactNode }) => <Provider>{children}</Provider>;
