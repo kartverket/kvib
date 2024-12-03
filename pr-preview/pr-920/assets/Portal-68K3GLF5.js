e8f93407311c786d9b1b4fc7a25bca951e80114c
chrhein <chrhein@users.noreply.github.com>
2024-12-03 09:33:09 +0000

    Remove preview for PR 920 🛬

--- pr-preview/pr-920/assets/Portal-68K3GLF5.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as s}from"./index-C2WH5l5l.js";import{ae as a,af as i,ag as m}from"./index-Cq1f4riH.js";import{P as p,a as o}from"./Portal.stories-BN51yyYm.js";import"./index-Wp2u197Z.js";import"./iframe-DhKz9-LK.js";import"../sb-preview/runtime.js";import"./index-C2tM1_0x.js";import"./index-yiGAEVS0.js";import"./index-DrFu-skq.js";import"./index-Ckh_gHQY.js";function n(t){const r={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:p}),`
-`,e.jsx(r.h1,{id:"portal",children:"Portal"}),`
-`,e.jsx(r.p,{children:"Portal bruker ReactDOM.createPortal-APIet for å legge til et element på slutten av document.body eller en spesifikk container."}),`
-`,e.jsx(r.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Portal } from "@kvib/react";
-`})}),`
-`,e.jsx(r.h2,{id:"props",children:"Props"}),`
-`,e.jsx(i,{of:o}),`
-`,e.jsx(m,{of:o})]})}function M(t={}){const{wrapper:r}={...s(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(n,{...t})}):n(t)}export{M as default};
