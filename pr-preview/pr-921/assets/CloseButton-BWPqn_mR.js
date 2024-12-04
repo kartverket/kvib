3696ec138cbc748f711924b8c9b82b072b4d559c
chrhein <chrhein@users.noreply.github.com>
2024-12-04 08:58:29 +0000

    Remove preview for PR 921 🛬

--- pr-preview/pr-921/assets/CloseButton-BWPqn_mR.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./index-D6_wGsWO.js";import{useMDXComponents as s}from"./index-BGZZYR5m.js";import{ae as i,af as a,ag as m}from"./index-TbOp0XQz.js";import{C as p,P as t}from"./CloseButton.stories-CLQIsn5R.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-BGQlILji.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-CdN4NO-W.js";function r(n){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:p}),`
-`,o.jsx(e.h1,{id:"close-button",children:"Close Button"}),`
-`,o.jsx(e.p,{children:"CloseButton er en knapp med et lukkeikon. Den skal brukes til å lukke funksjonalitet i komponenter."}),`
-`,o.jsx(e.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`import { CloseButton } from "@kvib/react";
-`})}),`
-`,o.jsx(e.h2,{id:"props",children:"Props"}),`
-`,o.jsx(a,{of:t}),`
-`,o.jsx(m,{of:t})]})}function M(n={}){const{wrapper:e}={...s(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(r,{...n})}):r(n)}export{M as default};
