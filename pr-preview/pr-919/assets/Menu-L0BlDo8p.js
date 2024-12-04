e5ad9ff824db6755b6fb1e83aee157485530be67
chrhein <chrhein@users.noreply.github.com>
2024-12-04 14:03:45 +0000

    Remove preview for PR 919 🛬

--- pr-preview/pr-919/assets/Menu-L0BlDo8p.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./index-D6_wGsWO.js";import{useMDXComponents as s}from"./index-BGZZYR5m.js";import{r as i,s as a}from"./index-4EABdPr2.js";import{ae as m,ah as d,ag as u}from"./index-COQje-rV.js";import{M as p,P as o}from"./Menu.stories-DZPU0HOF.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-DuMkG_2c.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";function t(e){const n={h1:"h1",h2:"h2",p:"p",...s(),...e.components};return r.jsxs(r.Fragment,{children:[r.jsx(m,{of:p}),`
-`,r.jsx(n.h1,{id:"menu",children:"Menu"}),`
-`,r.jsx(n.p,{children:"Menu er en dropdown-meny for det vanlige dropdown menu button designmønsteret. Den bruker roving tabIndex for fokus. Det er mulig å søke i MenuItems ved å bruke tastaturet."}),`
-`,r.jsx(n.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,r.jsx(n.h2,{id:"props",children:"Props"}),`
-`,r.jsx(i,{variant:"outline",borderRadius:"0",children:r.jsx(a,{children:r.jsx(d,{of:o})})}),`
-`,r.jsx(u,{of:o})]})}function v(e={}){const{wrapper:n}={...s(),...e.components};return n?r.jsx(n,{...e,children:r.jsx(t,{...e})}):t(e)}export{v as default};
