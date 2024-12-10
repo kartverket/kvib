2e287023a70dead816133df42be4805cc00835f6
Christian Hein <scheiehein@gmail.com>
2024-12-10 15:02:17 +0100

    Ny doc for alle på A og B

---
+++ apps/storybook/stories/components/breadcrumb/BreadcrumbDocs.tsx
@@ -0,0 +1,23 @@
+import { Dokumentasjonsside } from "../../documentation/utils/Dokumentasjonsside";
+import { Liste } from "../../documentation/utils/Liste";
+import { Listeelement } from "../../documentation/utils/Listeelement";
+import { Tekstblokk } from "../../documentation/utils/Tekstblokk";
+
+export const BreadcrumbDocs = () => (
+  <Dokumentasjonsside
+    tittel="Breadcrumb"
+    beskrivelse={`Vi bruker brødsmulestier (breadcrumbs) som et navigasjonselement for å vise brukerens plassering på et overordnet nivå, for eksempel på et nettsted. De plasseres høyt på siden, gjerne rett under hovedmenyen og -navigasjonen, men over sidetittelen.`}
+  >
+    <Tekstblokk tittel="Egnet til:">
+      <Liste>
+        <Listeelement>når det er tre eller flere nivåer i en hierarkisk struktur.</Listeelement>
+        <Listeelement>å gå tilbake til et tidligere trinn eller et overordnet nivå i løsningen.</Listeelement>
+      </Liste>
+    </Tekstblokk>
+    <Tekstblokk tittel="Ikke egnet til:">
+      <Liste>
+        <Listeelement>lineær navigasjon, som for eksempel trinnene i en arbeidsflyt eller et skjema.</Listeelement>
+      </Liste>
+    </Tekstblokk>
+  </Dokumentasjonsside>
+);
