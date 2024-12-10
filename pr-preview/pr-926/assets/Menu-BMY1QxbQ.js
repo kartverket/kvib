c6e65eb8961acc1ef6fd7cae30d6402fee9131e7
chrhein <chrhein@users.noreply.github.com>
2024-12-10 14:04:09 +0000

    Deploy preview for PR 926 🛫

--- pr-preview/pr-926/assets/Menu-BMY1QxbQ.js
+++
@@ -1,7 +0,0 @@
-import{j as n}from"./index-D6_wGsWO.js";import{useMDXComponents as s}from"./index-BGZZYR5m.js";import{J as i,K as a}from"./index-aLGUikq2.js";import{ae as m,ah as d,ag as u}from"./index-knASgBQQ.js";import{M as p,P as o}from"./Menu.stories-BZ_tErWg.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-C0K_q9cv.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";function t(e){const r={h1:"h1",h2:"h2",p:"p",...s(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(m,{of:p}),`
-`,n.jsx(r.h1,{id:"menu",children:"Menu"}),`
-`,n.jsx(r.p,{children:"Menu er en dropdown-meny for det vanlige dropdown menu button designmønsteret. Den bruker roving tabIndex for fokus. Det er mulig å søke i MenuItems ved å bruke tastaturet."}),`
-`,n.jsx(r.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,n.jsx(r.h2,{id:"props",children:"Props"}),`
-`,n.jsx(i,{variant:"outline",borderRadius:"0",children:n.jsx(a,{children:n.jsx(d,{of:o})})}),`
-`,n.jsx(u,{of:o})]})}function v(e={}){const{wrapper:r}={...s(),...e.components};return r?n.jsx(r,{...e,children:n.jsx(t,{...e})}):t(e)}export{v as default};
---
+++ pr-preview/pr-926/assets/Stat.stories-RYmvGGgL.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-D6_wGsWO.js";import{X as s,Y as i,Z as c}from"./index-DarVU0NI.js";const S={title:"Komponenter/Stat",component:s,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}}},t={render:n=>e.jsxs(s,{...n,children:[e.jsx(i,{children:"Utgifter"}),e.jsx(c,{children:"3200kr"})]})};var r,a,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: (args: StatProps) => <KvibStat {...args}>
+      <KvibStatLabel>Utgifter</KvibStatLabel>
+      <KvibStatNumber>3200kr</KvibStatNumber>
+    </KvibStat>
+}`,...(o=(a=t.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const m=["Preview"],b=Object.freeze(Object.defineProperty({__proto__:null,Preview:t,__namedExportsOrder:m,default:S},Symbol.toStringTag,{value:"Module"}));export{t as P,b as S};
