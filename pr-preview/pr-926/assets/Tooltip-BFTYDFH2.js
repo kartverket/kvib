0f12f678e45517baf9afb72d63f1a992366d816b
chrhein <chrhein@users.noreply.github.com>
2024-12-11 10:12:01 +0000

    Deploy preview for PR 926 🛫

--- pr-preview/pr-926/assets/Tooltip-BFTYDFH2.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./index-D6_wGsWO.js";import{useMDXComponents as i}from"./index-BGZZYR5m.js";import"./index-DarVU0NI.js";import{ae as s,af as m,ag as a}from"./index-CeYvw0LM.js";import{b as p}from"./Feedback-x2xwMiKv.js";import{T as l,P as r}from"./Tooltip.stories-BZ1JSPTB.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-DXDnh3uR.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";function e(t){const n={h1:"h1",h2:"h2",p:"p",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:l}),`
-`,o.jsx(n.h1,{id:"tooltip",children:"Tooltip"}),`
-`,o.jsx(n.p,{children:"Et tooltip er en informativ melding som dukker opp når en bruker samhandler med et element."}),`
-`,o.jsx(n.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,o.jsx(p,{light:!0,children:"Props"}),`
-`,o.jsx(m,{of:r}),`
-`,o.jsx(a,{of:r})]})}function D(t={}){const{wrapper:n}={...i(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{D as default};
