074e478051851c2ed43fa3bef9664a8f3b9e7574
chrhein <31273371+chrhein@users.noreply.github.com>
2024-12-12 08:00:50 +0000

    Deploying to gh-pages from @ kartverket/kvib@0e61f77d74f15d9edd933b9b9e3c8ce1decdfd18 🚀

--- assets/Badge-fGh8wKEE.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./index-D6_wGsWO.js";import{useMDXComponents as a}from"./index-BGZZYR5m.js";import{T as l}from"./index-4EABdPr2.js";import{ae as d,af as c,ag as m}from"./index-COQje-rV.js";import{D as s,F as g,b as t}from"./Feedback-CV99xfW7.js";import{B as j,P as i}from"./Badge.stories-D69XCQJF.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-DuMkG_2c.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";function o(n){const r={code:"code",h1:"h1",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...a(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:j}),`
-`,e.jsxs(s,{children:[e.jsx(r.h1,{id:"badge",children:"Badge"}),e.jsx(r.p,{children:"Et Badge brukes til å fremheve elementers status slik at brukeren raskt gjenkjenner tilstanden."}),e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Badge } from "@kvib/react";
-`})}),e.jsx(r.p,{children:e.jsx(r.strong,{children:"Egnet til:"})}),e.jsxs(r.ul,{children:[`
-`,e.jsx(r.li,{children:"Vise statusen til et element"}),`
-`]}),e.jsx(r.p,{children:e.jsx(r.strong,{children:"Uegnet til:"})}),e.jsxs(r.ul,{children:[`
-`,e.jsx(r.li,{children:"Kategorisering av elementer (bruk heller tag)"}),`
-`]}),e.jsx(g,{component:"Badge"})]}),`
-`,e.jsxs(s,{children:[e.jsx(t,{light:!0,children:"Retningslinjer"}),e.jsx(l,{paddingLeft:"1rem",children:e.jsx(r.p,{children:`Ikke bli forvirret av Badge og Tag. Badge skal brukes for å raskt kunne se statusen på noe. Bruk grønn for positive og
-rød for negative statuser. Solid variant brukes for å fremheve Badgen mer. Vær konsekvent med fargebruk.`})}),e.jsx(t,{light:!0,children:"Props"}),e.jsx(c,{of:i}),e.jsx(m,{of:i})]})]})}function E(n={}){const{wrapper:r}={...a(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(o,{...n})}):o(n)}export{E as default};
