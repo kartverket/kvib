6a6b7a5687307c1a43607b13495b665709d41854
chrhein <chrhein@users.noreply.github.com>
2024-12-13 14:09:00 +0000

    Deploy preview for PR 932 🛫

---
+++ pr-preview/pr-932/assets/Link-BBLT5zkR.js
@@ -0,0 +1,16 @@
+import{j as e}from"./index-D6_wGsWO.js";import{useMDXComponents as o}from"./index-BGZZYR5m.js";import{ae as i,af as a,ag as l}from"./index-B7jnqeF9.js";import{D as m,b as d}from"./Feedback-Doi1vxZW.js";import{L as c,P as r}from"./Link.stories-DJv5I9AI.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-DkeMPOOt.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-BTO1GrQj.js";function t(s){const n={a:"a",code:"code",em:"em",h1:"h1",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...o(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(n.h1,{id:"link",children:"Link"}),`
+`,e.jsx(n.p,{children:`Tekstlenker kan benyttes for å henvise til både interne og eksterne nettsider.
+Hvis lenken man trykker på tar deg ut av siden, eller åpner et nytt vindu, skal de vises med et ikon. Det er også viktig at lenketeksten er beskrivende.`}),`
+`,e.jsxs(n.p,{children:["Dersom ",e.jsx(n.em,{children:"Link"})," skal benyttes sammen med et routing bibliotek, se ",e.jsx(n.a,{href:"https://chakra-ui.com/docs/components/link/usage#usage-with-routing-library",rel:"nofollow",children:"Chakras dokumentasjon om 'Usage with Routing Library'"}),` for eksempler og dokumentasjon.
+For bruk av `,e.jsx(n.em,{children:"Link"})," med Next.js, se ",e.jsx(n.a,{href:"https://chakra-ui.com/docs/components/link/usage#usage-with-nextjs",rel:"nofollow",children:"Chakras dokumentasjon om 'Usage with Next.js'"})," for eksempler og dokumentasjon."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { Link } from "@kvib/react";
+`})}),`
+`,e.jsxs(m,{children:[e.jsx(n.p,{children:e.jsx(n.strong,{children:"Egnet til:"})}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Henvise til andre nettsider"}),`
+`]}),e.jsx(n.p,{children:e.jsx(n.strong,{children:"Uegnet til:"})}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Bli brukt som knapp"}),`
+`]})]}),`
+`,e.jsx(d,{light:!0,children:"Props"}),`
+`,e.jsx(a,{of:r}),`
+`,e.jsx(l,{of:r})]})}function D(s={}){const{wrapper:n}={...o(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{D as default};
---
+++ pr-preview/pr-932/assets/Logo.stories-DLDnC8zK.js
@@ -0,0 +1,22 @@
+import{j as o}from"./index-D6_wGsWO.js";import{b4 as e,d as u}from"./index-BTO1GrQj.js";const v={title:"Komponenter/Logo",component:e,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}},argTypes:{label:{table:{type:{summary:"string"}},control:"text"},variant:{table:{type:{summary:"vertical | horizontal"},defaultValue:{summary:"vertical"}},options:["vertical","horizontal"],control:{type:"radio"}},size:{table:{type:{summary:"number"}},control:"number"}}},a={args:{label:"Logo"},render:r=>o.jsx(e,{...r})},s={args:{label:"Logo",variant:"horizontal"},render:r=>o.jsx(e,{...r})},t={args:{label:"Logo",variant:"horizontal"},render:r=>o.jsxs(u,{children:[o.jsx(e,{...r,size:100}),o.jsx(e,{...r,size:200}),o.jsx(e,{...r,size:300})]})};var n,i,l;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    label: "Logo"
+  },
+  render: args => <KvibLogo {...args} />
+}`,...(l=(i=a.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var c,g,m;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: "Logo",
+    variant: "horizontal"
+  },
+  render: args => <KvibLogo {...args} />
+}`,...(m=(g=s.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var p,d,b;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: "Logo",
+    variant: "horizontal"
+  },
+  render: args => <KvibStack>
+      <KvibLogo {...args} size={100} />
+      <KvibLogo {...args} size={200} />
+      <KvibLogo {...args} size={300} />
+    </KvibStack>
+}`,...(b=(d=t.parameters)==null?void 0:d.docs)==null?void 0:b.source}}};const L=["Preview","LogoHorizontal","LogoSizes"],S=Object.freeze(Object.defineProperty({__proto__:null,LogoHorizontal:s,LogoSizes:t,Preview:a,__namedExportsOrder:L,default:v},Symbol.toStringTag,{value:"Module"}));export{S as L,a as P,s as a,t as b};
