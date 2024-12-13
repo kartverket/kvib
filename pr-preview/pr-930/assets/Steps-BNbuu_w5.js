2b3a49cd29ca45eede7d73a9232ee23c3a9fd18a
chrhein <chrhein@users.noreply.github.com>
2024-12-13 11:56:51 +0000

    Deploy preview for PR 930 🛫

---
+++ pr-preview/pr-930/assets/Steps-BNbuu_w5.js
@@ -0,0 +1,5 @@
+import{j as t}from"./index-D6_wGsWO.js";import{useMDXComponents as n}from"./index-BGZZYR5m.js";import"./index-CS007hxM.js";import{ae as i,af as m,ag as p}from"./index-CdvX09sR.js";import"./Feedback-bo275ffA.js";import{S as a,P as r}from"./Steps.stories-XT26pkQQ.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-5g3BYLcv.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";function s(e){const o={h1:"h1",p:"p",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a}),`
+`,t.jsx(o.h1,{id:"steps",children:"Steps"}),`
+`,t.jsx(o.p,{children:"Steps brukes til å vise fremgang i en prosess med flere steg."}),`
+`,t.jsx(m,{of:r}),`
+`,t.jsx(p,{of:r})]})}function v(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(s,{...e})}):s(e)}export{v as default};
