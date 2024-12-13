d9d72e301ce7aed45b4907aba4e71995a1c252bb
chrhein <chrhein@users.noreply.github.com>
2024-12-13 12:15:48 +0000

    Deploy preview for PR 930 🛫

---
+++ pr-preview/pr-930/assets/Container.stories-DuB7g7vp.js
@@ -0,0 +1,10 @@
+import{j as i}from"./index-D6_wGsWO.js";import{b as n}from"./index-BOVC0mmX.js";const s={title:"Komponenter/Container",component:n,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}}},e={args:{bg:"green.50",p:"2"},render:a=>i.jsx(n,{...a,children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    bg: "green.50",
+    p: "2"
+  },
+  render: args => <KvibContainer {...args}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua.
+    </KvibContainer>
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Preview"],p=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:d,default:s},Symbol.toStringTag,{value:"Module"}));export{p as C,e as P};
