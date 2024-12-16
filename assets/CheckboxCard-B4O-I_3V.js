8a2ce5ceef5d650198a61219033958ca19cded08
chrhein <31273371+chrhein@users.noreply.github.com>
2024-12-16 13:06:00 +0000

    Deploying to gh-pages from @ kartverket/kvib@30b0fb7d3878e56c02a2e7138dd3a082a3432c1c 🚀

---
+++ assets/CheckboxCard-B4O-I_3V.js
@@ -0,0 +1,4 @@
+import{j as o}from"./index-D6_wGsWO.js";import{useMDXComponents as n}from"./index-BGZZYR5m.js";import{ae as a,af as i,ag as m}from"./index-D--UbNyr.js";import{C as p,P as t}from"./CheckboxCard.stories-BE91nCUC.js";import{D as x}from"./Dokumentasjonsside-qxtufe8C.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-Dva9g9Q3.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-hsVzZxqS.js";const f=()=>o.jsx(x,{tittel:"Checkbox Card",beskrivelse:"Vi brukes Checkbox Card for å velge eller oppheve valget av alternativer som vises innenfor kort."});function s(e){return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:p}),`
+`,o.jsx(f,{}),`
+`,o.jsx(i,{of:t}),`
+`,o.jsx(m,{of:t})]})}function g(e={}){const{wrapper:r}={...n(),...e.components};return r?o.jsx(r,{...e,children:o.jsx(s,{...e})}):s()}export{g as default};
---
+++ assets/kom-i-gang-DX7O_TqS.js
@@ -0,0 +1,88 @@
+import{j as e}from"./index-D6_wGsWO.js";import{useMDXComponents as g}from"./index-BGZZYR5m.js";import{F as p,aV as r,y as l,T as h,B as t,I as s,t as o}from"./index-hsVzZxqS.js";import"./chunk-NUUEMKO5-DOiPOBnS.js";import{ae as m}from"./index-D--UbNyr.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-Dva9g9Q3.js";import"../sb-preview/runtime.js";import"./react-18-BzWPIXJh.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";const a=i=>e.jsxs(p,{gap:"18px",...i,children:[e.jsxs(r,{href:"mailto:kvib-feedbacks-aaaahwh252gnftmofucpaa47ca@kartverketgroup.slack.com",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(l,{icon:"mail"}),"Send en mail"]}),e.jsxs(r,{href:"https://slack.com/app_redirect?channel=gen-designsystem",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(l,{icon:"tag"}),"Bli med i slack-kanalen"]}),e.jsxs(r,{href:"https://github.com/kartverket/kvib",fontSize:"lg",fontWeight:400,m:"0",p:"0",children:[e.jsx(l,{size:40,icon:"polyline"}),"Lag en pull-request på github"]})]});try{a.displayName="ContributeLinks",a.__docgenInfo={description:"",displayName:"ContributeLinks",props:{}}}catch{}function d(i){const n={h1:"h1",h2:"h2",li:"li",ol:"ol",p:"p",strong:"strong",ul:"ul",...g(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Kom i gang/For designere"}),`
+`,e.jsx(h,{as:"bold",fontSize:"2xl",children:"For designere"}),`
+`,e.jsx(n.h1,{id:"kom-i-gang-med-design",children:"Kom i gang med design"}),`
+`,e.jsx(n.p,{children:`Kartverkets Designsystem skal hjelpe deg med å bygge digitale løsninger og inneholder alt som er felles på tvers av team og produkter.
+Ved å følge designsystemet sikrer vi at alle våre produkter har et enhetlig uttrykk og god brukeropplevelse.
+Nedenfor finner du en liten guide for å komme i gang med design i Kartverket.`}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"bli-kjent",children:"Bli kjent"}),`
+`,e.jsxs(n.p,{children:[`Før du begynner å designe løsninger for Kartverket, bør du bli kjent med vår
+`,e.jsx(r,{href:"https://www.kartverket.no/om-kartverket/kartverkets-identitet",children:"visuelle identitet"}),`
+for å lære mer om Kartverkets mål, visjon og designprinsipper.`]}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"figma",children:"Figma"}),`
+`,e.jsx(n.p,{children:`Vi bruker Figma som hovedverktøy. Opprett en Figmabruker når du har fått tildelt e-post fra Kartverket.
+Dersom du ønsker Editor-tilgang kan du sende en forespørsel via Figma eller ta kontakt med @Wanni på slack, eller mail @Wanwisa Wanpake Johnsrud.`}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Full editor-tilgang:"})," Primært for designere. Begrenset antall lisenser."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Dev-mode:"})," For utviklere som vil jobbe direkte ut fra skissene."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Viewer-restricted:"})," For utviklere og andre som ønsker å se skissene."]}),`
+`]}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"opprett-prosjekt",children:"Opprett prosjekt"}),`
+`,e.jsx(n.p,{children:"Bruk vår cover template når du oppretter et nytt prosjekt i Figma."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[`Kopier template til ditt prosjekt. Denne finner du i
+`,e.jsx(r,{href:"https://design.kartverket.no",isExternal:!1,children:"Kartverkets Designsystem"}),"."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Legg til prosjektnavn og eier av prosjektet"}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Høyreklikk på cover og trykk “set as thumbnail”"}),`
+`]}),`
+`]}),`
+`,e.jsx(s,{src:"assets/FigmaOpprettProsjekt.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"aktiver-biblioteket",children:"Aktiver biblioteket"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Gå til panelet på venstre siden trykk på assets"}),`
+`,e.jsx(n.li,{children:"Åpne Libraires"}),`
+`,e.jsx(n.li,{children:"Huk av på Kartverkets Designsystem"}),`
+`,e.jsx(n.li,{children:"Nå er det bare å ta i bruk de komponentene du trenger til dine skisser"}),`
+`]}),`
+`,e.jsx(s,{src:"assets/FigmaAktiverBibliotek.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"komponentbiblioteket",children:"Komponentbiblioteket"}),`
+`,e.jsx(n.p,{children:`Kartverket sitt bibliotek i Figma er en fil med alle våre UI-komponenter som er klare til bruk.
+Ved å koble seg til biblioteket får du tilgang til universale komponenter som farger, typografi, knapper, inputfelter, navigasjon og lignende.
+Slik kommer du raskt og effektivt i gang med å sette opp brukergrensensitt til ditt prosjekt.`}),`
+`,e.jsx(s,{src:"assets/FigmaKomponentbiblioteket.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"design-i-figma",children:"Design i Figma"}),`
+`,e.jsxs(n.p,{children:[`Før du setter i gang er det et par ting som er verdt å tenke på.
+Når du har aktivert biblioteket har du nå tilgang til en rekke stiler i designsystemet (design tokens) som vi ønsker at dere bruker,
+istedenfor egendefinerte verdier på f.eks farger og typografi.
+Å bruke
+`,e.jsx(r,{href:"https://help.figma.com/hc/en-us/articles/15339657135383-Guide-to-variables-in-Figma",children:"variables"}),`
+gjør det enkelt å oppdatere hele systemet, og vil også gjøre at du benytter deg av korrekte farger.`]}),`
+`,e.jsx(s,{src:"assets/FigmaVariables.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(n.p,{children:`Kartverket sine hovedfarger er grønn og blå og er lagt inn som to “themes” i Figma.
+Dette gjør det enkelt å f.eks skifte fargene på alle komponenter i et større designelement.`}),`
+`,e.jsx(s,{src:"assets/FigmaThemes.svg",width:"926px",margin:"auto"}),`
+`,e.jsxs(n.p,{children:[`Komponentene er satt opp med
+`,e.jsx(r,{href:"https://help.figma.com/hc/en-us/articles/5731482952599-Add-auto-layout-to-a-design",children:"autolayout"}),`
+og
+`,e.jsx(r,{href:"https://help.figma.com/hc/en-us/articles/360056440594-Create-and-use-variants",children:"variants"}),`
+Vi anbefaler alle som bruker Figma regelmessig å gjøre seg kjent med AutoLayout, dette vil redusere tiden du bruker på å lage detaljerte skisser.`]}),`
+`,e.jsx(s,{src:"assets/FigmaAutolayout.svg",width:"926px",margin:"auto"}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"nyttige-lenker",children:"Nyttige lenker"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(r,{href:"https://design.kartverket.no",children:"Storybook"}),": vår dokumentasjonsside, hvor du finner alt som tilhører vårt designsystem."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(r,{href:"https://kartverket.atlassian.net/jira/software/projects/KOMP/boards/80?atlOrigin=eyJpIjoiYjlmOGFmN2E5ODkwNGNmMjhjYTZkNTRhOTVlZGYwMDciLCJwIjoiaiJ9",children:"Jira"}),": vi bruker Jira til å legge opp oppgaver for hele teamet."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(r,{href:"https://miro.com/app/dashboard/",children:"Miro"}),": vårt samhandlingsverktøy. Benyttes i retro, felles planleging, idemyldring og fasilitering."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(r,{href:"https://github.com/kartverket/kvib",children:"Github"}),": dokumentasjonssiden til designsystemet i Storybook og komponentbiblioteket i React."]}),`
+`,e.jsx(n.li,{children:"Slack:  #gen-designsystem, #designsystem-status, #laug-design"}),`
+`,e.jsxs(n.li,{children:[e.jsx(r,{href:"https://kartverket.sharepoint.com",children:"Innsida"}),": Vi bruker Innsida til å synligjøre designsystemet for hele organisasjonen."]}),`
+`]}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(o,{rightIcon:"chevron_right",mb:"9px",w:"926px",size:"lg",children:"Les mer om hvorda du kan bidra med design"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o,{rightIcon:"chevron_right",mt:"9px",w:"926px",size:"lg",children:"Les om Universell utforming"}),`
+`,e.jsx(t,{py:"41px"}),`
+`,e.jsx(n.h2,{id:"vil-du-bidra",children:"Vil du bidra?"}),`
+`,e.jsx(n.p,{children:"Vi setter pris på alle tilbakemeldinger, ideer og innspill, så ikke nøl med å ta kontakt."}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t,{mb:"87px"})]})}function D(i={}){const{wrapper:n}={...g(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(d,{...i})}):d(i)}export{D as default};
