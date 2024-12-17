31b8d88f443183eb4cd1b4329c7259a8c4e00407
chrhein <chrhein@users.noreply.github.com>
2024-12-17 14:48:17 +0000

    Deploy preview for PR 939 🛫

---
+++ pr-preview/pr-939/assets/Show-DSlgGq8Y.js
@@ -0,0 +1,12 @@
+import{j as e}from"./index-D6_wGsWO.js";import{useMDXComponents as s}from"./index-BGZZYR5m.js";import{ae as i,af as m,ag as a}from"./index-I5YSD9No.js";import{S as p,P as o}from"./Show.stories-BCCbEfpb.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-CSjimUsX.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DX2yoEI4.js";function t(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:p}),`
+`,e.jsx(n.h1,{id:"show",children:"Show"}),`
+`,e.jsx(n.p,{children:"Show er en wrapperkomponent som render eller ikke render child-elementer hvis en gitt betingelse er sann."}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`
+## Ta i bruk
+
+\`\`\`jsx
+import { Show } from "@kvib/react";
+`})}),`
+`,e.jsx(n.h2,{id:"props",children:"Props"}),`
+`,e.jsx(m,{of:o}),`
+`,e.jsx(a,{of:o})]})}function k(r={}){const{wrapper:n}={...s(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{k as default};
