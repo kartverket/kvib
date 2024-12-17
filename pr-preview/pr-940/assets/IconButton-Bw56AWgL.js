45bf5f487feb9575cf0767ca37dcbb02e2742964
chrhein <chrhein@users.noreply.github.com>
2024-12-17 15:35:08 +0000

    Remove preview for PR 940 🛬

--- pr-preview/pr-940/assets/IconButton-Bw56AWgL.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./index-D6_wGsWO.js";import{useMDXComponents as s}from"./index-BGZZYR5m.js";import{ae as i,af as a,ag as m}from"./index-BM91so7J.js";import{I as p,P as r}from"./IconButton.stories-Dd_AuM1X.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-bMspTtLn.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-DX2yoEI4.js";function t(o){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...s(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:p}),`
-`,e.jsx(n.h1,{id:"icon-button",children:"Icon Button"}),`
-`,e.jsx(n.p,{children:"Her er en oversikt over utvalget av ikonknapper i Kartverkets Designbibliotek. Disse er blant annet bygget opp ved bruk av design tokens fra biblioteket, eksempelvis farger og ikoner. I dokumentasjonen følger beskrivelse av komponenten og bruksområde, sammen med kodeeksempler."}),`
-`,e.jsx(n.h2,{id:"ta-i-bruk",children:"Ta i bruk"}),`
-`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { IconButton } from "@kvib/react";
-`})}),`
-`,e.jsx(n.h2,{id:"props",children:"Props"}),`
-`,e.jsx(a,{of:r}),`
-`,e.jsx(m,{of:r})]})}function v(o={}){const{wrapper:n}={...s(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(t,{...o})}):t(o)}export{v as default};
