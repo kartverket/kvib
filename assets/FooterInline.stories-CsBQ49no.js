d263e731032bdec39c40e89d7400a12ba21be37f
chrhein <31273371+chrhein@users.noreply.github.com>
2024-12-17 14:55:00 +0000

    Deploying to gh-pages from @ kartverket/kvib@77007aece2f2b18c1187a912b0312ea4c8fd0849 🚀

---
+++ assets/FooterInline.stories-CsBQ49no.js
@@ -0,0 +1,12 @@
+import{j as s}from"./index-D6_wGsWO.js";import{bd as o,aV as p}from"./index-DX2yoEI4.js";const u={title:"Komponenter/Footer Inline",component:o,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"shown"}}},argTypes:{accessibilityUrl:{description:"Link to accessibility statement",table:{type:{summary:"string"}},control:"text"},privacyUrl:{description:"Link to privacy statement",table:{type:{summary:"string"}},control:"text"},logoLink:{description:"Href for logo link",table:{type:{summary:"string"},defaulValue:{summary:"/"}},control:"text"}}},e={render:r=>s.jsx(o,{...r})},t={args:{accessibilityUrl:"https://uustatus.no/nb/erklaringer/publisert/f048c5ff-2167-48c5-b706-bd106c15e9c9"},render:r=>s.jsx(o,{...r,children:s.jsx(p,{href:"https://github.com/kartverket/kvib","aria-label":"Besøk Kartverkets Designsystem på Github",fontWeight:"bold",children:"Github"})})};var n,i,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => <KvibFooterInline {...args} />
+}`,...(a=(i=e.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var c,l,m;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    accessibilityUrl: "https://uustatus.no/nb/erklaringer/publisert/f048c5ff-2167-48c5-b706-bd106c15e9c9"
+  },
+  render: args => <KvibFooterInline {...args}>
+      <Link href="https://github.com/kartverket/kvib" aria-label="Besøk Kartverkets Designsystem på Github" fontWeight="bold">
+        Github
+      </Link>
+    </KvibFooterInline>
+}`,...(m=(l=t.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const b=["Preview","FooterInlineCustom"],k=Object.freeze(Object.defineProperty({__proto__:null,FooterInlineCustom:t,Preview:e,__namedExportsOrder:b,default:u},Symbol.toStringTag,{value:"Module"}));export{k as F,e as P,t as a};
