73b8df7c290c1f79938f76b0d8c0519f297a7eac
chrhein <chrhein@users.noreply.github.com>
2024-12-18 11:36:29 +0000

    Deploy preview for PR 941 🛫

---
+++ pr-preview/pr-941/assets/Fieldset.stories-CDJmzDEG.js
@@ -0,0 +1,6 @@
+import{j as t}from"./index-D6_wGsWO.js";import{aM as i,aN as a,aO as l}from"./index-Cs5bAGmQ.js";const d={title:"Komponenter/Fieldset",component:i,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"hidden"}}},argTypes:{invalid:{description:"Indicates whether the fieldset is invalid.",table:{type:{summary:"boolean"}},control:"boolean"}}},e={render:o=>t.jsxs(i,{...o,children:[t.jsx(a,{children:"Tittel i et Fieldset"}),t.jsx(l,{children:"En samling av flere Field"})]})};var s,r,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: args => <Fieldset {...args}>
+      <FieldsetLegend>Tittel i et Fieldset</FieldsetLegend>
+      <FieldsetContent>En samling av flere Field</FieldsetContent>
+    </Fieldset>
+}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const c=["Preview"],F=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:c,default:d},Symbol.toStringTag,{value:"Module"}));export{F,e as P};
