6a157007d4efe24ebbdac5d7db85de250e85b0ff
chrhein <chrhein@users.noreply.github.com>
2024-12-19 13:54:23 +0000

    Deploy preview for PR 948 🛫

--- pr-preview/pr-948/assets/Link.stories-CHZO9JMt.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./index-D6_wGsWO.js";import{aV as i}from"./index-BOKewEU3.js";const s={title:"Komponenter/Link",component:i,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"hidden"}}},argTypes:{variant:{description:"The visual style of the component.",table:{type:{summary:"string"}},defaultValue:{summary:"underline | plain"},options:["underline","plain"],control:{type:"radio"}},children:{description:"This is the linktext",table:{type:{summary:"string"}},control:"text"},external:{description:"If true, an icon will be included.",table:{type:{summary:"Boolean"},defaultValue:{summary:"false"}},control:"boolean"},href:{description:"This is the link.",table:{type:{summary:"string"}},control:"text"}}},e={args:{children:"Dette er en lenke",href:"/?path=/"},render:r=>o.jsx(i,{...r,children:r.children})};var t,n,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: "Dette er en lenke",
-    href: "/?path=/"
-  },
-  render: args => <KvibLink {...args}>{args.children}</KvibLink>
-}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const l=["Preview"],p=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:l,default:s},Symbol.toStringTag,{value:"Module"}));export{p as L,e as P};
---
+++ pr-preview/pr-948/assets/Portal-G0gplBoL.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-D6_wGsWO.js";import{useMDXComponents as s}from"./index-BGZZYR5m.js";import{ae as i,af as m,ag as a}from"./index-CAqO-VyW.js";import{P as p,a as n}from"./Portal.stories-Cico0lHF.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-CUTScTSX.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-COVntcoL.js";function e(o){const{PortalDocs:r}={...s(),...o.components};return r||c("PortalDocs"),t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(r,{}),`
+`,t.jsx(m,{of:n}),`
+`,t.jsx(a,{of:n})]})}function w(o={}){const{wrapper:r}={...s(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}function c(o,r){throw new Error("Expected component `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{w as default};
