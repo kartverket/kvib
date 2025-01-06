864556951d96fd895848728a1e5dc4bb59707e49
chrhein <chrhein@users.noreply.github.com>
2025-01-06 11:52:07 +0000

    Remove preview for PR 955 🛬

--- pr-preview/pr-955/assets/Footer-DpUiZdW3.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./index-D6_wGsWO.js";import{useMDXComponents as j}from"./index-BGZZYR5m.js";import{ae as d,af as o,ag as s}from"./index-DqgGlLQr.js";import{F as g,P as i,a}from"./Footer.stories-CpTbhWkl.js";import{D as m}from"./Dokumentasjonsside-DXrbaQxy.js";import{T as p,L as v,a as r}from"./Tekstblokk-CRRShKMn.js";import"./index-C7I6S4tq.js";import"./index-EN4ZLXvn.js";import"./iframe-D5RDOusJ.js";import"../sb-preview/runtime.js";import"./index-CHGET4sZ.js";import"./index-DrFu-skq.js";import"./index-BQkUhusG.js";const x=()=>e.jsx(m,{tittel:"Footer",beskrivelse:"En footer er en seksjon som vises nederst på en nettside og sikrer at brukerne kan finne nødvendig informasjon på et kjent sted. Komponten er med å tydeliggjøre Kartverket som avsender.",children:e.jsx(p,{tittel:"Nyttige retningslinjer",children:e.jsxs(v,{children:[e.jsx(r,{children:"unngå overbelastning av innhold og inkluder bare nødvendige og relevante lenker."}),e.jsx(r,{children:"innholdet bør tilpasses skjermstørrelsen, og eventuelle navigasjonslenger skal være lett tilgjengelig."}),e.jsx(r,{children:"Husk at footeren må inneholde lenke til personvern og tilgjengelighet."})]})})}),f=()=>e.jsx(m,{tittel:"Footer Inline",beskrivelse:"Footeren kommer i to varianter: en inline-variant og en vanlig variant. Inline-varianten er en footer som er mer minimalistisk og tar mindre plass enn den vanlige varianten. Den vanlige varianten er en mer tradisjonell footer som viser mer informasjon. Begge disse kan tilpasses med ulike props."});function l(n){return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:g}),`
-`,e.jsx(x,{}),`
-`,e.jsx(o,{of:i}),`
-`,e.jsx(s,{of:i}),`
-`,e.jsx("br",{}),`
-`,e.jsx("br",{}),`
-`,e.jsx(f,{}),`
-`,e.jsx(o,{of:a}),`
-`,e.jsx(s,{of:a})]})}function w(n={}){const{wrapper:t}={...j(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(l,{...n})}):l()}export{w as default};
