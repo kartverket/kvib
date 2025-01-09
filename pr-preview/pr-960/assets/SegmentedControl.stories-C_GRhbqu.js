089b8530596204e7dc9259ce64957855a1c20a82
chrhein <chrhein@users.noreply.github.com>
2025-01-09 14:28:12 +0000

    Deploy preview for PR 960 🛫

---
+++ pr-preview/pr-960/assets/SegmentedControl.stories-C_GRhbqu.js
@@ -0,0 +1,15 @@
+import{j as a}from"./jsx-runtime-DgHZ7iq2.js";import{aT as l}from"./index-CGNKToUm.js";const s={title:"Komponenter/Segmented Control",component:l,parameters:{docs:{story:{inline:!0},canvas:{sourceState:"hidden"}}},argTypes:{defaultValue:{description:"The initial value of the segment group when it is first rendered. Use when you do not need to control the state of the segment group.",table:{type:{summary:"string"}}},colorPalette:{description:"Color palette of the component",table:{type:{summary:"green | blue | gray | red"},defaultValue:{summary:"green"}},options:["green","blue","gray","red"],control:{type:"radio"}},size:{description:"Size of the component",table:{type:{summary:"sm | md | lg"},defaultValue:{summary:"md"}},options:["sm","md","lg"],control:{type:"radio"}},orientation:{description:"Orientation of the component",table:{type:{summary:"horizontal | vertical"},defaultValue:{summary:"horizontal"}},options:["horizontal","vertical"],control:{type:"radio"}},value:{description:"The value of the checked segment",table:{type:{summary:"string"}}},onValueChange:{description:"Callback invoked when the value changes",table:{type:{summary:"(details: ValueChangeDetails) => void"}}}}},i=t=>a.jsx(l,{...t}),e={args:{items:[{value:"en",label:"En"},{value:"to",label:"To"},{value:"tre",label:"Tre"}]},render:t=>a.jsx(i,{...t})};var o,r,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      value: "en",
+      label: "En"
+    }, {
+      value: "to",
+      label: "To"
+    }, {
+      value: "tre",
+      label: "Tre"
+    }]
+  },
+  render: args => <SegmentedControlExample {...args} />
+}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const m=["Preview"],p=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:m,default:s},Symbol.toStringTag,{value:"Module"}));export{e as P,p as S};
--- pr-preview/pr-960/assets/Stat.stories-C3BBuuQM.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./jsx-runtime-DgHZ7iq2.js";import{b9 as s,ba as i,bb as m}from"./index-Dsql8TDy.js";const l={title:"Komponenter/Stat",component:s,parameters:{docs:{story:{inline:!0}}},argTypes:{size:{description:"The size of the component",table:{type:{summary:" sm | md | lg"},defaultValue:{summary:"md"}},options:["sm","md","lg"],control:{type:"radio"}}}},t={render:n=>e.jsxs(s,{...n,children:[e.jsx(i,{children:"Utgifter"}),e.jsx(m,{children:"3200kr"})]})};var r,a,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: (args: StatProps) => <KvibStat {...args}>
-      <KvibStatLabel>Utgifter</KvibStatLabel>
-      <KvibStatNumber>3200kr</KvibStatNumber>
-    </KvibStat>
-}`,...(o=(a=t.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const p=["Preview"],b=Object.freeze(Object.defineProperty({__proto__:null,Preview:t,__namedExportsOrder:p,default:l},Symbol.toStringTag,{value:"Module"}));export{t as P,b as S};
