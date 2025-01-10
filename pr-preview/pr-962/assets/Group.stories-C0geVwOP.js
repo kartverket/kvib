ebca101a06d01f159b8d5d162b54a16070f39511
chrhein <chrhein@users.noreply.github.com>
2025-01-10 12:11:47 +0000

    Remove preview for PR 962 🛬

--- pr-preview/pr-962/assets/Group.stories-C0geVwOP.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-DgHZ7iq2.js";import{bf as r,B as o}from"./index-CoJY4uhr.js";const l={title:"Komponenter/Layout/Group",component:r,parameters:{docs:{story:{inline:!0}}},argTypes:{attached:{description:"Attach children to each other",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}},control:{type:"boolean"}},grow:{description:"Grow children to fill the container",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}},control:{type:"boolean"}}}};o.displayName="Button";r.displayName="Group";const e={render:i=>t.jsxs(r,{...i,children:[t.jsx(o,{variant:"outline",children:"First"}),t.jsx(o,{variant:"outline",children:"Second"})]})};var a,n,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  render: args => <Group {...args}>
-      <Button variant="outline">First</Button>
-      <Button variant="outline">Second</Button>
-    </Group>
-}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const u=["Preview"],d=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:u,default:l},Symbol.toStringTag,{value:"Module"}));export{d as G,e as P};
