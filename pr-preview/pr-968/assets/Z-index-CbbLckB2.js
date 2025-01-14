f9ae0c1b71d7433de9ea7f39a424e6e870bf56e3
chrhein <chrhein@users.noreply.github.com>
2025-01-14 08:31:26 +0000

    Deploy preview for PR 968 🛫

---
+++ pr-preview/pr-968/assets/Z-index-CbbLckB2.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-DQFEoQHt.js";import{useMDXComponents as r}from"./index-DQ9BjFnV.js";import"./chunk-NUUEMKO5-C_iWeKMf.js";import{ae as i,al as s}from"./index-0EjOLlsg.js";import"./index-CbFAwPTn.js";import"./iframe-DPmRFo0M.js";import"../sb-preview/runtime.js";import"./react-18-DdqO7fqk.js";import"./index-CqJK9h7E.js";import"./index-Crg4-8o8.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";const m=`
+export const zIndices = {
+  hide: -1,
+  auto: "auto",
+  base: 0,
+  docked: 10,
+  dropdown: 1000,
+  sticky: 1100,
+  banner: 1200,
+  overlay: 1300,
+  modal: 1400,
+  popover: 1500,
+  skipLink: 1600,
+  toast: 1700,
+  tooltip: 1800,
+};
+`;function e(t){const n={h2:"h2",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Komponenter/Tokens/Z-index"}),`
+`,o.jsx(n.h2,{id:"z-index",children:"Z-index"}),`
+`,o.jsx(s,{code:m,dark:!0})]})}function y(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{y as default};
