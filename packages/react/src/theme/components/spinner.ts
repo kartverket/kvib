64f485e5c2e23c0f45acdcaf1b32c6c7c9a45fe1
Christian Hein <scheiehein@gmail.com>
2025-01-15 10:49:56 +0100

    Fiks for Spinner

--- packages/react/src/theme/components/spinner.ts
+++ packages/react/src/theme/components/spinner.ts
@@ -3,7 +3,6 @@ import { defineRecipe } from "@chakra-ui/react";
 export const spinnerTheme = defineRecipe({
   className: "kvib-spinner",
   base: {
-    colorPalette: "green",
     color: "currentColor",
   },
 });
