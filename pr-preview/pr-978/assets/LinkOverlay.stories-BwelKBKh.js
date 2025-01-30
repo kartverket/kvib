31ee143e7922e760dddba610ee41a8628311cb71
Wannijoh <Wannijoh@users.noreply.github.com>
2025-01-30 13:00:27 +0000

    Remove preview for PR 978 🛬

--- pr-preview/pr-978/assets/LinkOverlay.stories-BwelKBKh.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./jsx-runtime-DQFEoQHt.js";import{L as a,a as t}from"./link-box-1ZHhZhF1.js";import{T as n}from"./text-sMekb8FJ.js";const m={title:"Komponenter/Link Overlay",component:a,parameters:{docs:{story:{inline:!0}}}};t.displayName="LinkBox";a.displayName="LinkOverlay";n.displayName="Text";const e={render:d=>r.jsxs(t,{as:"article",maxW:"sm",p:"5",borderWidth:"1px",rounded:"md",children:[r.jsx(a,{...d,href:"#"}),r.jsx(n,{children:"This area is clickable"})]})};var i,s,o;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  render: args => <KvibLinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
-      <KvibLinkOverlay {...args} href="#" />
-      <KvibText>This area is clickable</KvibText>
-    </KvibLinkBox>
-}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const l=["Preview"],v=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:l,default:m},Symbol.toStringTag,{value:"Module"}));export{v as L,e as P};
