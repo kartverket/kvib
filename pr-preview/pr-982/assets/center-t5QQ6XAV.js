5dbf963c4276f447145e29dfe7440884dd531dae
Wannijoh <Wannijoh@users.noreply.github.com>
2025-02-03 08:26:25 +0000

    Remove preview for PR 982 🛬

--- pr-preview/pr-982/assets/center-t5QQ6XAV.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./factory-C_AV5G8T.js";const n=e("div",{base:{display:"flex",alignItems:"center",justifyContent:"center"},variants:{inline:{true:{display:"inline-flex"}}}});n.displayName="Center";export{n as C};
