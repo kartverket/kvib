ad681a9561b9a2a7db2869d4441d4d48ed142bef
jonandresen <jonandresen@users.noreply.github.com>
2025-04-14 10:39:41 +0000

    Remove preview for PR 996 🛬

--- pr-preview/pr-996/assets/Logo.stories-fuqNPMfO.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{am as r}from"./index-BXsEK0gK.js";const l={title:"Komponenter/Logo",component:r,parameters:{docs:{story:{inline:!0}}},argTypes:{label:{table:{type:{summary:"string"}},control:"text"},variant:{table:{type:{summary:"vertical | horizontal"},defaultValue:{summary:"vertical"}},options:["vertical","horizontal"],control:{type:"radio"}},size:{table:{type:{summary:"number"}},control:"number"}}};r.displayName="Logo";const e={args:{label:"Logo"},render:s=>n.jsx(r,{...s})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    label: "Logo"
-  },
-  render: args => <KvibLogo {...args} />
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const m=["Preview"],c=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:m,default:l},Symbol.toStringTag,{value:"Module"}));export{c as L,e as P};
--- pr-preview/pr-996/assets/index-C-jSWjhw.js
+++
@@ -1,1 +0,0 @@
-import"./preview-CGEjFD73.js";import"./DocsRenderer-CFRXHY34-CZm-dBqO.js";import"./index-CtEARYBd.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
