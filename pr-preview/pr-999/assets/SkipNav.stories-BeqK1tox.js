3673f336338905df38f151f461645b1329626900
Wannijoh <Wannijoh@users.noreply.github.com>
2025-04-22 07:04:54 +0000

    Remove preview for PR 999 🛬

--- pr-preview/pr-999/assets/SkipNav.stories-BeqK1tox.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{S as t}from"./skip-nav-link-BC8crZtK.js";import{C as i}from"./code-1_VME73E.js";const p={title:"Komponenter/SkipNav",component:t,parameters:{docs:{story:{inline:!0}}},argTypes:{children:{description:"The content of the SkipNavLink",table:{type:{summary:"ReactNode"}},control:"text"}},args:{children:"Skip to content"}};t.displayName="SkipNavLink";const e={render:n=>r.jsxs("div",{children:["Click and press ",r.jsx(i,{children:"tab"}),".",r.jsx(t,{...n})]})};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: args => <div>
-      Click and press <Code>tab</Code>.
-      <KvibSkipNavLink {...args} />
-    </div>
-}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const d=["Preview"],v=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:d,default:p},Symbol.toStringTag,{value:"Module"}));export{e as P,v as S};
--- pr-preview/pr-999/assets/kbd-B2j_-PiN.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./create-recipe-context-DYiacIet.js";const{withContext:e}=t({key:"kbd"}),c=e("kbd");export{c as K};
