d529a07087c71a73d82d0ff69b31310c19f92b77
Wannijoh <101319269+Wannijoh@users.noreply.github.com>
2025-08-04 13:08:36 +0000

    Deploying to gh-pages from @ kartverket/kvib@9c93a0be5aa85002a117f72b3019edc9fd3da49e 🚀

--- assets/Stat.stories-B4T66OuI.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{b4 as a,b5 as i,b6 as m}from"./index-Culm_dvB.js";const l={title:"Komponenter/Stat",component:a,parameters:{docs:{story:{inline:!0}}},argTypes:{size:{description:"The size of the component",table:{type:{summary:" sm | md | lg"},defaultValue:{summary:"md"}},options:["sm","md","lg"],control:{type:"radio"}}}};a.displayName="Stat";i.displayName="StatLabel";m.displayName="StatNumber";const e={render:n=>t.jsxs(a,{...n,children:[t.jsx(i,{children:"Label"}),t.jsx(m,{children:"3200"})]})};var r,s,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: (args: StatProps) => <KvibStat {...args}>
-      <KvibStatLabel>Label</KvibStatLabel>
-      <KvibStatNumber>3200</KvibStatNumber>
-    </KvibStat>
-}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const p=["Preview"],c=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:p,default:l},Symbol.toStringTag,{value:"Module"}));export{e as P,c as S};
--- assets/index-wDeikmCD.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Cg1FgIlW.js";import"./DocsRenderer-CFRXHY34-BNgSwpEI.js";import"./index-BckPB46V.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
