219d47807ecefe7cbd2e29017158171dab5a312e
DanielBerge <DanielBerge@users.noreply.github.com>
2025-08-12 10:05:27 +0000

    Remove preview for PR 1026 🛬

--- pr-preview/pr-1026/assets/Fieldset.stories-DwAXhrW8.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{F as o,a as d,b as l}from"./fieldset-DGwjdXII.js";const a={title:"Komponenter/Fieldset",component:o,parameters:{docs:{story:{inline:!0}}},argTypes:{invalid:{description:"Indicates whether the fieldset is invalid.",table:{type:{summary:"boolean"}},control:"boolean"}}},e={render:i=>t.jsxs(o,{...i,children:[t.jsx(d,{children:"Fieldset title"}),t.jsx(l,{children:"Fieldset content"})]})};var s,r,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  render: args => <Fieldset {...args}>
-      <FieldsetLegend>Fieldset title</FieldsetLegend>
-      <FieldsetContent>Fieldset content</FieldsetContent>
-    </Fieldset>
-}`,...(n=(r=e.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const c=["Preview"],F=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:c,default:a},Symbol.toStringTag,{value:"Module"}));export{F,e as P};
