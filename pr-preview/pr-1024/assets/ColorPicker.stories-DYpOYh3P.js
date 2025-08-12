bf2b2ee516475a1550d8b828048716204ee60441
DanielBerge <DanielBerge@users.noreply.github.com>
2025-08-12 09:46:08 +0000

    Deploy preview for PR 1024 🛫

---
+++ pr-preview/pr-1024/assets/ColorPicker.stories-DYpOYh3P.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{z as r,D as i,E as n,F as s,G as c,J as p,K as d,H as u,L as m,M as C}from"./index-BFEzvpvH.js";const y={title:"Komponenter/Color Picker",component:r,parameters:{docs:{story:{inline:!0}}},argTypes:{defaultValue:{description:"The initial value of the color picker when it is first rendered. Use when you do not need to control the state of the color picker.",table:{type:{summary:"Color"}},defaultValue:{summary:"#000000"}},value:{description:"The current color value",table:{type:{summary:"Color"}}},format:{description:"The format of the color picker",table:{type:{summary:"ColorFormat"},defaultValue:{summary:"rgba"}}},defaultOpen:{description:"Whether the color picker is open by default",table:{type:{summary:"boolean"},defaultValue:{summary:"false"}}},positioning:{description:"The positioning of the color picker",table:{type:{summary:"PositioningOptions"}}},open:{description:"Whether the color picker is open",table:{type:{summary:"boolean"}}},onValueChange:{description:"Function called when the color value changes",table:{type:{summary:"(details: ValueChangeDetails) => void"}}},onValueChangeEnd:{description:"Handler that is called when the user stops dragging.",table:{type:{summary:"(details: ValueChangeDetails) => void"}}},onPointerDownOutside:{description:"Function called when the pointer is pressed down outside the component",table:{type:{summary:"(event: PointerDownOutsideEvent) => void"}}},openAutoFocus:{description:"Whether the color picker should autofocus when opened",table:{type:{summary:"boolean"},defaultValue:{summary:"true"}}}}};r.displayName="ColorPicker";d.displayName="ColorPickerArea";p.displayName="ColorPickerContent";n.displayName="ColorPickerControl";m.displayName="ColorPickerEyeDropper";s.displayName="ColorPickerInput";i.displayName="ColorPickerLabel";C.displayName="ColorPickerSliders";c.displayName="ColorPickerTrigger";u.displayName="HStack";const o={render:k=>e.jsxs(r,{...k,children:[e.jsx(i,{children:"Select color"}),e.jsxs(n,{children:[e.jsx(s,{}),e.jsx(c,{})]}),e.jsxs(p,{children:[e.jsx(d,{}),e.jsxs(u,{children:[e.jsx(m,{}),e.jsx(C,{})]})]})]})};var t,a,l;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => <ColorPicker {...args}>
+      <ColorPickerLabel>Select color</ColorPickerLabel>
+      <ColorPickerControl>
+        <ColorPickerInput />
+        <ColorPickerTrigger />
+      </ColorPickerControl>
+      <ColorPickerContent>
+        <ColorPickerArea />
+        <HStack>
+          <ColorPickerEyeDropper />
+          <ColorPickerSliders />
+        </HStack>
+      </ColorPickerContent>
+    </ColorPicker>
+}`,...(l=(a=o.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const h=["Preview"],b=Object.freeze(Object.defineProperty({__proto__:null,Preview:o,__namedExportsOrder:h,default:y},Symbol.toStringTag,{value:"Module"}));export{b as C,o as P};
---
+++ pr-preview/pr-1024/assets/index-R67ZkzIN.js
@@ -0,0 +1,1 @@
+import"./preview-BY8bLSJQ.js";import"./DocsRenderer-CFRXHY34-F_bkMFF1.js";import"./index-BX_1-HY4.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
