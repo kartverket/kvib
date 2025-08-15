6cfb67de6d41f8377c29e9d0e6c5693a5d32b7f7
Daniel Berge <daniel.berge@bekk.no>
2025-08-15 09:21:19 +0200

    Legg til logo

--- packages/react/assets/index.ts
+++ packages/react/assets/index.ts
@@ -1,2 +1,3 @@
 export * from "./logo_horizontal";
 export * from "./logo_vertical";
+export * from "./logo_symbol";
--- packages/react/src/logo/Logo.tsx
+++ packages/react/src/logo/Logo.tsx
@@ -1,11 +1,11 @@
-import { LogoHorizontal, LogoVertical } from "@kvib/react/assets";
+import { LogoHorizontal, LogoVertical, LogoSymbol } from "@kvib/react/assets";
 
 export type LogoProps = {
   /** The variant of the logo
    * @default "vertical"*/
-  variant?: "vertical" | "horizontal";
+  variant?: "vertical" | "horizontal" | "symbol";
   /** The size of the logo. Width in pixels.
-   * @default 184/125*/
+   * @default 184/125/37*/
   size?: number;
   /** Aria-label for the logo.
    * @default "logo"*/
@@ -15,15 +15,36 @@ export type LogoProps = {
 export const Logo = ({ variant = "vertical", size, label }: LogoProps) => {
   const { width, height } = computeDimensions(variant, size);
 
-  if (variant === "horizontal") {
-    return <LogoHorizontal label={label} width={width} height={height} />;
+  switch (variant) {
+    case "horizontal":
+      return <LogoHorizontal label={label} width={width} height={height} />;
+    case "symbol":
+      return <LogoSymbol label={label} width={width} height={height} />;
+    case "vertical":
+    default:
+      return <LogoVertical label={label} width={width} height={height} />;
   }
-  return <LogoVertical label={label} width={width} height={height} />;
 };
 
-const computeDimensions = (variant: "vertical" | "horizontal", size?: number) => {
-  const defaultWidth = variant === "horizontal" ? 184 : 125;
-  const aspectRatio = variant === "horizontal" ? 64 / 184 : 91 / 125;
+const computeDimensions = (variant: "vertical" | "horizontal" | "symbol", size?: number) => {
+  let defaultWidth: number;
+  let aspectRatio: number;
+
+  switch (variant) {
+    case "horizontal":
+      defaultWidth = 184;
+      aspectRatio = 64 / 184;
+      break;
+    case "symbol":
+      defaultWidth = 42;
+      aspectRatio = 60 / 42;
+      break;
+    case "vertical":
+    default:
+      defaultWidth = 125;
+      aspectRatio = 91 / 125;
+      break;
+  }
 
   const width = size ?? defaultWidth;
   const height = width * aspectRatio;
