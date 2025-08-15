f10d542107f6c313c3d5518c7bae295aa21bf136
Daniel Berge <daniel.berge@bekk.no>
2025-08-15 11:07:17 +0200

    Revert "Oppdater docs"
    This reverts commit 7cccc32006b565ffad1fba08b4037a37b7545269.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,18 +1,26 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { dirname, join } from "path";
 
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm",
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
   ],
   core: {
     builder: "@storybook/builder-vite",
   },
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
 
