6239da7fc494513f486cf1efe684b8157c24ff47
Erlend Stølan <112624037+erlendst@users.noreply.github.com>
2025-08-22 07:32:46 +0200

    Fikser styling på mobil i Header (#1030)

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,5 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { dirname, join } from "path";
+import { dirname, join } from "node:path";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
 
 /**
  * This function is used to resolve the absolute path of a package.
