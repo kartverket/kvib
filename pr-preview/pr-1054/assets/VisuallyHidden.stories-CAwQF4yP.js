af34542481fd5f2d9255ce106928f335b9251efd
jonandresen <jonandresen@users.noreply.github.com>
2026-01-14 11:59:56 +0000

    Remove preview for PR 1054 🛬

--- pr-preview/pr-1054/assets/VisuallyHidden.stories-CAwQF4yP.js
+++
@@ -1,6 +0,0 @@
-import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{V as r}from"./visually-hidden-C6HJDgNH.js";import{B as a}from"./index-C0A5lRCM.js";import{T as n}from"./index-vDsMs_XN.js";const l={title:"Komponenter/Visually Hidden",component:r,parameters:{docs:{story:{inline:!0}}}};r.displayName="VisuallyHidden";n.displayName="Text";a.displayName="Box";const e={render:d=>s.jsxs(a,{children:[s.jsx(r,{...d,children:"This text is visually hidden"}),s.jsx(n,{children:"Check the DOM to see hidden element"})]})};var t,i,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => <Box>
-      <KvibVisuallyHidden {...args}>This text is visually hidden</KvibVisuallyHidden>
-      <Text>Check the DOM to see hidden element</Text>
-    </Box>
-}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const m=["Preview"],y=Object.freeze(Object.defineProperty({__proto__:null,Preview:e,__namedExportsOrder:m,default:l},Symbol.toStringTag,{value:"Module"}));export{e as P,y as V};
--- pr-preview/pr-1054/assets/index-B9Psn0MX.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./index-BqsZ9I6k.js";import{r as o}from"./index-BYXsJVy_.js";var t=o();const m=r(t);export{m as $,t as r};
