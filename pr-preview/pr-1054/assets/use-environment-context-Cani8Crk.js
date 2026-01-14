f3d697ed7be82baf384a69a8bc16bbb59ee63a78
jonandresen <jonandresen@users.noreply.github.com>
2026-01-14 11:54:01 +0000

    Deploy preview for PR 1054 🛫

---
+++ pr-preview/pr-1054/assets/use-environment-context-Cani8Crk.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-DkbpjeHr.js";const[n,o]=e({name:"EnvironmentContext",hookName:"useEnvironmentContext",providerName:"<EnvironmentProvider />",strict:!1,defaultValue:{getRootNode:()=>document,getDocument:()=>document,getWindow:()=>window}});export{n as E,o as u};
