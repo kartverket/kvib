70be6026169f435257dece5ce09efa5cb1c9b030
magnram <magnram@users.noreply.github.com>
2026-01-14 07:56:02 +0000

    Remove preview for PR 1050 🛬

--- pr-preview/pr-1050/assets/index-Cf8No4lJ.js
+++
@@ -1,1 +0,0 @@
-import"./preview-ClJ1aAEq.js";import"./DocsRenderer-CFRXHY34-mM0wNh3M.js";import"./index-DbjterFs.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
--- pr-preview/pr-1050/assets/spinner-D1u7r_-W.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./create-recipe-context-DELc7dO7.js";const{withContext:n,PropsProvider:r}=e({key:"spinner"}),o=n("span");o.displayName="Spinner";const s=r;export{o as S,s as a};
