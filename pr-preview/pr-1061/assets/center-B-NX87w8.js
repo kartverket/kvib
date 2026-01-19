4752652aa2e30f40d50cdd554b8124021e66f66b
magnram <magnram@users.noreply.github.com>
2026-01-19 12:49:46 +0000

    Deploy preview for PR 1061 🛫

---
+++ pr-preview/pr-1061/assets/center-B-NX87w8.js
@@ -0,0 +1,1 @@
+import{c as e}from"./factory-D1GN6RGf.js";const n=e("div",{base:{display:"flex",alignItems:"center",justifyContent:"center"},variants:{inline:{true:{display:"inline-flex"}}}});n.displayName="Center";export{n as C};
---
+++ pr-preview/pr-1061/assets/spinner-2fgN4LEd.js
@@ -0,0 +1,1 @@
+import{c as e}from"./create-recipe-context-C9VbXybd.js";const{withContext:n,PropsProvider:r}=e({key:"spinner"}),o=n("span");o.displayName="Spinner";const s=r;export{o as S,s as a};
